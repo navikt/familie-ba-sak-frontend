e55836147119214aa49eaacf9b60b8a32502006b
Meng Zhu <meng.zhu@nav.no>
2019-12-05 11:24:38 +0100

    create components

--- src/frontend/komponenter/Fagsak/Vedtak/OppsummeringVedtak.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/OppsummeringVedtak.tsx
@@ -1,16 +1,40 @@
-import { Systemtittel, Undertittel } from 'nav-frontend-typografi';
-import * as React from 'react';
 import { IFagsak } from '../../../typer/fagsak';
+import { Knapp } from 'nav-frontend-knapper';
+import * as React from 'react';
+import { Systemtittel } from 'nav-frontend-typografi';
 
 interface IVedtakProps {
     fagsak: IFagsak;
 }
 
+interface IOppsummeringFrameProps {
+    content: string;
+    [name: string]: any;
+}
+
+const OppsummeringFrame: React.FunctionComponent<IOppsummeringFrameProps> = ({ ...props }) => {
+    const iframe = React.createRef<HTMLIFrameElement>();
+    React.useEffect(() => {
+        iframe.current.contentDocument.body.innerHTML = props.content;
+    });
+
+    return <iframe ref={iframe} className="iframe" />;
+};
+
 const OppsummeringVedtak: React.FunctionComponent<IVedtakProps> = ({ fagsak }) => {
     return (
         <div className="oppsummering">
             <Systemtittel children={'Vedtaksbrev'} />
-            {fagsak.saksnummer}
+            <br />
+            <OppsummeringFrame content="<h1>hello</h1>" />
+            <br />
+            <Knapp
+                onClick={() => {
+                    alert('TODO: send to backend');
+                }}
+            >
+                Send
+            </Knapp>
         </div>
     );
 };
