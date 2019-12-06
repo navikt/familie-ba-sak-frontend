a0ed84463d08137b71fa3dcedf66548685f2e38c
Meng Zhu <meng.zhu@nav.no>
2019-12-06 13:14:49 +0100

    add api call

--- src/frontend/komponenter/Fagsak/Vedtak/OppsummeringVedtak.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/OppsummeringVedtak.tsx
@@ -2,6 +2,8 @@ import { IFagsak } from '../../../typer/fagsak';
 import { Knapp } from 'nav-frontend-knapper';
 import * as React from 'react';
 import { Systemtittel } from 'nav-frontend-typografi';
+import { hentVedtaksbrev, IVedtaksBrev } from '../../../api/oppsummeringvedtak';
+import { Ressurs, RessursStatus } from '../../../typer/ressurs';
 
 interface IVedtakProps {
     fagsak: IFagsak;
@@ -13,20 +15,47 @@ interface IOppsummeringFrameProps {
 }
 
 const OppsummeringFrame: React.FunctionComponent<IOppsummeringFrameProps> = ({ ...props }) => {
-    const iframe = React.createRef<HTMLIFrameElement>();
+    const iframe = React.useRef();
+
     React.useEffect(() => {
         iframe.current.contentDocument.body.innerHTML = props.content;
     });
 
-    return <iframe ref={iframe} className="iframe" />;
+    return <iframe className="iframe" ref={iframe} />;
 };
 
 const OppsummeringVedtak: React.FunctionComponent<IVedtakProps> = ({ fagsak }) => {
+    const [brev, setBrev] = React.useState('Genererer forhåndsvisning...');
+
+    React.useEffect(() => {
+        hentVedtaksbrev(fagsak.id)
+            .then((response: Ressurs<IVedtaksBrev>) => {
+                if (response.status == RessursStatus.SUKSESS) {
+                    setBrev(response.data.content);
+                } else {
+                    setBrev(
+                        'kunne ikke generere forhåndsvisning'.concat(
+                            response.errorMelding != undefined
+                                ? ': ' + response.errorMelding
+                                : response.melding != undefined
+                                ? ': ' + response.melding
+                                : '.'
+                        )
+                    );
+                }
+            })
+            .catch(error => {
+                setBrev('kunne ikke generere forhåndsvisning. Internal Error: ' + error);
+            });
+    });
+
     return (
         <div className="oppsummering">
             <Systemtittel children={'Vedtaksbrev'} />
+
             <br />
-            <OppsummeringFrame content="<h1>hello</h1>" />
+            <OppsummeringFrame content={brev} />
+
             <br />
             <Knapp
                 onClick={() => {
