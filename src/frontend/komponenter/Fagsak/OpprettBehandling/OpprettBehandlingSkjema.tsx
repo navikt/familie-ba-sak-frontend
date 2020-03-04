9a14b139e50dfd7d8ee9e0cd56aa9a8dd69498f7
Meng Zhu <meng.zhu@nav.no>
2020-03-04 07:26:03 +0100

    allow revurder for debug

--- src/frontend/komponenter/Fagsak/OpprettBehandling/OpprettBehandlingSkjema.tsx
+++ src/frontend/komponenter/Fagsak/OpprettBehandling/OpprettBehandlingSkjema.tsx
@@ -54,10 +54,12 @@ const OpprettBehandlingSkjema: React.FunctionComponent<IOpprettBehandlingSkjema>
                 value={context.behandlingstype}
             >
                 {Object.keys(behandlingstyper)
-                    .filter(behandlingstype =>
-                        fagsak.behandlinger.length === 0
-                            ? behandlingstyper.REVURDERING.id !== behandlingstype
-                            : behandlingstyper.REVURDERING.id === behandlingstype
+                    .filter(
+                        behandlingstype =>
+                            true ||
+                            (fagsak.behandlinger.length === 0
+                                ? behandlingstyper.REVURDERING.id !== behandlingstype
+                                : behandlingstyper.REVURDERING.id === behandlingstype)
                     )
                     .map((key: string) => {
                         return (
