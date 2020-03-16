a453d7feaea1bd8ea4f39a4472c6c00cdc387a57
Henning Håkonsen <henninghaakonsen@me.com>
2020-03-16 11:54:23 +0100

    Endrer stegenum for vedtakssiden.

--- src/frontend/komponenter/Felleskomponenter/Venstremeny/sider.ts
+++ src/frontend/komponenter/Felleskomponenter/Venstremeny/sider.ts
@@ -14,7 +14,7 @@ export const sider: ISide[] = [
         steg: BehandlingSteg.VILKÅRSVURDERING,
     },
     { id: 'BEREGNING', navn: 'beregning', steg: BehandlingSteg.VILKÅRSVURDERING },
-    { id: 'VEDTAK', navn: 'vedtak', steg: BehandlingSteg.FORESLÅ_VEDTAK },
+    { id: 'VEDTAK', navn: 'vedtak', steg: BehandlingSteg.SEND_TIL_BESLUTTER },
 ];
 
 export const erSidenInaktiv = (side: ISide, steg?: BehandlingSteg) => {
