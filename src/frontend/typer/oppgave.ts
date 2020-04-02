78772981d255f5e7da3a73658b4682b529820edb
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-04-02 09:36:33 +0200

    API call, v1

---
+++ src/frontend/typer/oppgave.ts
@@ -0,0 +1,12 @@
+export interface IOppgave {
+    id: number;
+    tildeltEnhetsnr: string;
+    journalpostId: string;
+    saksreferanse: string;
+    aktoerid: string;
+    tilordnetRessurs: string;
+    behandlingstema: string;
+    oppgavetype: string;
+    prioritet: string;
+    status: string;
+}
