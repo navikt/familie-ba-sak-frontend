94859a52f6baf15e0af523363ff17c3436775511
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-22 12:41:09 +0200

    Flytt gruppemapping til utils

--- src/frontend/context/AppContext.tsx
+++ src/frontend/context/AppContext.tsx
@@ -6,6 +6,7 @@ import { håndterRessurs, loggFeil, preferredAxios } from '../api/axios';
 import { Ressurs } from '../typer/ressurs';
 import { ISaksbehandler } from '../typer/saksbehandler';
 import { BehandlerRolle } from '../typer/behandling';
+import { gruppeIdTilRolle } from '../utils/behandling';
 
 export interface IModal {
     content: string;
@@ -61,19 +62,6 @@ const [AppProvider, useApp] = createUseContext(({ innloggetSaksbehandler }: IPro
                 const responsRessurs: Ressurs<T> = error.response?.data;
                 return håndterRessurs(responsRessurs, innloggetSaksbehandler);
             });
-    };
-
-    const gruppeIdTilRolle = (gruppeId: string) => {
-        switch (gruppeId) {
-            case '199c2b39-e535-4ae8-ac59-8ccbee7991ae':
-                return BehandlerRolle.VEILEDER;
-            case '847e3d72-9dc1-41c3-80ff-f5d4acdd5d46':
-                return BehandlerRolle.SAKSBEHANDLER;
-            case '7a271f87-39fb-468b-a9ee-6cf3c070f548':
-                return BehandlerRolle.BESLUTTER;
-            default:
-                return BehandlerRolle.SYSTEM;
-        }
     };
 
     const hentSaksbehandlerRolle = (): BehandlerRolle | undefined => {
