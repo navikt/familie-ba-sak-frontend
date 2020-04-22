6e57d4cdeb80ba10ec28ad0ee4ba111b7521ae31
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-22 10:07:11 +0200

    Saksbehandlerrolle

--- src/frontend/utils/behandling.ts
+++ src/frontend/utils/behandling.ts
@@ -8,11 +8,16 @@ export const hentStegPåBehandlingOppe = (): BehandlingSteg | undefined => {
     return behandlingOppe.status === RessursStatus.SUKSESS ? behandlingOppe.data.steg : undefined;
 };
 
-export const erLesevisning = (): boolean | undefined => {
+export const hentSaksbehandlerRolle = (): string | undefined => {
+    // TODO: Enum for roller?
     const { innloggetSaksbehandler } = useApp();
-    const saksbehandlerRolle = innloggetSaksbehandler && innloggetSaksbehandler.firstName; // TODO: Hvis veileder-rolle
+    const saksbehandlerRolle = innloggetSaksbehandler && innloggetSaksbehandler.firstName; // TODO: Logikk som returnerer "høyeste" rolle?
+    return saksbehandlerRolle;
+};
+
+export const erLesevisning = (): boolean | undefined => {
     return (
-        saksbehandlerRolle === 'Nina' ||
+        hentSaksbehandlerRolle() === 'VEILEDERROLLE' || //TODO: Sjekk om veileder er høyeste blant grupper, evt om rolle er veileder
         hentStegPåBehandlingOppe() === BehandlingSteg.GODKJENNE_VEDTAK
     );
 };
