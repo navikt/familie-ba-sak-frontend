4a8df5121aed78ca75d9092cde53d5d335261f9a
Henning Håkonsen <henninghaakonsen@me.com>
2020-04-22 13:35:31 +0200

    Endrer til nais.io på redirects.

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -21,14 +21,14 @@ const konfigurerAzure = () => {
             process.env.GRAPH_API = 'http://familie-mock-server:1337/graph/me';
             break;
         case 'preprod':
-            process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.nais.preprod.local`;
-            process.env.AAD_REDIRECT_URL = `https://${host}.nais.preprod.local/auth/openid/callback`;
+            process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.dev-fss.nais.io`;
+            process.env.AAD_REDIRECT_URL = `https://${host}.dev-fss.nais.io/auth/openid/callback`;
             process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
             process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             break;
         case 'production':
-            process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.nais.adeo.no`;
-            process.env.AAD_REDIRECT_URL = `https://${host}.nais.adeo.no/auth/openid/callback`;
+            process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.prod-fss.nais.io`;
+            process.env.AAD_REDIRECT_URL = `https://${host}.prod-fss.nais.io/auth/openid/callback`;
             process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
             process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             break;
