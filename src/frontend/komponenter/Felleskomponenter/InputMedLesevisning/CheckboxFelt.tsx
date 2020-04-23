88f8c8fa872b47973ec90c01cc13ac8ebc155b0e
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-23 21:58:10 +0200

    ikon for å illustrere checkbokser

--- src/frontend/komponenter/Felleskomponenter/InputMedLesevisning/CheckboxFelt.tsx
+++ src/frontend/komponenter/Felleskomponenter/InputMedLesevisning/CheckboxFelt.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Checkbox, CheckboxProps } from 'nav-frontend-skjema';
-import Lesefelt from './Lesefelt';
+import { Normaltekst } from 'nav-frontend-typografi';
+import CheckTmp from '../../../ikoner/CheckTmp';
 
 interface IProps extends CheckboxProps {
     visLeseversjon: boolean;
@@ -10,7 +11,13 @@ class CheckboxFelt extends Component<IProps> {
     render() {
         const { visLeseversjon, label, checked, onChange } = this.props;
         return visLeseversjon ? (
-            checked && <Lesefelt verdi={label} /> // TODO: Legg til X og V og ta med de som ikke er avhuka? Det vil også løse håndtering av "rar visning" når ingen er huka av
+            checked && (
+                <div>
+                    <Normaltekst>
+                        <CheckTmp heigth={25} /> {label}
+                    </Normaltekst>
+                </div>
+            ) // TODO: Legg til X og V og ta med de som ikke er avhuka? Det vil også løse håndtering av "rar visning" når ingen er huka av
         ) : (
             <Checkbox label={label} checked={checked} onChange={onChange} />
         );
