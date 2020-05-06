f55ac6cb8c7554887f71255e9ac2d3c0d7ca7c6a
Meng Zhu <meng.zhu@nav.no>
2020-05-06 19:39:15 +0200

    refactor oppgavelist

--- src/frontend/komponenter/Oppgaver/OppgaveList.tsx
+++ src/frontend/komponenter/Oppgaver/OppgaveList.tsx
@@ -14,6 +14,7 @@ import Alertstripe from 'nav-frontend-alertstriper';
 import OppgavelisteSaksbehandler from './OppgavelisteSaksbehandler';
 import { ISaksbehandler } from '../../typer/saksbehandler';
 import OppgavelisteNavigator from './OppgavelisteNavigator';
+import 'nav-frontend-tabell-style';
 
 const intDatoTilNorskDato = (intDato: string) => {
     return `${intDato.substr(8, 2)}.${intDato.substr(5, 2)}.${intDato.substr(2, 2)}`;
@@ -28,35 +29,105 @@ interface IOppgaveListProps {
     innloggetSaksbehandler?: ISaksbehandler;
 }
 
+enum ListFelt {
+    OPPRETTET_TIDSPUNKT = 'OPPRETTET_TIDSPUNKT',
+    OPPGAVETYPE = 'OPPGAVETYPE',
+    BEHANDLINGSTEMA = 'BEHANDLINGSTEMA',
+    FRIST_FERDIGSTILLELSE = 'FRIST_FERDIGSTILLELSE',
+    PRIORITET = 'PRIORITET',
+    AKTOER_ID = 'AKTOER_ID',
+    TILDELT_ENHETSNR = 'TILDELT_ENHETSNR',
+    TILORDNET_RESSURS = 'TILORDNET_RESSURS',
+    HANDLINGER = 'HANDLINGER',
+    BESKRIVELSE = 'BESKRIVELSE',
+}
+
+enum FeltSortOrder {
+    NONE = 'none',
+    ASCENDANT = 'ascendant',
+    DESCENDANT = 'descendant',
+}
+
+const feltLabelMap = new Map<ListFelt, string>([
+    [ListFelt.OPPRETTET_TIDSPUNKT, 'Reg. dato'],
+    [ListFelt.OPPGAVETYPE, 'Oppgavetype'],
+    [ListFelt.BEHANDLINGSTEMA, 'Gjelder'],
+    [ListFelt.FRIST_FERDIGSTILLELSE, 'Frist'],
+    [ListFelt.PRIORITET, 'Prioritet'],
+    [ListFelt.AKTOER_ID, 'Bruker'],
+    [ListFelt.TILDELT_ENHETSNR, 'Enhet'],
+    [ListFelt.TILORDNET_RESSURS, 'Saksbehandler'],
+    [ListFelt.HANDLINGER, 'Handlinger'],
+    [ListFelt.BESKRIVELSE, 'Beskrivelse'],
+]);
+
+const oppgaveFeltMap = new Map<ListFelt, string>([
+    [ListFelt.OPPRETTET_TIDSPUNKT, 'opprettetTidspunkt'],
+    [ListFelt.OPPGAVETYPE, 'oppgavetype'],
+    [ListFelt.BEHANDLINGSTEMA, 'behandlingstema'],
+    [ListFelt.FRIST_FERDIGSTILLELSE, 'fristFerdigstillelse'],
+    [ListFelt.PRIORITET, 'prioritet'],
+    [ListFelt.AKTOER_ID, 'aktoerId'],
+    [ListFelt.TILDELT_ENHETSNR, 'tildeltEnhetsnr'],
+    [ListFelt.TILORDNET_RESSURS, 'tilordnetRessurs'],
+    [ListFelt.HANDLINGER, 'handlinger'],
+    [ListFelt.BESKRIVELSE, 'beskrivelse'],
+]);
+
+const ariaSortMap = new Map<FeltSortOrder, 'none' | 'descending' | 'ascending'>([
+    [FeltSortOrder.NONE, 'none'],
+    [FeltSortOrder.DESCENDANT, 'descending'],
+    [FeltSortOrder.ASCENDANT, 'ascending'],
+]);
+
+const sortLenkClassNameMap = new Map<
+    FeltSortOrder,
+    '' | 'tabell__th--sortert-desc' | 'tabell__th--sortert-asc'
+>([
+    [FeltSortOrder.NONE, ''],
+    [FeltSortOrder.DESCENDANT, 'tabell__th--sortert-desc'],
+    [FeltSortOrder.ASCENDANT, 'tabell__th--sortert-asc'],
+]);
+
+const initFeltOrder = new Map<ListFelt, FeltSortOrder>([
+    [ListFelt.OPPRETTET_TIDSPUNKT, FeltSortOrder.NONE],
+    [ListFelt.OPPGAVETYPE, FeltSortOrder.NONE],
+    [ListFelt.BEHANDLINGSTEMA, FeltSortOrder.NONE],
+    [ListFelt.FRIST_FERDIGSTILLELSE, FeltSortOrder.NONE],
+    [ListFelt.PRIORITET, FeltSortOrder.NONE],
+    [ListFelt.TILDELT_ENHETSNR, FeltSortOrder.NONE],
+    [ListFelt.TILORDNET_RESSURS, FeltSortOrder.NONE],
+]);
+
 const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaksbehandler }) => {
     const { oppgaver, sortOppgave, hentOppgaveSide } = useOppgaver();
 
-    interface IFeltOrder {
-        [key: string]: boolean;
-    }
+    const [feltSortOrder, settFeltSortOrder] = React.useState<Map<ListFelt, FeltSortOrder>>(
+        initFeltOrder
+    );
 
-    const initFeltOrder: IFeltOrder = {
-        opprettetTidspunkt: true,
-        oppgavetype: true,
-        behandlingstema: true,
-        fristFerdigstillelse: true,
-        prioritet: true,
-        tildeltEnhetsnr: true,
-        tilordnetRessurs: true,
+    const onColumnSort = (felt: ListFelt) => {
+        sortOppgave(oppgaveFeltMap.get(felt)!, feltSortOrder.get(felt) !== FeltSortOrder.ASCENDANT);
+        settFeltSortOrder(
+            new Map<ListFelt, FeltSortOrder>([
+                ...initFeltOrder,
+                [
+                    felt,
+                    feltSortOrder.get(felt) === FeltSortOrder.ASCENDANT
+                        ? FeltSortOrder.DESCENDANT
+                        : FeltSortOrder.ASCENDANT,
+                ],
+            ])
+        );
     };
 
-    const [feltOrder, settFeltOrder] = React.useState<IFeltOrder>(initFeltOrder);
+    const getAriaSort = (felt: ListFelt) => ariaSortMap.get(feltSortOrder.get(felt)!);
 
-    const onColumnSort = (felt: string) => {
-        sortOppgave(felt, feltOrder[felt]);
-        settFeltOrder({
-            ...feltOrder,
-            [felt]: !feltOrder[felt],
-        });
-    };
+    const getSortLenkClassName = (felt: ListFelt) =>
+        sortLenkClassNameMap.get(feltSortOrder.get(felt)!);
 
     React.useEffect(() => {
-        settFeltOrder(initFeltOrder);
+        settFeltSortOrder(initFeltOrder);
     }, [oppgaver.status]);
 
     return (
@@ -71,75 +142,35 @@ const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaks
                 <table className="tabell">
                     <thead className="tabell__head">
                         <tr className="tabell__head__tr">
-                            <th>
-                                <div className={'oppgavelist__tabell__regdato'}>
-                                    <Lenke
-                                        href="#"
-                                        onClick={() => onColumnSort('opprettetTidspunkt')}
+                            {Object.values(ListFelt).map(felt => {
+                                return feltSortOrder.get(felt) ? (
+                                    <th
+                                        role="columnheader"
+                                        aria-sort={getAriaSort(felt)}
+                                        className={getSortLenkClassName(felt)}
                                     >
-                                        Reg. dato
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__oppgavetype'}>
-                                    <Lenke href="#" onClick={() => onColumnSort('oppgavetype')}>
-                                        Oppgavetype
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__gjelder'}>
-                                    <Lenke href="#" onClick={() => onColumnSort('behandlingstema')}>
-                                        Gjelder
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__frist'}>
-                                    <Lenke
-                                        href="#"
-                                        onClick={() => onColumnSort('fristFerdigstillelse')}
-                                    >
-                                        Frist
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__prioritet'}>
-                                    <Lenke href="#" onClick={() => onColumnSort('prioritet')}>
-                                        Prioritet
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__bruker'}>Bruker</div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__enhet'}>
-                                    <Lenke href="#" onClick={() => onColumnSort('tildeltEnhetsnr')}>
-                                        Enhet
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__saksbehandler'}>
-                                    <Lenke
-                                        href="#"
-                                        onClick={() => onColumnSort('tilordnetRessurs')}
-                                    >
-                                        Saksbehandler
-                                    </Lenke>
-                                </div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__handlinger'}>Handlinger</div>
-                            </th>
-                            <th>
-                                <div className={'oppgavelist__tabell__beskrivelse'}>
-                                    Beskrivelse
-                                </div>
-                            </th>
+                                        <div
+                                            className={
+                                                'oppgavelist__tabell__' + oppgaveFeltMap.get(felt)
+                                            }
+                                        >
+                                            <Lenke href="#" onClick={() => onColumnSort(felt)}>
+                                                {feltLabelMap.get(felt)}
+                                            </Lenke>
+                                        </div>
+                                    </th>
+                                ) : (
+                                    <th>
+                                        <div
+                                            className={
+                                                'oppgavelist__tabell__' + oppgaveFeltMap.get(felt)
+                                            }
+                                        >
+                                            {feltLabelMap.get(felt)}
+                                        </div>
+                                    </th>
+                                );
+                            })}
                         </tr>
                     </thead>
                     {oppgaver.status === RessursStatus.SUKSESS && oppgaver.data.length > 0 && (
@@ -147,7 +178,7 @@ const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaks
                             {hentOppgaveSide().map((oppg: IOppgave, index) => (
                                 <tr key={index}>
                                     <td>
-                                        <div className={'oppgavelist__tabell__regdato'}>
+                                        <div className={'oppgavelist__tabell__opprettetTidspunkt'}>
                                             {intDatoTilNorskDato(oppg.opprettetTidspunkt)}
                                         </div>
                                     </td>
@@ -161,7 +192,7 @@ const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaks
                                         </div>
                                     </td>
                                     <td>
-                                        <div className={'oppgavelist__tabell__gjelder'}>
+                                        <div className={'oppgavelist__tabell__behandlingstema'}>
                                             {oppg.behandlingstema
                                                 ? GjelderFilter[
                                                       oppg.behandlingstema as keyof typeof GjelderFilter
@@ -170,7 +201,9 @@ const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaks
                                         </div>
                                     </td>
                                     <td>
-                                        <div className={'oppgavelist__tabell__frist'}>
+                                        <div
+                                            className={'oppgavelist__tabell__fristFerdigstillelse'}
+                                        >
                                             {intDatoTilNorskDato(oppg.fristFerdigstillelse)}
                                         </div>
                                     </td>
@@ -184,17 +217,17 @@ const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaks
                                         </div>
                                     </td>
                                     <td>
-                                        <div className={'oppgavelist__tabell__bruker'}>
+                                        <div className={'oppgavelist__tabell__aktoerId'}>
                                             {oppg.aktoerId}
                                         </div>
                                     </td>
                                     <td>
-                                        <div className={'oppgavelist__tabell__enhet'}>
+                                        <div className={'oppgavelist__tabell__tildeltEnhetsnr'}>
                                             {getEnheter(oppg.tildeltEnhetsnr)}
                                         </div>
                                     </td>
                                     <td>
-                                        <div className={'oppgavelist__tabell__saksbehandler'}>
+                                        <div className={'oppgavelist__tabell__tilordnetRessurs'}>
                                             <OppgavelisteSaksbehandler
                                                 oppgave={oppg}
                                                 innloggetSaksbehandler={innloggetSaksbehandler}
