39732e5e629c33d583e387cbc98db259904b1f21
Meng Zhu <meng.zhu@nav.no>
2020-05-07 11:31:25 +0200

    move to types.ts

--- src/frontend/komponenter/Oppgaver/OppgaveList.tsx
+++ src/frontend/komponenter/Oppgaver/OppgaveList.tsx
@@ -1,20 +1,31 @@
+import 'nav-frontend-tabell-style';
+
+import Alertstripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
+import { Systemtittel } from 'nav-frontend-typografi';
 import React from 'react';
+
+import { useOppgaver } from '../../context/OppgaverContext';
 import {
+    EnhetFilter,
     GjelderFilter,
+    IOppgave,
     OppgavetypeFilter,
     PrioritetFilter,
-    EnhetFilter,
-    IOppgave,
 } from '../../typer/oppgave';
 import { RessursStatus } from '../../typer/ressurs';
-import { useOppgaver } from '../../context/OppgaverContext';
-import { Systemtittel } from 'nav-frontend-typografi';
-import Lenke from 'nav-frontend-lenker';
-import Alertstripe from 'nav-frontend-alertstriper';
-import OppgavelisteSaksbehandler from './OppgavelisteSaksbehandler';
 import { ISaksbehandler } from '../../typer/saksbehandler';
 import OppgavelisteNavigator from './OppgavelisteNavigator';
-import 'nav-frontend-tabell-style';
+import OppgavelisteSaksbehandler from './OppgavelisteSaksbehandler';
+import {
+    ariaSortMap,
+    feltLabelMap,
+    FeltSortOrder,
+    initFeltOrder,
+    ListFelt,
+    oppgaveFeltMap,
+    sortLenkClassNameMap,
+} from './OppgaveListFelt';
 
 const intDatoTilNorskDato = (intDato: string) => {
     return `${intDato.substr(8, 2)}.${intDato.substr(5, 2)}.${intDato.substr(2, 2)}`;
@@ -28,76 +39,6 @@ const getEnheter = (enhetId: string) => {
 interface IOppgaveListProps {
     innloggetSaksbehandler?: ISaksbehandler;
 }
-
-enum ListFelt {
-    OPPRETTET_TIDSPUNKT = 'OPPRETTET_TIDSPUNKT',
-    OPPGAVETYPE = 'OPPGAVETYPE',
-    BEHANDLINGSTEMA = 'BEHANDLINGSTEMA',
-    FRIST_FERDIGSTILLELSE = 'FRIST_FERDIGSTILLELSE',
-    PRIORITET = 'PRIORITET',
-    AKTOER_ID = 'AKTOER_ID',
-    TILDELT_ENHETSNR = 'TILDELT_ENHETSNR',
-    TILORDNET_RESSURS = 'TILORDNET_RESSURS',
-    HANDLINGER = 'HANDLINGER',
-    BESKRIVELSE = 'BESKRIVELSE',
-}
-
-enum FeltSortOrder {
-    NONE = 'none',
-    ASCENDANT = 'ascendant',
-    DESCENDANT = 'descendant',
-}
-
-const feltLabelMap = new Map<ListFelt, string>([
-    [ListFelt.OPPRETTET_TIDSPUNKT, 'Reg. dato'],
-    [ListFelt.OPPGAVETYPE, 'Oppgavetype'],
-    [ListFelt.BEHANDLINGSTEMA, 'Gjelder'],
-    [ListFelt.FRIST_FERDIGSTILLELSE, 'Frist'],
-    [ListFelt.PRIORITET, 'Prioritet'],
-    [ListFelt.AKTOER_ID, 'Bruker'],
-    [ListFelt.TILDELT_ENHETSNR, 'Enhet'],
-    [ListFelt.TILORDNET_RESSURS, 'Saksbehandler'],
-    [ListFelt.HANDLINGER, 'Handlinger'],
-    [ListFelt.BESKRIVELSE, 'Beskrivelse'],
-]);
-
-const oppgaveFeltMap = new Map<ListFelt, string>([
-    [ListFelt.OPPRETTET_TIDSPUNKT, 'opprettetTidspunkt'],
-    [ListFelt.OPPGAVETYPE, 'oppgavetype'],
-    [ListFelt.BEHANDLINGSTEMA, 'behandlingstema'],
-    [ListFelt.FRIST_FERDIGSTILLELSE, 'fristFerdigstillelse'],
-    [ListFelt.PRIORITET, 'prioritet'],
-    [ListFelt.AKTOER_ID, 'aktoerId'],
-    [ListFelt.TILDELT_ENHETSNR, 'tildeltEnhetsnr'],
-    [ListFelt.TILORDNET_RESSURS, 'tilordnetRessurs'],
-    [ListFelt.HANDLINGER, 'handlinger'],
-    [ListFelt.BESKRIVELSE, 'beskrivelse'],
-]);
-
-const ariaSortMap = new Map<FeltSortOrder, 'none' | 'descending' | 'ascending'>([
-    [FeltSortOrder.NONE, 'none'],
-    [FeltSortOrder.DESCENDANT, 'descending'],
-    [FeltSortOrder.ASCENDANT, 'ascending'],
-]);
-
-const sortLenkClassNameMap = new Map<
-    FeltSortOrder,
-    '' | 'tabell__th--sortert-desc' | 'tabell__th--sortert-asc'
->([
-    [FeltSortOrder.NONE, ''],
-    [FeltSortOrder.DESCENDANT, 'tabell__th--sortert-desc'],
-    [FeltSortOrder.ASCENDANT, 'tabell__th--sortert-asc'],
-]);
-
-const initFeltOrder = new Map<ListFelt, FeltSortOrder>([
-    [ListFelt.OPPRETTET_TIDSPUNKT, FeltSortOrder.NONE],
-    [ListFelt.OPPGAVETYPE, FeltSortOrder.NONE],
-    [ListFelt.BEHANDLINGSTEMA, FeltSortOrder.NONE],
-    [ListFelt.FRIST_FERDIGSTILLELSE, FeltSortOrder.NONE],
-    [ListFelt.PRIORITET, FeltSortOrder.NONE],
-    [ListFelt.TILDELT_ENHETSNR, FeltSortOrder.NONE],
-    [ListFelt.TILORDNET_RESSURS, FeltSortOrder.NONE],
-]);
 
 const OppgaveList: React.FunctionComponent<IOppgaveListProps> = ({ innloggetSaksbehandler }) => {
     const { oppgaver, sortOppgave, hentOppgaveSide } = useOppgaver();
