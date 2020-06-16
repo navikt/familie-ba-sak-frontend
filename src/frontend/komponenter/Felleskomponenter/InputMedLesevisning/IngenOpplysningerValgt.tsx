ca5a51cd6d7767a1c5c602c1204ce325884ecee7
hanngu <hanne@informatikk.org>
2020-06-16 14:37:58 +0200

    Fjern FamilieTextareaControlled og legg til felleskomponent. Fiks IngenOpplysningerValgt til Ã¨å matche felleskomponenter

--- src/frontend/komponenter/Felleskomponenter/InputMedLesevisning/IngenOpplysningerValgt.tsx
+++ src/frontend/komponenter/Felleskomponenter/InputMedLesevisning/IngenOpplysningerValgt.tsx
@@ -1,15 +1,17 @@
 import { Normaltekst } from 'nav-frontend-typografi';
 import React from 'react';
-import { useBehandling } from '../../../context/BehandlingContext';
 
-interface IProps {
+interface IIngenOpplysningerValgtProps {
     minimumOpplysning: boolean[];
+    erLesevisning: boolean;
 }
 
-const IngenOpplysningerValgt: React.FC<IProps> = ({ minimumOpplysning }) => {
-    const harOpplysningerÅVise = minimumOpplysning.filter(Boolean);
-    const { erLesevisning } = useBehandling();
-    return erLesevisning() && harOpplysningerÅVise.length === 0 ? (
+const IngenOpplysningerValgt: React.FC<IIngenOpplysningerValgtProps> = ({
+    minimumOpplysning,
+    erLesevisning,
+}) => {
+    const harOpplysningerÅVise = minimumOpplysning.filter(Boolean).length === 0;
+    return erLesevisning && harOpplysningerÅVise ? (
         <Normaltekst className={'skjemaelement'} children={'Ingen opplysninger valgt.'} />
     ) : null;
 };
