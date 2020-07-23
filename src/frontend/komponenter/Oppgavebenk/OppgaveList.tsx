641c87d0dbe791b4a48c2af922463e0288b24fe2
Henning Håkonsen <henning.hakonsen@nav.no>
2020-07-23 13:02:41 +0200

    Feature/forbedre oppgaveliste (#201)
    * Diverse forbedringer og forenklinger av kode og stiler.
    
    * Skrevet om måten filterverdier og labels blir brukt. Henter automatisk oppgaver dersom det finnes en query-string som passer til filterne.
    
    * Fikser liten ALLE bug.
    
    * Bruker riktig verdi på saksbehandler ved henting av oppgaver.
    
    * Endrer på verdier mot api for alle/alle ufordelte på saksbehandler feltet.
    
    * Fjerner E fra enhetsverdien ved henting av oppgaver.
    
    * Flere forbedringer på request mot backend for henting av oppgaver.
    
    * Mindre endringer for å prøve å se om det hjelper mot oppgave apiet.
    
    * Fikser bug med antall oppgaver på request.
    
    * Setter saksbehandler til undefined om alle er valgt. Her er det nok en bug på filtrering av ufordelte/fordelte. Fikser den i egen PR isåfall.
    
    * Fikser mapping fra api til output i listen over oppgaver.
    
    * Fjerner ubrukt import.
    
    * Fikser bug ved undefined på enhetsnr.
    
    * Bruker fordelt ressurs og tildelt ressurs riktig.
    
    * Setter riktige verdier på tildelt ressurs felt.
    
    * Sikrer undefined kall på enhet.
    
    * Sett alle tilordnetRessurs som default filter.
    
    * Tilbakestiller sort order ved henting

---
+++ src/frontend/komponenter/Oppgavebenk/OppgaveList.tsx
@@ -0,0 +1,178 @@
+import Alertstripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
+import 'nav-frontend-tabell-style';
+import { Systemtittel } from 'nav-frontend-typografi';
+import React from 'react';
+import { useApp } from '../../context/AppContext';
+import { useOppgaver } from '../../context/OppgaverContext';
+import {
+    IOppgave,
+    OppgavetypeFilter,
+    PrioritetFilter,
+    oppgaveTypeFilter,
+    gjelderFilter,
+    enhetFilter,
+} from '../../typer/oppgave';
+import { RessursStatus } from '@navikt/familie-typer';
+import OppgavelisteNavigator from './OppgavelisteNavigator';
+import OppgavelisteSaksbehandler from './OppgavelisteSaksbehandler';
+import { ariaSortMap, FeltSortOrder, sortLenkClassNameMap, IOppgaveFelt } from './oppgavefelter';
+import classNames from 'classnames';
+
+const intDatoTilNorskDato = (intDato: string) => {
+    return `${intDato.substr(8, 2)}.${intDato.substr(5, 2)}.${intDato.substr(2, 2)}`;
+};
+
+const OppgaveList: React.FunctionComponent = () => {
+    const { oppgaver, sortOppgave, oppgaveFelter, hentOppgaveSide } = useOppgaver();
+    const { innloggetSaksbehandler } = useApp();
+
+    const onColumnSort = (felt: IOppgaveFelt) => {
+        sortOppgave(felt.nøkkel, felt.order !== FeltSortOrder.ASCENDANT);
+    };
+
+    const getAriaSort = (felt: IOppgaveFelt) => ariaSortMap.get(felt.order || FeltSortOrder.NONE);
+
+    const getSortLenkClassName = (felt: IOppgaveFelt) =>
+        sortLenkClassNameMap.get(felt.order || FeltSortOrder.NONE);
+
+    const sortertClassName = (felt: IOppgaveFelt) =>
+        felt.order !== FeltSortOrder.NONE ? 'tabell__td--sortert' : '';
+
+    return (
+        <div className={'oppgavelist'}>
+            <div className={'oppgavelist__header'}>
+                <Systemtittel>Oppgaveliste</Systemtittel>
+                <OppgavelisteNavigator />
+            </div>
+            <div>
+                <table className="tabell">
+                    <thead>
+                        <tr>
+                            {Object.values(oppgaveFelter).map((felt: IOppgaveFelt) => {
+                                return felt.order ? (
+                                    <th
+                                        role="columnheader"
+                                        aria-sort={getAriaSort(felt)}
+                                        className={classNames(
+                                            getSortLenkClassName(felt),
+                                            felt.nøkkel
+                                        )}
+                                        key={felt.nøkkel}
+                                    >
+                                        <Lenke href="#" onClick={() => onColumnSort(felt)}>
+                                            {felt.label}
+                                        </Lenke>
+                                    </th>
+                                ) : (
+                                    <th
+                                        role="columnheader"
+                                        key={felt.nøkkel}
+                                        className={felt.nøkkel}
+                                    >
+                                        {felt.label}
+                                    </th>
+                                );
+                            })}
+                        </tr>
+                    </thead>
+                    {oppgaver.status === RessursStatus.SUKSESS &&
+                        oppgaver.data.oppgaver.length > 0 && (
+                            <tbody>
+                                {hentOppgaveSide().map((oppg: IOppgave, index) => (
+                                    <tr key={index}>
+                                        <td
+                                            className={sortertClassName(
+                                                oppgaveFelter.opprettetTidspunkt
+                                            )}
+                                        >
+                                            {intDatoTilNorskDato(oppg.opprettetTidspunkt)}
+                                        </td>
+                                        <td
+                                            className={classNames(
+                                                'oppgavetype',
+                                                sortertClassName(oppgaveFelter.oppgavetype)
+                                            )}
+                                        >
+                                            {oppgaveTypeFilter[oppg.oppgavetype].navn}
+                                        </td>
+                                        <td
+                                            className={sortertClassName(
+                                                oppgaveFelter.behandlingstema
+                                            )}
+                                        >
+                                            {oppg.behandlingstema
+                                                ? gjelderFilter[oppg.behandlingstema].navn
+                                                : 'Ikke satt'}
+                                        </td>
+                                        <td
+                                            className={sortertClassName(
+                                                oppgaveFelter.fristFerdigstillelse
+                                            )}
+                                        >
+                                            {intDatoTilNorskDato(oppg.fristFerdigstillelse)}
+                                        </td>
+                                        <td className={sortertClassName(oppgaveFelter.prioritet)}>
+                                            {
+                                                PrioritetFilter[
+                                                    oppg.prioritet as keyof typeof PrioritetFilter
+                                                ]
+                                            }
+                                        </td>
+                                        <td className={'beskrivelse'}>{oppg.beskrivelse}</td>
+                                        <td>{oppg.aktoerId}</td>
+                                        <td
+                                            className={classNames(
+                                                'tildelt-enhetsnr',
+                                                sortertClassName(oppgaveFelter.tildeltEnhetsnr)
+                                            )}
+                                        >
+                                            {oppg.tildeltEnhetsnr
+                                                ? enhetFilter[`E${oppg.tildeltEnhetsnr}`]?.navn
+                                                : 'Ikke satt'}
+                                        </td>
+                                        <td
+                                            className={classNames(
+                                                'tilordnet-ressurs',
+                                                sortertClassName(oppgaveFelter.tilordnetRessurs)
+                                            )}
+                                        >
+                                            <OppgavelisteSaksbehandler
+                                                oppgave={oppg}
+                                                innloggetSaksbehandler={innloggetSaksbehandler}
+                                            />
+                                        </td>
+                                        <td className={'handlinger'}>
+                                            {oppgaveTypeFilter[oppg.oppgavetype].id ===
+                                                OppgavetypeFilter.JFR && (
+                                                <a href={`/oppgaver/journalfør/${oppg.id}`}>
+                                                    Gå til oppg
+                                                </a>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        )}
+                </table>
+            </div>
+            {oppgaver.status === RessursStatus.SUKSESS && oppgaver.data.oppgaver.length === 0 && (
+                <Alertstripe type="advarsel" className="oppgavelist__info">
+                    Ingen oppgaver
+                </Alertstripe>
+            )}
+            {oppgaver.status === RessursStatus.FEILET && (
+                <Alertstripe type="feil" className="oppgavelist__info">
+                    {oppgaver.frontendFeilmelding}
+                </Alertstripe>
+            )}
+            {oppgaver.status === RessursStatus.HENTER && (
+                <Alertstripe type="info" className="oppgavelist__info">
+                    Henter...
+                </Alertstripe>
+            )}
+        </div>
+    );
+};
+
+export default OppgaveList;
