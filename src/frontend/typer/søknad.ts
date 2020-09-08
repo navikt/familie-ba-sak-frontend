3e2450878e981aba34a3088c68b987863830aac3
Daphne Leebeek <daphne.leebeek@nav.no>
2020-09-08 10:09:03 +0200

    Målformvelger, og gå mot nytt api i backend.

--- src/frontend/typer/søknad.ts
+++ src/frontend/typer/søknad.ts
@@ -1,4 +1,5 @@
 import { BehandlingUnderkategori } from './behandling';
+import { INøkkelPar } from './common';
 
 export interface IRestRegistrerSøknad {
     søknad: ISøknadDTO;
@@ -9,19 +10,34 @@ export interface ISøknadDTO {
     underkategori: BehandlingUnderkategori;
     søkerMedOpplysninger: ISøkerMedOpplysninger;
     barnaMedOpplysninger: IBarnMedOpplysninger[];
+    endringAvOpplysningerBegrunnelse: string;
 }
 
 export interface ISøkerMedOpplysninger {
     ident: string;
+    målform: Målform | undefined;
 }
 
 export interface IBarnMedOpplysninger {
     inkludertISøknaden: boolean;
     ident: string;
-    oppholderSegINorge: boolean;
-    borMedSøker: boolean;
-    harOppholdtSegINorgeSiste12Måneder: boolean;
-    tilleggsopplysninger?: string;
     navn?: string;
     fødselsdato?: string;
+    manueltRegistrert: boolean;
 }
+
+export enum Målform {
+    NB = 'NB',
+    NN = 'NN',
+}
+
+export const målform: INøkkelPar = {
+    NB: {
+        id: 'NB',
+        navn: 'Bokmål',
+    },
+    NN: {
+        id: 'NN',
+        navn: 'Nynorsk',
+    },
+};
