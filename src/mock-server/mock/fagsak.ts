a4a5b389f51c79afbffae102eeb5819a57cc03f8
Henning Håkonsen <henninghaakonsen@me.com>
2020-09-08 14:36:51 +0200

    Fikser byggfeil.

--- src/mock-server/mock/fagsak.ts
+++ src/mock-server/mock/fagsak.ts
@@ -38,6 +38,7 @@ export const mockFagsak3 = (id: number, søkerFødselsnummer: string): Ressurs<I
         opprettetTidspunkt: '2020-03-19T09:08:56.8',
         saksnummer: '1234',
         status: FagsakStatus.LØPENDE,
+        underBehandling: false,
         behandlinger: [mockBehandling(1, true, 'VILKÅRSVURDERING')],
     };
 
