4d0fabf44107af1ab57fcff8d4db7e74f689baee
Henning Håkonsen <henning.hakonsen@nav.no>
2020-10-30 08:15:34 +0100

    Legger til Fritekst på revurdering select.

--- src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/BrevModul/typer.ts
+++ src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/BrevModul/typer.ts
@@ -329,7 +329,7 @@ export const dokumenter: ISelectOptionMedBrevtekst[] = [
 export const årsaker: ISelectOptionMedBrevtekst[] = [
     {
         value: 'annet',
-        label: 'Annet',
+        label: 'Annet (Fritekst)',
         brevtekst: {
             NB: '',
             NN: '',
