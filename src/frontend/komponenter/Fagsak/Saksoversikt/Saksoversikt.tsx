9489d76ac585e5f1a64d49d95112a7c269525cbf
Daphne <daphne.leebeek@nav.no>
2020-11-02 13:49:14 +0100

    Legg til melding for neste mnd utbetaling (#359)

--- src/frontend/komponenter/Fagsak/Saksoversikt/Saksoversikt.tsx
+++ src/frontend/komponenter/Fagsak/Saksoversikt/Saksoversikt.tsx
@@ -19,10 +19,18 @@ import Opphør from './Opphør';
 import { periodeOverlapperMedValgtDato } from '../../../utils/tid';
 import { useApp } from '../../../context/AppContext';
 import { ToggleNavn } from '../../../typer/toggles';
+import { datoformat, formaterIsoDato } from '../../../utils/formatter';
+import styled from 'styled-components';
+import Lenke from 'nav-frontend-lenker';
 
 interface IProps {
     fagsak: IFagsak;
 }
+
+const FlexSpaceBetween = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
 
 const Saksoversikt: React.FunctionComponent<IProps> = ({ fagsak }) => {
     const { toggles } = useApp();
@@ -52,6 +60,65 @@ const Saksoversikt: React.FunctionComponent<IProps> = ({ fagsak }) => {
     const beregningOversiktInneværendeMåned = beregningOversikt.find(periode =>
         periodeOverlapperMedValgtDato(periode.periodeFom, periode.periodeTom, new Date())
     );
+    const nesteMåned = moment(new Date()).add(1, 'month').startOf('month').toDate();
+    const beregningOversiktNesteMåned = beregningOversikt.find(periode =>
+        periodeOverlapperMedValgtDato(periode.periodeFom, periode.periodeTom, nesteMåned)
+    );
+
+    const lenkeTilBehandlingsresultat = () => {
+        return aktivBehandling ? (
+            <Lenke href={`/fagsak/${fagsak.id}/${aktivBehandling.behandlingId}/tilkjent-ytelse`}>
+                Se behandlingsresultat for detaljer
+            </Lenke>
+        ) : null;
+    };
+
+    const løpendeMånedligUtbetaling = () => {
+        if (beregningOversiktInneværendeMåned) {
+            return beregningOversiktInneværendeMåned.utbetaltPerMnd < 1 &&
+                gjeldendeBehandling?.kategori === BehandlingKategori.EØS ? (
+                <AlertStripe className={'saksoversikt__alert'} type={'info'}>
+                    Siste gjeldende vedtak er en EØS-sak uten månedlige utbetalinger fra NAV
+                </AlertStripe>
+            ) : (
+                <>
+                    {beregningOversiktNesteMåned &&
+                        beregningOversiktNesteMåned !== beregningOversiktInneværendeMåned && (
+                            <AlertStripe className={'saksoversikt__alert'} type={'info'}>
+                                <FlexSpaceBetween>
+                                    {`Utbetalingen endres fra og med ${formaterIsoDato(
+                                        nesteMåned.toDateString(),
+                                        datoformat.MÅNED_NAVN
+                                    )}`}
+                                    {lenkeTilBehandlingsresultat()}
+                                </FlexSpaceBetween>
+                            </AlertStripe>
+                        )}
+                    <Utbetalinger beregningOversikt={beregningOversiktInneværendeMåned} />
+                    {toggles[ToggleNavn.visTekniskOpphør] && <Opphør fagsak={fagsak} />}
+                </>
+            );
+        } else if (beregningOversiktNesteMåned) {
+            return (
+                <AlertStripe className={'saksoversikt__alert'} type={'info'}>
+                    <FlexSpaceBetween>
+                        {`Utbetalingen starter ${formaterIsoDato(
+                            nesteMåned.toDateString(),
+                            datoformat.MÅNED_NAVN
+                        )}`}
+                        {lenkeTilBehandlingsresultat()}
+                    </FlexSpaceBetween>
+                </AlertStripe>
+            );
+        } else {
+            return (
+                <AlertStripe className={'saksoversikt__alert'} type={'feil'}>
+                    Noe gikk galt ved henting av utbetalinger. Prøv igjen eller kontakt brukerstøtte
+                    hvis problemet vedvarer.
+                </AlertStripe>
+            );
+        }
+    };
 
     return (
         <div className={'saksoversikt'}>
@@ -60,34 +127,16 @@ const Saksoversikt: React.FunctionComponent<IProps> = ({ fagsak }) => {
             <FagsakLenkepanel fagsak={fagsak} />
 
             {fagsak.status === FagsakStatus.LØPENDE && (
-                <Systemtittel>Løpende månedlig utbetaling</Systemtittel>
+                <>
+                    <Systemtittel>Løpende månedlig utbetaling</Systemtittel>
+                    {løpendeMånedligUtbetaling()}
+                </>
             )}
-
-            {fagsak.status === FagsakStatus.LØPENDE &&
-                (beregningOversiktInneværendeMåned ? (
-                    beregningOversiktInneværendeMåned?.utbetaltPerMnd < 1 &&
-                    gjeldendeBehandling?.kategori === BehandlingKategori.EØS ? (
-                        <AlertStripe className={'saksoversikt__alert'} type={'info'}>
-                            Siste gjeldende vedtak er en EØS-sak uten månedlige utbetalinger fra NAV
-                        </AlertStripe>
-                    ) : (
-                        <>
-                            <Utbetalinger beregningOversikt={beregningOversiktInneværendeMåned} />
-                            {toggles[ToggleNavn.visTekniskOpphør] && <Opphør fagsak={fagsak} />}
-                        </>
-                    )
-                ) : (
-                    <AlertStripe className={'saksoversikt__alert'} type={'feil'}>
-                        Noe gikk galt ved henting av utbetalinger. Prøv igjen eller kontakt
-                        brukerstøtte hvis problemet vedvarer.
-                    </AlertStripe>
-                ))}
 
             <Behandlinger fagsak={fagsak} />
         </div>
     );
 };
-
 export const sakstype = (behandling?: IBehandling) => {
     if (!behandling) {
         return 'Ikke satt';
