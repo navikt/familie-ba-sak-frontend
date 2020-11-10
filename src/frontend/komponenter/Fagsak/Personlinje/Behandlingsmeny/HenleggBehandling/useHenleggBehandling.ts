5340f0ffc3efb7961d9672f81c981f3c7b183127
MarcusGustafson74 <marcus.gustafson@nav.no>
2020-11-10 09:37:05 +0100

    Henlegg behandling (#344)
    * Henlegg behandling WIP
    
    * La til integrasjon mot ba-sak
    
    * Lag veivalgsmodal (etter henleggelse)
    Flytt henleggBehandling fra fagsakapi til useHenleggBehandling
    
    * feilhåndtering på skjemagruppe
    
    * bruk skjema abstraksjon
    
    * endre test-url til ordentlig url
    
    * La til HENLAGT i enum behandlingsResultater
    
    * Endringer etter demo
    
    * Endringer etter demo: Det skal ikke være mulig å henlegge etter at oppgave blitt sendt til beslutter
    
    * Oppdaterte venstremenyen til å bare vise steg behandlingen vært innom ved henleggelse
    
    * endre mock etter interfaceendring
    
    * fjern overflødig kode + bug
    
    * underlinker skal også gråes ut..
    
    * ta i bruk styled-components
    
    * useOpprettBehandling -> useHenleggBehandling
    
    * BehandlingResultat-enum skal ha tilhørende visningsmapping definert som en Record.
    
    * Henlegg behandling kun klikkbar dersom behandler har skrivetilgang
    
    * HenleggelseÅrsak som type i IFelt
    
    * Oppdateringer etter tilbakemelding på Mini demo
    
    * Legg til skjult legend for skjermleser
    
    * La til mulighet å toggle henleggelse funksjonaliteten
    
    Co-authored-by: Geirmund Simonsen <geirmund.simonsen@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@gmail.com>

---
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/HenleggBehandling/useHenleggBehandling.ts
@@ -0,0 +1,59 @@
+import { byggTomRessurs, Ressurs } from '@navikt/familie-typer';
+import { useState } from 'react';
+import { useFagsakRessurser } from '../../../../../context/FagsakContext';
+import { HenleggelseÅrsak } from '../../../../../typer/behandling';
+import { IFagsak } from '../../../../../typer/fagsak';
+import { feil, IFelt, nyttFelt, ok } from '../../../../../typer/felt';
+import { useSkjema } from '../../../../../typer/skjema';
+
+const useHenleggBehandling = (lukkModal: () => void) => {
+    const [visVeivalgModal, settVisVeivalgModal] = useState(false);
+    const [begrunnelse, settBegrunnelse] = useState('');
+
+    const { settFagsak } = useFagsakRessurser();
+    const { hentFeltProps, settInitialState, onSubmit, oppdaterFeltISkjema, skjema } = useSkjema<
+        IFagsak
+    >({
+        felter: {
+            årsak: nyttFelt<HenleggelseÅrsak | ''>('', (felt: IFelt<HenleggelseÅrsak | ''>) =>
+                felt.verdi !== '' ? ok(felt) : feil(felt, 'Du må velge årsak')
+            ),
+            begrunnelse: nyttFelt(''),
+        },
+        skjemanavn: 'henleggbehandling',
+        submitRessurs: byggTomRessurs(),
+        visFeilmeldinger: false,
+    });
+
+    const onBekreft = (behandlingId: number) => {
+        onSubmit(
+            {
+                method: 'PUT',
+                data: {
+                    årsak: skjema.felter.årsak.verdi,
+                    begrunnelse: skjema.felter.begrunnelse.verdi,
+                },
+                url: `/familie-ba-sak/api/behandlinger/${behandlingId}/henlegg`,
+            },
+            (ressurs: Ressurs<IFagsak>) => {
+                settFagsak(ressurs);
+                lukkModal();
+                settVisVeivalgModal(true);
+            }
+        );
+    };
+
+    return {
+        begrunnelse,
+        hentFeltProps,
+        onBekreft,
+        oppdaterFeltISkjema,
+        settInitialState,
+        settBegrunnelse,
+        settVisVeivalgModal,
+        skjema,
+        visVeivalgModal,
+    };
+};
+
+export default useHenleggBehandling;
