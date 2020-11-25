1466ab4f1d271ae7a52007676a9c373499fb91fb
Daphne Leebeek <daphne.leebeek@nav.no>
2020-11-25 14:23:20 +0100

    Test for formaterIsoDato

--- src/frontend/utils/familieDayjs.ts
+++ src/frontend/utils/familieDayjs.ts
@@ -1,9 +1,14 @@
-import dayjs from 'dayjs';
+import dayjs, { ConfigType } from 'dayjs';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isBetween from 'dayjs/plugin/isBetween';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import LocalizedFormat from 'dayjs/plugin/LocalizedFormat';
+
+export type Dayjs = dayjs.Dayjs;
+
+const norskTidssone = 'Europe/Oslo';
 
 require('dayjs/locale/nb');
 dayjs.extend(isSameOrBefore);
@@ -11,10 +16,16 @@ dayjs.extend(isSameOrAfter);
 dayjs.extend(isBetween);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(LocalizedFormat);
 
-dayjs.tz.setDefault('Europe/Oslo');
+dayjs.tz.setDefault(norskTidssone);
 dayjs.locale('nb');
 
-const familieDayjs = dayjs.tz;
+const familieDayjs = (config?: ConfigType, format?: string): Dayjs => {
+    if (config && format) {
+        return dayjs.tz(config, format, norskTidssone);
+    }
+    return config ? dayjs.tz(config) : dayjs.tz(dayjs());
+};
 
 export default familieDayjs;
