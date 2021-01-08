c6658b9ffcd20cf931d7d67a7468053f2bb21c17
Meng Zhu <meng.zhu@nav.no>
2021-01-08 14:06:45 +0100

    proxy

--- src/backend/proxy.ts
+++ src/backend/proxy.ts
@@ -33,6 +33,55 @@ export const doProxy: any = () => {
     });
 };
 
+// eslint-disable-next-line
+export const doHentDokumentProxy: any = () => {
+    return createProxyMiddleware('/proxy/api/hentDokument', {
+        changeOrigin: true,
+        logLevel: 'info',
+        onProxyReq: restream,
+        pathRewrite: (path: string, _req: Request) => {
+            const newPath = path.replace('/proxy/api/hentDokument', '/journalpost');
+            return `/api${newPath}`;
+        },
+        secure: true,
+        target: `${proxyUrl}`,
+        logProvider: () => stdoutLogger,
+        onProxyRes: (proxyRes, _, res) => {
+            let dokumentData = Buffer.from('Ukjent feil ved vis dokument');
+            let visfrontendFeilmelding = true;
+            const _end = res.end;
+            res.write = () => true;
+            proxyRes.on('data', data => {
+                try {
+                    JSON.parse(Buffer.from(data).toString(), (k, v) => {
+                        if ((k === 'data' || k === 'frontendFeilmelding') && v) {
+                            dokumentData = Buffer.from(v, 'utf-8');
+                        }
+                        if (k === 'data' && v) {
+                            visfrontendFeilmelding = false;
+                        }
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+            res.end = () => {
+                console.log(visfrontendFeilmelding, dokumentData);
+                res.setHeader('content-length', Buffer.byteLength(dokumentData));
+                if (visfrontendFeilmelding) {
+                    res.setHeader('content-encoding', 'utf-8');
+                    res.setHeader('Content-Type', 'text/plain');
+                    _end.call(res, dokumentData.toString('utf-8'), 'utf-8');
+                } else {
+                    res.setHeader('content-encoding', 'base64');
+                    res.setHeader('Content-Type', 'application/pdf');
+                    _end.call(res, dokumentData.toString('utf-8'), 'base64');
+                }
+            };
+        },
+    });
+};
+
 export const attachToken = (authClient: Client) => {
     return async (req: Request, _res: Response, next: NextFunction) => {
         getOnBehalfOfAccessToken(authClient, req, oboConfig).then((accessToken: string) => {
