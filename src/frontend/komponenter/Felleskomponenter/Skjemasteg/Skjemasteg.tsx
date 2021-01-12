65cc53904cf989886f0b85107064f286610b31ce
Meng Zhu <meng.zhu@nav.no>
2021-01-13 00:43:51 +0100

    reverted some changes

--- src/frontend/komponenter/Felleskomponenter/Skjemasteg/Skjemasteg.tsx
+++ src/frontend/komponenter/Felleskomponenter/Skjemasteg/Skjemasteg.tsx
@@ -113,6 +113,7 @@ const Skjemasteg: React.FunctionComponent<IProps> = ({
                     )}
                     {tilbakestillOnClick ? (
                         <TilbakestillKnapp
+                            type={'hoved'}
                             disabled={!skalAktivereTilbakestillKnapp}
                             onClick={() => {
                                 tilbakestillOnClick();
