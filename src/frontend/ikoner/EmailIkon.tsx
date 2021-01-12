0e64982544b6387ca123f91c5aa0f3e1a5c20eaf
Meng Zhu <meng.zhu@nav.no>
2021-01-12 08:45:06 +0100

    update icon

--- src/frontend/ikoner/EmailIkon.tsx
+++ src/frontend/ikoner/EmailIkon.tsx
@@ -1,13 +1,39 @@
 import * as React from 'react';
 
 interface IEmailIkon {
+    filled?: boolean;
     className?: string;
     height?: number;
     width?: number;
 }
 
-export const EmailIkon: React.FC<IEmailIkon> = ({ className, width = 48, height = 48 }) => {
-    return (
+export const EmailIkon: React.FC<IEmailIkon> = ({
+    className,
+    filled = false,
+    width = 48,
+    height = 48,
+}) => {
+    return filled ? (
+        <svg
+            version="1.1"
+            id="Filled_Version"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width={width}
+            height={height}
+            viewBox="0 0 24 24"
+            enable-background="new 0 0 24 24"
+        >
+            <g fill="#0067c5" stroke="none">
+                <path
+                    d="M22.709,4.976l-10.394,8.413C12.224,13.463,12.111,13.5,12,13.5s-0.223-0.037-0.314-0.111L1.293,4.976
+		C1.111,5.276,1,5.624,1,6v11c0,1.103,0.898,2,2,2h18c1.104,0,2-0.897,2-2V6C23,5.624,22.89,5.276,22.709,4.976z"
+                />
+                <path d="M21.988,4.271C21.695,4.104,21.361,4,21,4H3C2.64,4,2.306,4.104,2.013,4.271L12,12.357L21.988,4.271z" />
+            </g>
+        </svg>
+    ) : (
         <svg
             aria-labelledby={'email'}
             xmlns="http://www.w3.org/2000/svg"
