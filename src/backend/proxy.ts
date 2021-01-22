daaed4d4ccc71dd32999f376960b7d6e5590fe96
Meng Zhu <meng.zhu@nav.no>
2021-01-22 08:53:58 +0100

    Feature/vis dokument i tab (#664)
    * show document in tab
    
    * proxy
    
    * proxy
    
    * remove log
    
    * make proxy general
    
    * uu
    
    * debug
    
    * debug
    
    * fix multi-chunk bug

--- src/backend/proxy.ts
+++ src/backend/proxy.ts
@@ -33,6 +33,62 @@ export const doProxy: any = () => {
     });
 };
 
+const pdfProxyUrlRecord: Record<string, string> = {
+    '/api/pdf/journalpost': '/api/journalpost',
+};
+
+// eslint-disable-next-line
+export const doPdfProxy: any = () => {
+    return createProxyMiddleware('/api/pdf', {
+        changeOrigin: true,
+        logLevel: 'info',
+        onProxyReq: restream,
+        pathRewrite: (path: string, _req: Request) => {
+            const urlKey = Object.keys(pdfProxyUrlRecord).find(k => path.includes(k));
+            const newPath = urlKey ? path.replace(urlKey, pdfProxyUrlRecord[urlKey]) : path;
+            return `${newPath}`;
+        },
+        secure: true,
+        target: `${proxyUrl}`,
+        logProvider: () => stdoutLogger,
+        onProxyRes: (proxyRes, _, res) => {
+            let dokumentData = '';
+            const _end = res.end;
+            res.write = () => true;
+            proxyRes.on('data', chunk => {
+                dokumentData += chunk;
+            });
+
+            res.end = () => {
+                try {
+                    let dataVises = 'Ukjent feil ved visning dokument';
+                    let visfrontendFeilmelding = true;
+                    JSON.parse(dokumentData, (k, v) => {
+                        if ((k === 'data' || k === 'frontendFeilmelding') && v) {
+                            dataVises = v;
+                        }
+                        if (k === 'data' && v) {
+                            visfrontendFeilmelding = false;
+                        }
+                    });
+                    res.setHeader('content-length', Buffer.byteLength(dataVises));
+                    if (visfrontendFeilmelding) {
+                        res.setHeader('content-encoding', 'utf-8');
+                        res.setHeader('Content-Type', 'text/plain');
+                        _end.call(res, dataVises, 'utf-8');
+                    } else {
+                        res.setHeader('content-encoding', 'base64');
+                        res.setHeader('Content-Type', 'application/pdf');
+                        _end.call(res, dataVises, 'base64');
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            };
+        },
+    });
+};
+
 export const attachToken = (authClient: Client) => {
     return async (req: Request, _res: Response, next: NextFunction) => {
         getOnBehalfOfAccessToken(authClient, req, oboConfig).then((accessToken: string) => {
