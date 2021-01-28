920a0568697878f2093c526c0a67761f303086c0
Meng Zhu <meng.zhu@nav.no>
2021-01-28 12:08:02 +0100

    revert

--- src/frontend/komponenter/ManuellJournalfør/KnyttTilNyBehandling.tsx
+++ src/frontend/komponenter/ManuellJournalfør/KnyttTilNyBehandling.tsx
@@ -5,20 +5,52 @@ import styled from 'styled-components';
 import { CheckboksPanelGruppe } from 'nav-frontend-skjema';
 import { Undertittel } from 'nav-frontend-typografi';
 
+import { FamilieReactSelect } from '@navikt/familie-form-elements';
+
 import { useManuellJournalfør } from '../../context/ManuellJournalførContext';
 import { Behandlingstype, BehandlingÅrsak } from '../../typer/behandling';
-import {
-    BehandlingstypeSelect,
-    BehandlingsårsakSelect,
-} from '../Felleskomponenter/Behandlingsprops/BehandlingspropSelect';
-
-const StyledSelectDiv = styled.div`
-    width: 20rem;
-`;
 
 const StyledCheckboxDiv = styled.div`
     width: 20rem;
 `;
+
+const StyledSelect = styled(FamilieReactSelect)`
+    width: 20rem;
+`;
+
+const tilAlternativ = (key: Behandlingstype | BehandlingÅrsak) =>
+    (
+        key.toString().charAt(0).toLocaleUpperCase() +
+        key.toString().substring(1, key.toString().length).toLocaleLowerCase()
+    ).replace('_', ' ');
+
+const tilBehandlingstype = (type: string) =>
+    Behandlingstype[type.replace(' ', '_').toLocaleUpperCase() as keyof typeof Behandlingstype];
+
+const tilBehandlingÅrsak = (årsak: string) =>
+    BehandlingÅrsak[årsak.replace(' ', '_').toLocaleUpperCase() as keyof typeof BehandlingÅrsak];
+
+const nyBehandlingstyper = [
+    {
+        value: tilAlternativ(Behandlingstype.FØRSTEGANGSBEHANDLING),
+        label: tilAlternativ(Behandlingstype.FØRSTEGANGSBEHANDLING),
+    },
+    {
+        value: tilAlternativ(Behandlingstype.REVURDERING),
+        label: tilAlternativ(Behandlingstype.REVURDERING),
+    },
+];
+
+const nyBehandlingsårsaker = [
+    {
+        value: tilAlternativ(BehandlingÅrsak.SØKNAD),
+        label: tilAlternativ(BehandlingÅrsak.SØKNAD),
+    },
+    {
+        value: tilAlternativ(BehandlingÅrsak.NYE_OPPLYSNINGER),
+        label: tilAlternativ(BehandlingÅrsak.NYE_OPPLYSNINGER),
+    },
+];
 
 export const KnyttTilNyBehandling: React.FC = () => {
     const {
@@ -50,28 +82,47 @@ export const KnyttTilNyBehandling: React.FC = () => {
                 />
             </StyledCheckboxDiv>
             {knyttTilNyBehandling && (
-                <StyledSelectDiv>
+                <>
                     <br />
-                    <BehandlingstypeSelect
-                        options={[
-                            Behandlingstype.FØRSTEGANGSBEHANDLING,
-                            Behandlingstype.REVURDERING,
-                        ]}
-                        value={nyBehandlingstype}
+                    <StyledSelect
+                        creatable={false}
+                        erLesevisning={false}
                         label={'Behandlingstype'}
-                        onChange={value => settNyBehandlingstype(value)}
+                        id="select"
+                        isMulti={false}
+                        options={nyBehandlingstyper}
+                        value={{
+                            value: tilAlternativ(nyBehandlingstype),
+                            label: tilAlternativ(nyBehandlingstype),
+                        }}
+                        onChange={value => {
+                            if (value && 'value' in value) {
+                                settNyBehandlingstype(tilBehandlingstype(value.value));
+                            }
+                        }}
                     />
-                </StyledSelectDiv>
+                </>
             )}
-            {knyttTilNyBehandling && nyBehandlingstype === Behandlingstype.REVURDERING && (
-                <StyledSelectDiv>
-                    <BehandlingsårsakSelect
-                        options={[BehandlingÅrsak.SØKNAD, BehandlingÅrsak.NYE_OPPLYSNINGER]}
-                        value={nyBehandlingsårsak}
+            {nyBehandlingstype === Behandlingstype.REVURDERING && (
+                <>
+                    <StyledSelect
+                        creatable={false}
+                        erLesevisning={false}
                         label={'Årsak'}
-                        onChange={value => settNyBehandlingsårsak(value)}
+                        id="select"
+                        isMulti={false}
+                        options={nyBehandlingsårsaker}
+                        value={{
+                            value: tilAlternativ(nyBehandlingsårsak),
+                            label: tilAlternativ(nyBehandlingsårsak),
+                        }}
+                        onChange={value => {
+                            if (value && 'value' in value) {
+                                settNyBehandlingsårsak(tilBehandlingÅrsak(value.value));
+                            }
+                        }}
                     />
-                </StyledSelectDiv>
+                </>
             )}
         </>
     );
