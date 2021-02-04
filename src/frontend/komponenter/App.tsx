8f55291054bc38a884661b59bb45249e3b9dce33
Henning Håkonsen <henning.hakonsen@nav.no>
2021-02-04 19:36:21 +0100

    Logger skjermstørrelse ved sidelast.

--- src/frontend/komponenter/App.tsx
+++ src/frontend/komponenter/App.tsx
@@ -6,17 +6,20 @@ import { ISaksbehandler } from '@navikt/familie-typer';
 
 import { hentInnloggetBruker } from '../api/saksbehandler';
 import { AppProvider } from '../context/AppContext';
+import { useAmplitude } from '../utils/amplitude';
 import Container from './Container';
 import ErrorBoundary from './Felleskomponenter/ErrorBoundary/ErrorBoundary';
 
 Modal.setAppElement(document.getElementById('modal-a11y-wrapper'));
 
 const App: React.FC = () => {
+    const { loggSkjermstørrelse } = useAmplitude();
     const [autentisertSaksbehandler, settInnloggetSaksbehandler] = React.useState<
         ISaksbehandler | undefined
     >(undefined);
 
     React.useEffect(() => {
+        loggSkjermstørrelse();
         hentInnloggetBruker().then((innhentetInnloggetSaksbehandler: ISaksbehandler) => {
             settInnloggetSaksbehandler(innhentetInnloggetSaksbehandler);
         });
