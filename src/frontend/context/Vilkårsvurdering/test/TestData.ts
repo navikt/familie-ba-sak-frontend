638211e9ef19c69ba62c02ad6ef438cb8ff75dbf
Marcus Gustafson <marcus.gustafson@computas.com>
2021-03-02 16:02:47 +0100

    Endrett testene til å bli mer enkle å lese

--- src/frontend/context/Vilkårsvurdering/test/TestData.ts
+++ src/frontend/context/Vilkårsvurdering/test/TestData.ts
@@ -1,234 +1,44 @@
-import { IFagsak } from '../../../typer/fagsak';
-import { Resultat } from '../../../typer/vilkår';
+import { IGrunnlagPerson } from '../../../typer/person';
+import { IRestPersonResultat } from '../../../typer/vilkår';
 
-export const testFagsak: IFagsak = JSON.parse(`{
-    "opprettetTidspunkt": "2021-02-17T12:03:14.529",
-    "id": 1051701,
-    "søkerFødselsnummer": "11079219845",
-    "status": "OPPRETTET",
-    "underBehandling": true,
-    "behandlinger": [
-      {
-        "aktiv": true,
-        "arbeidsfordelingPåBehandling": {
-          "behandlendeEnhetId": "4842",
-          "behandlendeEnhetNavn": "NAV Familie- og pensjonsytelser Stord",
-          "manueltOverstyrt": false
-        },
-        "årsak": "SØKNAD",
-        "skalBehandlesAutomatisk": false,
-        "behandlingId": 1054601,
-        "type": "FØRSTEGANGSBEHANDLING",
-        "status": "UTREDES",
-        "steg": "VILKÅRSVURDERING",
-        "stegTilstand": [
-          {
-            "behandlingSteg": "REGISTRERE_PERSONGRUNNLAG",
-            "behandlingStegStatus": "UTFØRT"
-          },
-          {
-            "behandlingSteg": "REGISTRERE_SØKNAD",
-            "behandlingStegStatus": "UTFØRT"
-          },
-          {
-            "behandlingSteg": "VILKÅRSVURDERING",
-            "behandlingStegStatus": "IKKE_UTFØRT"
-          }
-        ],
-        "kategori": "NASJONAL",
-        "personer": [
-          {
-            "type": "BARN",
-            "fødselsdato": "2018-06-12",
-            "personIdent": "12061879860",
-            "navn": "TVILSOM SAKS",
-            "kjønn": "KVINNE",
-            "målform": "NB"
-          },
-          {
+export const genererPersonresultat = (
+    annenVurderingEnResultat: string,
+    annenVurderingToResultat: string
+): IRestPersonResultat =>
+    JSON.parse(`{
+        "personIdent": "11079219845",
+        "vilkårResultater": [],
+        "andreVurderinger": [
+            {
+                "id": 2,
+                "begrunnelse": null,
+                "behandlingId": 1054601,
+                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
+                "endretTidspunkt": "2021-02-17T12:40:08.458",
+                "erVurdert": true,
+                "resultat": "${annenVurderingEnResultat}",
+                "type": "OPPLYSNINGSPLIKT"
+            },
+            {
+                "id": 3,
+                "begrunnelse": null,
+                "behandlingId": 1054601,
+                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
+                "endretTidspunkt": "2021-02-17T12:40:08.458",
+                "erVurdert": true,
+                "resultat": "${annenVurderingToResultat}",
+                "type": "OPPLYSNINGSPLIKT"
+            }
+        ]
+    }`);
+
+export const genererPerson: IGrunnlagPerson = JSON.parse(
+    `{
             "type": "SØKER",
             "fødselsdato": "1992-07-11",
             "personIdent": "11079219845",
             "navn": "FRODIG HEST",
             "kjønn": "KVINNE",
             "målform": "NB"
-          }
-        ],
-        "opprettetTidspunkt": "2021-02-17T12:03:23.283",
-        "underkategori": "ORDINÆR",
-        "personResultater": [
-          {
-            "personIdent": "11079219845",
-            "vilkårResultater": [
-              {
-                "id": 1108486,
-                "vilkårType": "LOVLIG_OPPHOLD",
-                "resultat": "OPPFYLT",
-                "periodeFom": "1992-07-11",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:08.458",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": false
-              },
-              {
-                "id": 1108485,
-                "vilkårType": "BOSATT_I_RIKET",
-                "resultat": "OPPFYLT",
-                "periodeFom": "1992-07-11",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:08.458",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": false
-              }
-            ],
-            "andreVurderinger": [
-              {
-                "id": 2,
-                "begrunnelse": null,
-                "behandlingId": 1054601,
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:08.458",
-                "erVurdert": true,
-                "resultat": "OPPFYLT",
-                "type": "OPPLYSNINGSPLIKT"
-              }
-            ]
-          },
-          {
-            "personIdent": "12061879860",
-            "vilkårResultater": [
-              {
-                "id": 1108480,
-                "vilkårType": "BOR_MED_SØKER",
-                "resultat": "OPPFYLT",
-                "periodeFom": "2018-06-12",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:16.221",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": false
-              },
-              {
-                "id": 1108484,
-                "vilkårType": "GIFT_PARTNERSKAP",
-                "resultat": "OPPFYLT",
-                "periodeFom": "2018-06-12",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:08.458",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": true,
-                "erEksplisittAvslagPåSøknad": null
-              },
-              {
-                "id": 1108483,
-                "vilkårType": "UNDER_18_ÅR",
-                "resultat": "OPPFYLT",
-                "periodeFom": "2018-06-12",
-                "periodeTom": "2036-05-10",
-                "begrunnelse": "Vurdert og satt automatisk",
-                "endretAv": "F_Z994954.E_Z994954@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-19T10:26:16.245",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": null
-              },
-              {
-                "id": 1108481,
-                "vilkårType": "BOSATT_I_RIKET",
-                "resultat": "OPPFYLT",
-                "periodeFom": "2018-06-12",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:19.027",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": false
-              },
-              {
-                "id": 1108482,
-                "vilkårType": "LOVLIG_OPPHOLD",
-                "resultat": "OPPFYLT",
-                "periodeFom": "2018-06-12",
-                "periodeTom": null,
-                "begrunnelse": "",
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:23.708",
-                "behandlingId": 1054601,
-                "erVurdert": true,
-                "erAutomatiskVurdert": false,
-                "erEksplisittAvslagPåSøknad": false
-              }
-            ],
-            "andreVurderinger": [
-              {
-                "id": 2,
-                "begrunnelse": null,
-                "behandlingId": 1054601,
-                "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-                "endretTidspunkt": "2021-02-17T12:40:08.458",
-                "erVurdert": true,
-                "resultat": "OPPFYLT",
-                "type": "OPPLYSNINGSPLIKT"
-              }
-            ]
-          }
-        ],
-        "resultat": "IKKE_VURDERT",
-        "vedtakForBehandling": [],
-        "totrinnskontroll": null,
-        "utbetalingsperioder": [],
-        "vedtaksperioder": [],
-        "personerMedAndelerTilkjentYtelse": [],
-        "endretAv": "F_Z991772.E_Z991772@trygdeetaten.no",
-        "opplysningsplikt": null
-      }
-    ]
-  }`);
-
-export const søkerIdent = '11079219845';
-export const barnIdent = '12061879860';
-
-export const lagAndreVurderingResultater = (fagsak: IFagsak, resultat: Resultat) => {
-    fagsak.behandlinger.forEach(behandling =>
-        behandling.personResultater.forEach(personResultat =>
-            personResultat.andreVurderinger.forEach(
-                annenVurdering => (annenVurdering.resultat = resultat)
-            )
-        )
-    );
-    return fagsak;
-};
-
-export const lagPersonAndreVurderingResultater = (
-    fagsak: IFagsak,
-    personIdent: string,
-    resultat: Resultat
-) => {
-    fagsak.behandlinger.forEach(behandling =>
-        behandling.personResultater.forEach(
-            personResultat =>
-                personResultat.personIdent === personIdent &&
-                personResultat.andreVurderinger.forEach(
-                    annenVurdering => (annenVurdering.resultat = resultat)
-                )
-        )
-    );
-    return fagsak;
-};
+          }`
+);
--- src/frontend/context/Vilkårsvurdering/test/validering.test.ts
+++ src/frontend/context/Vilkårsvurdering/test/validering.test.ts
@@ -1,102 +1,59 @@
 import { Valideringsstatus } from '@navikt/familie-skjema';
 
-import { PersonType } from '../../../typer/person';
-import { Resultat } from '../../../typer/vilkår';
 import { kjørValidering } from '../validering';
 import { mapFraRestPersonResultatTilPersonResultat } from '../vilkårsvurdering';
-import {
-    barnIdent,
-    lagAndreVurderingResultater,
-    lagPersonAndreVurderingResultater,
-    søkerIdent,
-    testFagsak,
-} from './TestData';
+import { genererPerson, genererPersonresultat } from './TestData';
 
 describe('vilkårsvurdering/validering', () => {
     describe('validering', () => {
-        test('Skal validere gjennom å returnere tom liste', () => {
-            const testFagsakIkkeVurdert = lagAndreVurderingResultater(
-                testFagsak,
-                Resultat.IKKE_VURDERT
+        test('List med to andrevurderinger som begge validerer OK', () => {
+            const p = mapFraRestPersonResultatTilPersonResultat(
+                [genererPersonresultat('OPPFYLT', 'IKKE_OPPFYLT')],
+                [genererPerson]
             );
-            const personResultaterIkkeVurdert = mapFraRestPersonResultatTilPersonResultat(
-                testFagsakIkkeVurdert.behandlinger[0].personResultater,
-                testFagsakIkkeVurdert.behandlinger[0].personer
-            );
-
-            const søkerPersonResultatIkkeVurdert = personResultaterIkkeVurdert.find(
-                resultat => resultat.person.type === PersonType.SØKER
-            );
-
-            const validert =
-                søkerPersonResultatIkkeVurdert && kjørValidering([søkerPersonResultatIkkeVurdert]);
+            const validert = kjørValidering(p);
             expect(!!validert).toBe(true);
-            const personResultatMedAndreVurderingerIkkeFeil = validert?.find(personresultat =>
-                personresultat.andreVurderinger.find(
+
+            const anneVurderingMedOKValidering = validert
+                ?.flatMap(personResultat => personResultat.andreVurderinger)
+                .filter(
                     annenVurdering => annenVurdering.valideringsstatus !== Valideringsstatus.FEIL
-                )
-            );
-            expect(!!personResultatMedAndreVurderingerIkkeFeil).toBe(false);
+                );
+
+            expect(anneVurderingMedOKValidering?.length).toBe(2);
         });
 
-        test('Skal validere gjennom å returnere tom liste', () => {
-            const testFagsakOppfylt = lagAndreVurderingResultater(testFagsak, Resultat.OPPFYLT);
-
-            const personResultaterOppfylt = mapFraRestPersonResultatTilPersonResultat(
-                testFagsakOppfylt.behandlinger[0].personResultater,
-                testFagsakOppfylt.behandlinger[0].personer
+        test('List med to andrevurderinger som begge validerer FEIL', () => {
+            const p = mapFraRestPersonResultatTilPersonResultat(
+                [genererPersonresultat('IKKE_VURDERT', 'IKKE_VURDERT')],
+                [genererPerson]
             );
-
-            const søkerPersonResultatOppfylt = personResultaterOppfylt.find(
-                resultat => resultat.person.type === PersonType.SØKER
-            );
-
-            const validert =
-                søkerPersonResultatOppfylt && kjørValidering([søkerPersonResultatOppfylt]);
+            const validert = kjørValidering(p);
             expect(!!validert).toBe(true);
-            const personResultatMedAndreVurderingerFeil = validert?.find(personresultat =>
-                personresultat.andreVurderinger.find(
-                    annenVurdering => annenVurdering.valideringsstatus === Valideringsstatus.FEIL
-                )
-            );
-            expect(!!personResultatMedAndreVurderingerFeil).toBe(false);
+            const anneVurderingMedOKValidering = validert
+                ?.flatMap(personResultat => personResultat.andreVurderinger)
+                .filter(
+                    annenVurdering => annenVurdering.valideringsstatus !== Valideringsstatus.FEIL
+                );
+
+            expect(anneVurderingMedOKValidering?.length).toBe(0);
         });
 
-        test('Skal validere gjennom å returnere riktig resultater', () => {
-            const testFagsakOppfylt = lagAndreVurderingResultater(testFagsak, Resultat.OPPFYLT);
-            const testFagsakMikst = lagPersonAndreVurderingResultater(
-                testFagsakOppfylt,
-                søkerIdent,
-                Resultat.IKKE_VURDERT
+        test('List med to andrevurderinger hvor en validerer FEIL og en OK', () => {
+            const p = mapFraRestPersonResultatTilPersonResultat(
+                [genererPersonresultat('OPPFYLT', 'IKKE_VURDERT')],
+                [genererPerson]
             );
-            const personResultater = mapFraRestPersonResultatTilPersonResultat(
-                testFagsakMikst.behandlinger[0].personResultater,
-                testFagsakMikst.behandlinger[0].personer
-            );
+            const validert = kjørValidering(p);
+            expect(!!validert).toBe(true);
 
-            const søkerResultat = personResultater.find(
-                personResultat => personResultat.personIdent === søkerIdent
-            );
-            const søkerValidert = søkerResultat && kjørValidering([søkerResultat]);
-            expect(!!søkerValidert).toBe(true);
-            const søkeResultatMedAndreVurderingIkkeFeil = søkerValidert?.find(personresultat =>
-                personresultat.andreVurderinger.find(
+            const anneVurderingMedOKValidering = validert
+                ?.flatMap(personResultat => personResultat.andreVurderinger)
+                .filter(
                     annenVurdering => annenVurdering.valideringsstatus !== Valideringsstatus.FEIL
-                )
-            );
-            expect(!!søkeResultatMedAndreVurderingIkkeFeil).toBe(false);
+                );
 
-            const barnResultat = personResultater.find(
-                personResultat => personResultat.personIdent === barnIdent
-            );
-            const barnValidert = barnResultat && kjørValidering([barnResultat]);
-            expect(!!barnValidert).toBe(true);
-            const barnResultatMedAndreVurderingerFeil = barnValidert?.find(personresultat =>
-                personresultat.andreVurderinger.find(
-                    annenVurdering => annenVurdering.valideringsstatus === Valideringsstatus.FEIL
-                )
-            );
-            expect(!!barnResultatMedAndreVurderingerFeil).toBe(false);
+            expect(anneVurderingMedOKValidering?.length).toBe(1);
         });
     });
 });
