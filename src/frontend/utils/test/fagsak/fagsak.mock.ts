bf2b1d5af068d53cec502c4fe7c0f513cb7b49a7
mundal <halvorbmundal@gmail.com>
2021-03-04 09:38:44 +0100

    Forenkle fagsak mock

--- src/frontend/utils/test/fagsak/fagsak.mock.ts
+++ src/frontend/utils/test/fagsak/fagsak.mock.ts
@@ -2,7 +2,7 @@ import { IBehandling } from '../../../typer/behandling';
 import { FagsakStatus, IFagsak } from '../../../typer/fagsak';
 import { mockBehandling } from '../behandling/behandling.mock';
 
-interface IFagsakMockInput {
+interface IMockFagsak {
     behandlinger?: IBehandling[];
     id?: number;
     opprettetTidspunkt?: string;
@@ -13,16 +13,14 @@ interface IFagsakMockInput {
 }
 
 export const mockFagsak = ({
-    behandlinger = [
-        mockBehandling({ behandlingId: 1, opprettetTidspunkt: '2020-10-19T02:01:00.0' }),
-    ],
+    behandlinger = [mockBehandling()],
     søkerFødselsnummer = '12345678910',
     id = 1,
     opprettetTidspunkt = '2020-09-19T09:08:56.8',
     saksnummer = '1234',
     status = FagsakStatus.LØPENDE,
     underBehandling = false,
-}: IFagsakMockInput): IFagsak => ({
+}: IMockFagsak = {}): IFagsak => ({
     behandlinger,
     id,
     søkerFødselsnummer,
