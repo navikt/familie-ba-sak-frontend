2d434a9b91a2d66a027742f64d77f53938350fb8
mundal <halvorbmundal@gmail.com>
2021-03-04 09:39:15 +0100

    Legg til test for alleVilkårOppfylt

--- src/frontend/utils/test/fagsak/fagsak.test.ts
+++ src/frontend/utils/test/fagsak/fagsak.test.ts
@@ -1,6 +1,8 @@
 import { IFagsak } from '../../../typer/fagsak';
-import { hentSisteBehandlingPåFagsak } from '../../fagsak';
+import { IVilkårResultat, Resultat, VilkårType } from '../../../typer/vilkår';
+import { alleVillkårOppfylt, hentSisteBehandlingPåFagsak } from '../../fagsak';
 import { mockBehandling } from '../behandling/behandling.mock';
+import { mockFeltstate, mockVilkårResultater } from '../vilkårsvurdering/vilkårsvurdering.mock';
 import { mockFagsak } from './fagsak.mock';
 
 describe('utils/fagsak', () => {
@@ -16,6 +18,7 @@ describe('utils/fagsak', () => {
             behandlinger: [
                 mockBehandling({ behandlingId: 1, opprettetTidspunkt: '2020-03-19T02:01:00.0' }),
                 mockBehandling({ behandlingId: 2, opprettetTidspunkt: '2020-03-19T03:00:00.0' }),
+                mockBehandling({ behandlingId: 3, opprettetTidspunkt: '2020-03-19T01:00:00.0' }),
             ],
         });
 
@@ -25,6 +28,43 @@ describe('utils/fagsak', () => {
 
         test('Skal returnere dato på format MM.YY', () => {
             expect(hentSisteBehandlingPåFagsak(fagsak2)?.behandlingId).toEqual(2);
+        });
+    });
+
+    describe('alleVilkårOppfylt', () => {
+        const vilkårRestultater1 = [
+            VilkårType.LOVLIG_OPPHOLD,
+            VilkårType.BOSATT_I_RIKET,
+            VilkårType.GIFT_PARTNERSKAP,
+            VilkårType.UNDER_18_ÅR,
+            VilkårType.BOR_MED_SØKER,
+        ].map(vilkårType =>
+            mockVilkårResultater({
+                resultat: Resultat.OPPFYLT,
+                vilkårType,
+            })
+        );
+
+        const ikkeOppfyltResultat = mockFeltstate(Resultat.IKKE_OPPFYLT);
+        const vilkårResultater2: IVilkårResultat[] = [
+            { ...vilkårRestultater1[0], resultat: ikkeOppfyltResultat },
+            ...vilkårRestultater1.slice(1),
+        ];
+
+        test('Skal returnere true', () => {
+            expect(
+                alleVillkårOppfylt(
+                    vilkårRestultater1.map(vilkårRestultat => mockFeltstate(vilkårRestultat))
+                )
+            ).toEqual(true);
+        });
+
+        test('Skal returnere false', () => {
+            expect(
+                alleVillkårOppfylt(
+                    vilkårResultater2.map(vilkårRestultat => mockFeltstate(vilkårRestultat))
+                )
+            ).toEqual(false);
         });
     });
 });
