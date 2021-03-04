7a2a1f15e672ecc2cce687f4e4e7cb72f0ec57af
mundal <halvorbmundal@gmail.com>
2021-03-04 09:37:51 +0100

    Flytt opprettelsen av person og restPersonResultat til mockfiler

--- src/frontend/utils/test/behandling/behandling.mock.ts
+++ src/frontend/utils/test/behandling/behandling.mock.ts
@@ -1,5 +1,3 @@
-import { kjønnType } from '@navikt/familie-typer';
-
 import {
     BehandlingKategori,
     BehandlingResultat,
@@ -11,11 +9,11 @@ import {
     BehandlingÅrsak,
     IBehandling,
 } from '../../../typer/behandling';
-import { IGrunnlagPerson, PersonType } from '../../../typer/person';
-import { Målform } from '../../../typer/søknad';
-import { IRestPersonResultat, Resultat, VilkårType } from '../../../typer/vilkår';
+import { IRestPersonResultat } from '../../../typer/vilkår';
+import { mockBarn, mockSøker } from '../person/person.mock';
+import { mockRestPersonResultat } from '../vilkårsvurdering/vilkårsvurdering.mock';
 
-interface IMockBehandlingInput {
+interface IMockBehandling {
     behandlingId?: number;
     aktiv?: boolean;
     steg?: string;
@@ -27,114 +25,18 @@ export const mockBehandling = ({
     opprettetTidspunkt = '2020-03-19T09:08:56.8',
     steg = 'VILKÅRSVURDERING',
     aktiv = true,
-}: IMockBehandlingInput): IBehandling => {
-    const barn: IGrunnlagPerson = {
-        personIdent: '12345678903',
-        fødselsdato: '2006-11-20',
-        type: PersonType.BARN,
-        kjønn: 'KVINNE' as kjønnType,
-        navn: 'Mock Barn',
-        målform: Målform.NB,
-    };
+}: IMockBehandling = {}): IBehandling => {
+    const barn = mockBarn;
 
-    const søker: IGrunnlagPerson = {
-        personIdent: '12345678930',
-        fødselsdato: '1979-01-14',
-        type: PersonType.SØKER,
-        kjønn: 'KVINNE' as kjønnType,
-        navn: 'Mock Søker',
-        målform: Målform.NB,
-    };
+    const søker = mockSøker;
 
-    const søkerPersonResultat: IRestPersonResultat = {
+    const søkerRestPersonResultat: IRestPersonResultat = mockRestPersonResultat({
         personIdent: søker.personIdent,
-        vilkårResultater: [
-            {
-                id: 1,
-                vilkårType: VilkårType.LOVLIG_OPPHOLD,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: '2000-01-01',
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-        ],
-    };
+    });
 
-    const barnPersonResultat: IRestPersonResultat = {
+    const barnRestPersonResultat: IRestPersonResultat = mockRestPersonResultat({
         personIdent: barn.personIdent,
-        vilkårResultater: [
-            {
-                id: 2,
-                vilkårType: VilkårType.BOSATT_I_RIKET,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: undefined,
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-            {
-                id: 3,
-                vilkårType: VilkårType.LOVLIG_OPPHOLD,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: undefined,
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-            {
-                id: 4,
-                vilkårType: VilkårType.GIFT_PARTNERSKAP,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: undefined,
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-            {
-                id: 5,
-                vilkårType: VilkårType.UNDER_18_ÅR,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: undefined,
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-            {
-                id: 6,
-                vilkårType: VilkårType.BOR_MED_SØKER,
-                resultat: Resultat.IKKE_VURDERT,
-                periodeFom: undefined,
-                periodeTom: undefined,
-                begrunnelse: '',
-                endretAv: 'VL',
-                erVurdert: false,
-                erAutomatiskVurdert: false,
-                endretTidspunkt: '2020-03-19T09:08:56.8',
-                behandlingId,
-            },
-        ],
-    };
+    });
 
     return {
         behandlingId,
@@ -163,7 +65,7 @@ export const mockBehandling = ({
         kategori: BehandlingKategori.NASJONAL,
         underkategori: BehandlingUnderkategori.ORDINÆR,
         status: BehandlingStatus.FATTER_VEDTAK,
-        personResultater: [søkerPersonResultat, barnPersonResultat],
+        personResultater: [søkerRestPersonResultat, barnRestPersonResultat],
         vedtakForBehandling: [],
         endretAv: 'VL',
         totrinnskontroll: {
@@ -172,7 +74,7 @@ export const mockBehandling = ({
             godkjent: true,
             opprettetTidspunkt,
         },
-        utbetalingsperioder: [],
+        vedtaksperioder: [],
         personerMedAndelerTilkjentYtelse: [],
         årsak: BehandlingÅrsak.SØKNAD,
         skalBehandlesAutomatisk: false,
