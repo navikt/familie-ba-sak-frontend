9c7ec35a0babc568318c367800fdb76eeb9e2801
Nina Kjekstad <nina.kjekstad@nav.no>
2021-03-05 09:50:53 +0100

    Populer multiselectoptions

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/AvslagBegrunnelseMultiselect.tsx
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/AvslagBegrunnelseMultiselect.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { FamilieReactSelect } from '@navikt/familie-form-elements';
+import { RessursStatus } from '@navikt/familie-typer';
 
 import { useBehandling } from '../../../../context/BehandlingContext';
+import { useVedtakBegrunnelser } from '../../../../context/VedtakBegrunnelseContext';
+import { IRestVedtakBegrunnelseTilknyttetVilkår } from '../../../../typer/vedtak';
 
 interface IProps {
     visFeilmeldinger: boolean;
@@ -15,10 +18,18 @@ interface IOptionType {
 
 const AvslagBegrunnelseMultiselect: React.FC<IProps> = ({ visFeilmeldinger }) => {
     const { erLesevisning } = useBehandling();
+    const { vilkårBegrunnelser } = useVedtakBegrunnelser();
 
-    const lagoptions = (): IOptionType[] => {
-        return [];
-    };
+    const options: IOptionType[] =
+        vilkårBegrunnelser.status === RessursStatus.SUKSESS
+            ? vilkårBegrunnelser.data.AVSLAG.map(
+                  (begrunnelse: IRestVedtakBegrunnelseTilknyttetVilkår) => ({
+                      label: begrunnelse.navn,
+                      value: begrunnelse.id,
+                  })
+              )
+            : [];
+
     return (
         <FamilieReactSelect
             label={'Begrunnelse(r) til vedtaksbrev'}
@@ -30,7 +41,7 @@ const AvslagBegrunnelseMultiselect: React.FC<IProps> = ({ visFeilmeldinger }) =>
                 console.log(valgteOptions);
                 console.log(visFeilmeldinger);
             }}
-            options={lagoptions()}
+            options={options}
         />
     );
 };
