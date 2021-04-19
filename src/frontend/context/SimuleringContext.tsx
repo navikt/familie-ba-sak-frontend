e4ca3e79e00849331805da149880fbc86c96d8ca
Nina Kjekstad <nina.kjekstad@nav.no>
2021-04-19 16:02:00 +0200

    debug

--- src/frontend/context/SimuleringContext.tsx
+++ src/frontend/context/SimuleringContext.tsx
@@ -27,6 +27,7 @@ const [SimuleringProvider, useSimulering] = constate(({ åpenBehandling }: IProp
             method: 'GET',
             url: `/familie-ba-sak/api/simulering/${aktivtVedtak?.id}`,
         }).then(response => {
+            console.log('setter resultat');
             settSimuleringresultat(response);
         });
     }, [aktivtVedtak]);
@@ -39,17 +40,61 @@ const [SimuleringProvider, useSimulering] = constate(({ åpenBehandling }: IProp
             .sort((a, b) => (dayjs(a).isAfter(dayjs(b)) ? 1 : -1));
         const førstePeriode = fomDatoer[0];
         const sistePeriode = fomDatoer[fomDatoer.length - 1];
-
+        console.log('---henter---');
+        console.log('fomDatoer: ' + fomDatoer);
         let aktuellPeriode = førstePeriode;
         for (let i = 0; i < dayjs(sistePeriode).diff(dayjs(førstePeriode), 'M'); i++) {
             aktuellPeriode = familieDayjs(aktuellPeriode).add(1, 'M').format();
             if (!fomDatoer.includes(aktuellPeriode)) {
+                console.log('Fant ikke aktuellPeriode: ' + aktuellPeriode);
                 perioder.push({
                     fom: aktuellPeriode,
                     tom: '',
                 });
+            } else {
+                console.log('Fant: ' + aktuellPeriode);
             }
         }
+        /*
+        Foreslått løsning:
+
+        console.log('----henter----');
+        const sortertePerioder = perioder
+            .map(periode => familieDayjs(periode.fom))
+            .sort((a, b) => (a.isAfter(b) ? 1 : -1));
+        const førstePeriode = sortertePerioder[0];
+        const sistePeriode = sortertePerioder[sortertePerioder.length - 1];
+
+        let aktuellPeriode = førstePeriode;
+        for (let i = 0; i < sistePeriode.diff(førstePeriode, 'M'); i++) {
+            aktuellPeriode = aktuellPeriode.add(1, 'M');
+            // eslint-disable-next-line
+            const periodeISimulering = sortertePerioder.filter(
+                // eslint-disable-next-line
+                dato =>
+                    // eslint-disable-next-line
+                    aktuellPeriode.month() === dato.month() && aktuellPeriode.year() === dato.year()
+            );
+            console.log(
+                aktuellPeriode.month() +
+                    '/' +
+                    aktuellPeriode.year() +
+                    ' ' +
+                    periodeISimulering.length
+            );
+            if (periodeISimulering.length === 0) {
+                //console.log('tom ', periodeISimulering);
+                perioder.push({
+                    fom: aktuellPeriode.format(),
+                    tom: '',
+                });
+            } else {
+                //console.log('ikke tom ', periodeISimulering);
+            }
+        }
+        
+         */
+
         perioder.sort((a, b) => (dayjs(a.fom).isAfter(dayjs(b.fom)) ? 1 : -1));
         return perioder;
     };
