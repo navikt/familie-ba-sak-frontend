360914e28678566690ad3e7179dd0e1994de960a
Øyvind Hagen <oyvind.hagen@gmail.com>
2021-04-20 16:51:53 +0200

    Bugfix/tom for utbetalt periode i simulering (#942)
    * Setter tom for utbetalt periode før neste periode til utbetaling.
    
    * Lagt til tester for bruk av dayjs.substract
    
    * Refaktorert ut egen funksjon for å hente dato en dag før en gitt annen dato.

--- src/frontend/komponenter/Fagsak/Simulering/SimuleringPanel.tsx
+++ src/frontend/komponenter/Fagsak/Simulering/SimuleringPanel.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import dayjs from 'dayjs';
 import styled from 'styled-components';
 
 import navFarger from 'nav-frontend-core';
@@ -9,6 +10,7 @@ import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { ISimuleringDTO } from '../../../typer/simulering';
 import familieDayjs from '../../../utils/familieDayjs';
 import { formaterBeløp, formaterIsoDato, datoformat } from '../../../utils/formatter';
+import { datoDagenFør } from '../../../utils/tid';
 
 const StyledPanel = styled(Panel)`
     max-width: 26rem;
@@ -47,14 +49,7 @@ interface ISimuleringProps {
 }
 
 const SimuleringPanel: React.FunctionComponent<ISimuleringProps> = ({
-    simulering: {
-        feilutbetaling,
-        fom,
-        etterbetaling,
-        tomDatoNestePeriode,
-        fomDatoNestePeriode,
-        perioder,
-    },
+    simulering: { feilutbetaling, fom, etterbetaling, fomDatoNestePeriode, perioder },
 }) => {
     const kapitaliserTekst = (tekst: string): string => {
         return tekst.charAt(0).toUpperCase() + tekst.slice(1).toLowerCase();
@@ -67,6 +62,18 @@ const SimuleringPanel: React.FunctionComponent<ISimuleringProps> = ({
     const nestePeriode = fomDatoNestePeriode
         ? perioder.find(periode => periode.fom === fomDatoNestePeriode) ?? undefined
         : undefined;
+    const sisteUtbetaltePeriode = perioder
+        .filter(periode => dayjs(periode.fom) < dayjs(fomDatoNestePeriode))
+        .sort((a, b) => (dayjs(a.fom).isAfter(dayjs(b.fom)) ? 1 : -1))
+        .slice(-1)
+        .pop();
+    const utbetaltPeriodeTom = fomDatoNestePeriode
+        ? datoDagenFør(familieDayjs(fomDatoNestePeriode, datoformat.ISO_DAG)).format(
+              datoformat.DATO
+          )
+        : sisteUtbetaltePeriode
+        ? sisteUtbetaltePeriode.tom
+        : '';
     return (
         <StyledPanel border>
             <StyledTable>
@@ -79,7 +86,7 @@ const SimuleringPanel: React.FunctionComponent<ISimuleringProps> = ({
                                     `for perioden ${formaterIsoDato(
                                         fom,
                                         datoformat.DATO
-                                    )} - ${formaterIsoDato(tomDatoNestePeriode, datoformat.DATO)}`}
+                                    )} - ${utbetaltPeriodeTom}`}
                             </Element>
                         </StyledTh>
                     </tr>
