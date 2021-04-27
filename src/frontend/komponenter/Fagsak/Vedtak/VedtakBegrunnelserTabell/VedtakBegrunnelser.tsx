7fe6bb7e3a7d539e898f98866434fb72c9b84eec
Nina Kjekstad <nina.kjekstad@nav.no>
2021-04-27 16:25:39 +0200

    Trekk ut filtreringslogikk, legg til tester og juster logikk

--- src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/VedtakBegrunnelser.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/VedtakBegrunnelser.tsx
@@ -4,62 +4,14 @@ import { useBehandling } from '../../../../context/BehandlingContext';
 import { FritekstVedtakBegrunnelserProvider } from '../../../../context/FritekstVedtakBegrunnelserContext';
 import { useVedtakBegrunnelser } from '../../../../context/VedtakBegrunnelserContext';
 import { IBehandling } from '../../../../typer/behandling';
-import { IRestVedtakBegrunnelse, VedtakBegrunnelseType } from '../../../../typer/vedtak';
 import { Vedtaksperiode, Vedtaksperiodetype } from '../../../../typer/vedtaksperiode';
-import familieDayjs, { familieDayjsDiff } from '../../../../utils/familieDayjs';
-import { datoformat } from '../../../../utils/formatter';
+import { filtrerOgSorterPerioderMedBegrunnelseBehov } from '../../../../utils/vedtakUtils';
 import OverskriftMedHjelpetekst from './Felles/OverskriftMedHjelpetekst';
 import VedtakBegrunnelsePanel from './VedtakBegrunnelsePanel';
 
 interface IVedtakBegrunnelserTabell {
     åpenBehandling: IBehandling;
 }
-
-export const filtrerOgSorterPerioderMedBegrunnelseBehov = (
-    utbetalingsperioder: Vedtaksperiode[],
-    vedtakBegrunnelser: IRestVedtakBegrunnelse[],
-    erLesevisning: boolean
-): Vedtaksperiode[] => {
-    return utbetalingsperioder
-        .slice()
-        .sort((a, b) =>
-            familieDayjsDiff(
-                familieDayjs(a.periodeFom, datoformat.ISO_DAG),
-                familieDayjs(b.periodeFom, datoformat.ISO_DAG)
-            )
-        )
-        .filter((vedtaksperiode: Vedtaksperiode) => {
-            return (
-                vedtaksperiode.vedtaksperiodetype === Vedtaksperiodetype.UTBETALING ||
-                vedtaksperiode.vedtaksperiodetype === Vedtaksperiodetype.OPPHØR
-            );
-        })
-        .filter((vedtaksperiode: Vedtaksperiode) => {
-            const vedtakBegrunnelserForPeriode = vedtakBegrunnelser.filter(
-                (vedtakBegrunnelse: IRestVedtakBegrunnelse) => {
-                    return (
-                        vedtakBegrunnelse.begrunnelseType !== VedtakBegrunnelseType.AVSLAG &&
-                        vedtakBegrunnelse.fom === vedtaksperiode.periodeFom &&
-                        vedtakBegrunnelse.tom === vedtaksperiode.periodeTom
-                    );
-                }
-            );
-
-            if (erLesevisning) {
-                // Viser kun perioder som har begrunnelse dersom man er i lesemodus.
-                return !!vedtakBegrunnelserForPeriode.length;
-            } else {
-                // Fjern perioder hvor fom er mer enn 2 måneder frem i tid.
-                return (
-                    familieDayjsDiff(
-                        familieDayjs(vedtaksperiode.periodeFom),
-                        familieDayjs(),
-                        'month'
-                    ) < 2
-                );
-            }
-        });
-};
 
 const VedtakBegrunnelser: React.FC<IVedtakBegrunnelserTabell> = ({ åpenBehandling }) => {
     const { erLesevisning } = useBehandling();
---
+++ src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/test/VedtakBegrunnelserContext.test.ts
@@ -0,0 +1,109 @@
+import { VedtakBegrunnelse, VedtakBegrunnelseType } from '../../../../../typer/vedtak';
+import { Vedtaksperiodetype } from '../../../../../typer/vedtaksperiode';
+import familieDayjs from '../../../../../utils/familieDayjs';
+import { datoformat } from '../../../../../utils/formatter';
+import { mockVedtakbegrunnelse } from '../../../../../utils/test/vedtak/vedtakbegrunnelse.mock';
+import {
+    mockAvslagssperiode,
+    mockOpphørsperiode,
+    mockUtbetalingsperiode,
+} from '../../../../../utils/test/vedtak/vedtaksperiode.mock';
+import { filtrerOgSorterPerioderMedBegrunnelseBehov } from '../../../../../utils/vedtakUtils';
+
+describe('VedtakBegrunnelserContext', () => {
+    describe('Test filtrerOgSorterPerioderMedBegrunnelseBehov', () => {
+        const fom = '2020-01-01';
+        const tom = '2020-02-28';
+        const opphørFom = '2020-03-01';
+
+        const vedtaksperioder = [
+            mockOpphørsperiode({ periodeFom: opphørFom }),
+            mockUtbetalingsperiode({ periodeFom: fom, periodeTom: tom }),
+            mockAvslagssperiode({ periodeFom: fom, periodeTom: tom }),
+        ];
+
+        test(`Test at kun vedtaksperioder av typen OPPHØR og UTBETALING returneres`, () => {
+            const periodeTyper = filtrerOgSorterPerioderMedBegrunnelseBehov(
+                vedtaksperioder,
+                [],
+                false
+            ).map(p => p.vedtaksperiodetype);
+            expect(periodeTyper.length).toBe(2);
+            expect(periodeTyper).toContain(Vedtaksperiodetype.OPPHØR);
+            expect(periodeTyper).toContain(Vedtaksperiodetype.UTBETALING);
+        });
+
+        test(`Test at returnerte perioder er sortert på fom-dato.`, () => {
+            const perioder = filtrerOgSorterPerioderMedBegrunnelseBehov(vedtaksperioder, [], false);
+            expect(perioder.length).toBe(2);
+            expect(perioder[0].vedtaksperiodetype).toBe(Vedtaksperiodetype.UTBETALING);
+            expect(perioder[1].vedtaksperiodetype).toBe(Vedtaksperiodetype.OPPHØR);
+        });
+
+        describe('Test lesevisning', () => {
+            const erLesevisning = true;
+            test(`Test at ubegrunnede perioder ikke returneres ved lesevisning`, () => {
+                const perioder = filtrerOgSorterPerioderMedBegrunnelseBehov(
+                    vedtaksperioder,
+                    [
+                        mockVedtakbegrunnelse({
+                            begrunnelse: VedtakBegrunnelse.OPPHØR_BARN_FLYTTET_FRA_SØKER,
+                            begrunnelseType: VedtakBegrunnelseType.OPPHØR,
+                            fom: opphørFom,
+                            tom: '',
+                        }),
+                    ],
+                    erLesevisning
+                );
+                expect(perioder.length).toBe(1);
+                expect(perioder[0].vedtaksperiodetype).toEqual(Vedtaksperiodetype.OPPHØR);
+            });
+            test(`Test at perioder med kun avslagsbegrunnelse ikke returneres ved lesevisning`, () => {
+                const perioder = filtrerOgSorterPerioderMedBegrunnelseBehov(
+                    vedtaksperioder,
+                    [
+                        mockVedtakbegrunnelse({
+                            begrunnelse: VedtakBegrunnelse.AVSLAG_MEDLEM_I_FOLKETRYGDEN,
+                            begrunnelseType: VedtakBegrunnelseType.AVSLAG,
+                            fom: fom,
+                            tom: tom,
+                        }),
+                    ],
+                    erLesevisning
+                );
+                expect(perioder.length).toBe(0);
+            });
+        });
+
+        describe('Test filtrering av perioder frem i tid', () => {
+            test(`Test at perioder med fom-dato før 2 mnd frem i tid returneres`, () => {
+                const enMndFremITid = familieDayjs().add(1, 'month');
+                const perioder = filtrerOgSorterPerioderMedBegrunnelseBehov(
+                    [
+                        mockUtbetalingsperiode({
+                            periodeFom: enMndFremITid.startOf('month').format(datoformat.ISO_DAG),
+                            periodeTom: enMndFremITid.endOf('month').format(datoformat.ISO_DAG),
+                        }),
+                    ],
+                    [],
+                    false
+                );
+                expect(perioder.length).toBe(1);
+            });
+            test(`Test at perioder med fom-dato fra og med 2 mnd frem i tid ikke returneres`, () => {
+                const enMndFremITid = familieDayjs().add(2, 'month');
+                const perioder = filtrerOgSorterPerioderMedBegrunnelseBehov(
+                    [
+                        mockUtbetalingsperiode({
+                            periodeFom: enMndFremITid.startOf('month').format(datoformat.ISO_DAG),
+                            periodeTom: enMndFremITid.endOf('month').format(datoformat.ISO_DAG),
+                        }),
+                    ],
+                    [],
+                    false
+                );
+                expect(perioder.length).toBe(0);
+            });
+        });
+    });
+});
