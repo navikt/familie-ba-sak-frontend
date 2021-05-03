7344a27105ff01a3ced8fc1926f7915457da0ffb
Henning Håkonsen <henning.hakonsen@nav.no>
2021-05-03 19:51:09 +0200

    Setter consumer-id ved proxy mot backend. (#995)
    * Setter consumer-id ved proxy mot backend.
    
    * Endrer på navn og setter den til statisk systembeskrivende string.

--- src/backend/proxy.ts
+++ src/backend/proxy.ts
@@ -94,6 +94,7 @@ export const attachToken = (authClient: Client) => {
     return async (req: Request, _res: Response, next: NextFunction) => {
         getOnBehalfOfAccessToken(authClient, req, oboConfig).then((accessToken: string) => {
             req.headers['Nav-Call-Id'] = uuidv4();
+            req.headers['Nav-Consumer-Id'] = 'familie-ba-sak-front';
             req.headers.Authorization = `Bearer ${accessToken}`;
             return next();
         });
