e13f5d5a18d2ca09b892c0736b7bd408628c57d5
mundal <halvorbmundal@gmail.com>
2021-05-12 12:26:54 +0200

    Legg til kalender diff for måned

--- src/frontend/utils/kalender/aritmetikk.ts
+++ src/frontend/utils/kalender/aritmetikk.ts
@@ -88,6 +88,9 @@ export const kalenderDiff = (første: Date, andre: Date) => {
     return første.getTime() - andre.getTime();
 };
 
+export const diffMåned = (første: DagMånedÅr, siste: DagMånedÅr) =>
+    12 * (siste.år - første.år) + (siste.måned - første.måned);
+
 export const dagVedEndringPåÅr = (dagMånedÅr: DagMånedÅr, nyttÅr: number) => {
     const antallDagerIMånedINyttÅr = antallDagerIMåned({ år: nyttÅr, måned: dagMånedÅr.måned });
     if (antallDagerIMånedINyttÅr < dagMånedÅr.dag) {
