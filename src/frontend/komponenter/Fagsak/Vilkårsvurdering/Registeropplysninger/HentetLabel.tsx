8394c76376b928baa8f15eab8f789228c2112605
Nina Kjekstad <nina.kjekstad@nav.no>
2021-06-03 19:46:13 +0200

    Trekk ut henteLabel, samle filer i pakke, legg til sekunder på tidspunkt

---
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/Registeropplysninger/HentetLabel.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import navFarger from 'nav-frontend-core';
+import { Undertekst } from 'nav-frontend-typografi';
+
+export const HentetLabel = styled(Undertekst)`
+    margin-bottom: 1rem;
+    color: ${navFarger.navGra40};
+`;
