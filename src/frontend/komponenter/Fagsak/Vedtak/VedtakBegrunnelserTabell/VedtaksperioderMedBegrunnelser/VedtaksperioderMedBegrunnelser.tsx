b214f5f60608728b0e9f07ac5c300dfa68d36c75
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-06-16 15:32:17 +0200

    Oppdatert lesevisning på multiselect og avslagsbegrunnelser på nytt format (#1195)
    * Støtt avslagsbegrunnelser på nytt format
    
    * Oppdater placeholder
    
    * Oppdater form-elements

--- src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/VedtaksperioderMedBegrunnelser/VedtaksperioderMedBegrunnelser.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/VedtaksperioderMedBegrunnelser/VedtaksperioderMedBegrunnelser.tsx
@@ -6,7 +6,11 @@ import { RessursStatus } from '@navikt/familie-typer';
 
 import { IBehandling } from '../../../../../typer/behandling';
 import { IFagsak } from '../../../../../typer/fagsak';
-import { IVedtaksperiodeMedBegrunnelser } from '../../../../../typer/vedtaksperiode';
+import {
+    IVedtaksperiodeMedBegrunnelser,
+    Vedtaksperiodetype,
+} from '../../../../../typer/vedtaksperiode';
+import { partition } from '../../../../../utils/commons';
 import { hentAktivVedtakPåBehandlig } from '../../../../../utils/fagsak';
 import { useVedtaksbegrunnelseTekster } from '../Context/VedtaksbegrunnelseTeksterContext';
 import { VedtaksperiodeMedBegrunnelserProvider } from '../Context/VedtaksperiodeMedBegrunnelserContext';
@@ -33,13 +37,39 @@ const VedtaksperioderMedBegrunnelser: React.FC<IVedtakBegrunnelserTabell> = ({
         return <AlertStripeFeil>Klarte ikke å hente inn begrunnelser for vedtak.</AlertStripeFeil>;
     }
 
+    const avslagOgResterende = partition(
+        vedtaksperiode => vedtaksperiode.type === Vedtaksperiodetype.AVSLAG,
+        vedtaksperioderMedBegrunnelser
+    );
+
     return vedtaksperioderMedBegrunnelser.length > 0 ? (
         <>
             <OverskriftMedHjelpetekst
                 overskrift={'Begrunnelser i vedtaksbrev'}
                 hjelpetekst={'Her skal du sette begrunnelsestekster for fortsatt innvilgelse'}
             />
-            {vedtaksperioderMedBegrunnelser.map(
+            {avslagOgResterende[1].map(
+                (vedtaksperiodeMedBegrunnelser: IVedtaksperiodeMedBegrunnelser) => (
+                    <VedtaksperiodeMedBegrunnelserProvider
+                        key={vedtaksperiodeMedBegrunnelser.id}
+                        fagsak={fagsak}
+                        åpenBehandling={åpenBehandling}
+                        vedtaksperiodeMedBegrunnelser={vedtaksperiodeMedBegrunnelser}
+                    >
+                        <VedtaksperiodeMedBegrunnelserPanel
+                            vedtaksperiodeMedBegrunnelser={vedtaksperiodeMedBegrunnelser}
+                        />
+                    </VedtaksperiodeMedBegrunnelserProvider>
+                )
+            )}
+
+            <OverskriftMedHjelpetekst
+                overskrift={'Begrunnelser for avslag i vedtaksbrev'}
+                hjelpetekst={
+                    'Her har vi hentet begrunnelsestekster for avslag som du har satt i vilkårsvurderingen.'
+                }
+            />
+            {avslagOgResterende[0].map(
                 (vedtaksperiodeMedBegrunnelser: IVedtaksperiodeMedBegrunnelser) => (
                     <VedtaksperiodeMedBegrunnelserProvider
                         key={vedtaksperiodeMedBegrunnelser.id}
