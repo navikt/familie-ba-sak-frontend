1267dfb90904c00586cd7aed97ffdfb79f413fff
mundal <halvorbmundal@gmail.com>
2021-06-23 15:49:45 +0200

    Endre design og ta inn flere felter

--- src/frontend/komponenter/Fagsak/journalposter/JournalpostListe.tsx
+++ src/frontend/komponenter/Fagsak/journalposter/JournalpostListe.tsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
 
-import { Element } from 'nav-frontend-typografi';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
+import { Sidetittel } from 'nav-frontend-typografi';
 
 import { useHttp } from '@navikt/familie-http';
-import { RessursStatus } from '@navikt/familie-typer';
+import { RessursStatus, Ressurs, byggTomRessurs } from '@navikt/familie-typer';
 
 import { IPersonInfo } from '../../../typer/person';
-import { DagMånedÅr, tilVisning } from '../../../utils/kalender';
+import { DagMånedÅr, tilVisning, kalenderDato } from '../../../utils/kalender';
 
 import 'nav-frontend-tabell-style';
 
 const Container = styled.div`
-    margin: 1rem;
+    margin: 4.1875rem 3.3125rem;
+`;
+
+const StyledSidetittel = styled(Sidetittel)`
+    margin-bottom: 1rem;
 `;
 
 interface IProps {
@@ -24,8 +29,10 @@ interface Journalpost {
     journalpostId: string;
     tittel: string;
     behandlingstema: string;
+    behandlingstemanavn: string;
+    sak: { fagsaksystem: string; tema: string };
+    avsenderMottaker: { navn: string };
     datoMottatt: DagMånedÅr;
-    tema: string;
     journalstatus: string;
 }
 
@@ -38,41 +45,61 @@ const JournalpostListe: React.FC<IProps> = ({ bruker }) => {
             method: 'GET',
             url: `/familie-ba-sak/api/journalpost/for-bruker/${bruker.personIdent}`,
         }).then(journalposterRessurs => {
-            if (journalposterRessurs.status === RessursStatus.SUKSESS) {
-                settjournalposter(journalposterRessurs.data);
-            }
+            journalposterRessurs.status === RessursStatus.SUKSESS &&
+                settJournalposterRessurs(journalposterRessurs);
         });
     }, [bruker]);
 
-    return (
-        <Container>
-            <h2>Journalposter</h2>
-            <table className="tabell">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Tittel</th>
-                        <th>behandlingstema</th>
-                        <th>tema</th>
-                        <th>journalstatus</th>
-                        <th>datoMottatt</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {journalposter.map(journalpost => (
-                        <tr key={journalpost.journalpostId}>
-                            <td>{journalpost.journalpostId}</td>
-                            <td>{journalpost.tittel}</td>
-                            <td>{journalpost.behandlingstema}</td>
-                            <td>{journalpost.tema}</td>
-                            <td>{journalpost.journalstatus}</td>
-                            <td>{tilVisning(journalpost.datoMottatt)}</td>
+    if (
+        journalposterRessurs.status === RessursStatus.FEILET ||
+        journalposterRessurs.status === RessursStatus.FUNKSJONELL_FEIL
+    ) {
+        return (
+            <Container>
+                <AlertStripeFeil>Klarte ikke å hente inn journalposter for fagsak.</AlertStripeFeil>
+            </Container>
+        );
+    }
+
+    if (journalposterRessurs.status === RessursStatus.SUKSESS) {
+        const journalposter = journalposterRessurs.data;
+
+        return (
+            <Container>
+                <StyledSidetittel>Dokumentoversikt</StyledSidetittel>
+                <table className="tabell tabell--stripet">
+                    <thead>
+                        <tr>
+                            <th>DatoMottatt</th>
+                            <th>Tittel</th>
+                            <th>Behandlingstema</th>
+                            <th>Fagsaksystem</th>
+                            <th>Fagsaktema</th>
+                            <th>Avsender</th>
+                            <th>Journalstatus</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </Container>
-    );
+                    </thead>
+                    <tbody>
+                        {journalposter.map(journalpost => (
+                            <tr key={journalpost.journalpostId}>
+                                <td>{tilVisning(journalpost.datoMottatt)}</td>
+                                <td>{journalpost.tittel}</td>
+                                <td>
+                                    {journalpost.behandlingstema}: {journalpost.behandlingstemanavn}
+                                </td>
+                                <td>{journalpost.sak.fagsaksystem}</td>
+                                <td>{journalpost.sak.tema}</td>
+                                <td>{journalpost.avsenderMottaker.navn}</td>
+                                <td>{journalpost.journalstatus}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Container>
+        );
+    } else {
+        return <></>;
+    }
 };
 
 export default JournalpostListe;
