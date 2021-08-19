dc5c37c49643b1f4f9840049657da24decf6d85b
Nina Kjekstad <nina.kjekstad@nav.no>
2021-08-19 12:00:07 +0200

    Fjern totrinnskontroll som dukker opp i bakgrunnen når totrinn er ferdig

--- src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/Hendelsesoversikt.tsx
+++ src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/Hendelsesoversikt.tsx
@@ -47,7 +47,7 @@ const Hendelsesoversikt = ({
                     settAktivTab={settAktivTab}
                     skalViseTotrinnskontroll={skalViseTotrinnskontroll}
                 />
-                {aktivTab === Tabs.Totrinnskontroll && (
+                {aktivTab === Tabs.Totrinnskontroll && skalViseTotrinnskontroll && (
                     <Totrinnskontroll fagsak={fagsak} åpenBehandling={åpenBehandling} />
                 )}
                 {aktivTab === Tabs.Historikk && hendelser.length > 0 && (
