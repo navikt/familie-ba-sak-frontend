97c7195f5523eca98b483798c112623ccde16e7b
Henning Håkonsen <henning.hakonsen@nav.no>
2021-09-16 13:36:52 +0200

    Mulig fiks for feil i prod.

--- src/frontend/utils/kalender/io.ts
+++ src/frontend/utils/kalender/io.ts
@@ -6,16 +6,10 @@ import { capString } from './utils';
 
 export const erIsoStringGyldig = (dato?: FamilieIsoDate): boolean => {
     if (!dato) return false;
+    else if (!dato.includes('-')) return false;
 
-    return dayjs(dato, datoformat.ISO_DAG).isValid();
-};
-
-export const parseIso8601String = (dato: FamilieIsoDate): DagMånedÅr => {
-    const dayjsDato = dayjs(dato, datoformat.ISO_DAG);
-
-    // Dayjs brukes ikke til noe annet enn å fange ugyldige inputs
-    if (!dayjsDato.isValid()) {
-        throw new Error(`Dato er ugyldig`);
+    if (dayjs(dato, datoformat.ISO_DAG).isValid()) {
+        return true;
     }
 
     const år: number = parseInt(dato.substr(0, 4), 10);
@@ -23,15 +17,42 @@ export const parseIso8601String = (dato: FamilieIsoDate): DagMånedÅr => {
     const dag: number = parseInt(dato.substr(8, 10), 10);
 
     if (år < 1800 || år > 2500) {
-        throw new Error(`År fra dato er '${år}' og er sannsynligvis feil`);
+        return false;
     }
 
     if (måned < 1 || måned > 12) {
-        throw new Error(`Måned fra dato er '${måned}' og er sannsynligvis feil`);
+        return false;
     }
 
     if (dag < 1 || dag > antallDagerIMåned({ år, måned: måned - 1 })) {
-        throw new Error(`Dag fra dato er '${dag}' og er sannsynligvis feil`);
+        return false;
+    }
+
+    return true;
+};
+
+export const parseIso8601String = (dato: FamilieIsoDate): DagMånedÅr => {
+    const dayjsDato = dayjs(dato, datoformat.ISO_DAG);
+
+    // Dayjs brukes ikke til noe annet enn å fange ugyldige inputs
+    if (!dayjsDato.isValid()) {
+        throw new Error(`Dato '${dato}' er ugyldig`);
+    }
+
+    const år: number = parseInt(dato.substr(0, 4), 10);
+    const måned: number = parseInt(dato.substr(5, 7), 10);
+    const dag: number = parseInt(dato.substr(8, 10), 10);
+
+    if (år < 1800 || år > 2500) {
+        throw new Error(`År fra dato '${dato}' er '${år}' og er sannsynligvis feil`);
+    }
+
+    if (måned < 1 || måned > 12) {
+        throw new Error(`Måned fra dato '${dato}' er '${måned}' og er sannsynligvis feil`);
+    }
+
+    if (dag < 1 || dag > antallDagerIMåned({ år, måned: måned - 1 })) {
+        throw new Error(`Dag fra dato '${dato}' er '${dag}' og er sannsynligvis feil`);
     }
 
     return {
