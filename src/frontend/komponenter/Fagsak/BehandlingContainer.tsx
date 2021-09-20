97a1dde34d4d530ae03ce2619f0c6813ff6edb69
halvorbmundal <halvorbmundal@users.noreply.github.com>
2021-09-20 12:14:12 +0200

    Feature/endre utbetalingsperiode (#1429)
    * Legg til skjema for å endre utbetalingsperiode og knapp for å vise det.
    
    * Fix toggle
    
    * Endre når periodeSkalUtbetalesTilSøker er valgt
    
    * Endre navn på styled component
    
    * Legg til Ferdig og Avbrytknapp
    
    * Endre styling
    
    * Gjør Endre utbetalingsperiode mini
    
    * Bruk felter direkte i skjemahooken og ikke lage egne variabler.
    
    * Bruk OptionType som type framfor "{ label: string; value: string }"
    
    * Endre skjemanavn
    
    * Lag EndreUtbetalinsperiodeContext

--- src/frontend/komponenter/Fagsak/BehandlingContainer.tsx
+++ src/frontend/komponenter/Fagsak/BehandlingContainer.tsx
@@ -7,6 +7,7 @@ import AlertStripe from 'nav-frontend-alertstriper';
 import { RessursStatus } from '@navikt/familie-typer';
 
 import { useBehandling } from '../../context/behandlingContext/BehandlingContext';
+import { EndreUtbetalingsperiodePriovider } from '../../context/EndreUtbetalinsperiodeContext';
 import { SimuleringProvider } from '../../context/SimuleringContext';
 import { SøknadProvider } from '../../context/SøknadContext';
 import { TidslinjeProvider } from '../../context/TidslinjeContext';
@@ -93,10 +94,12 @@ const BehandlingContainer: React.FunctionComponent<IProps> = ({ fagsak }) => {
                         render={() => {
                             return (
                                 <TidslinjeProvider>
-                                    <TilkjentYtelse
-                                        fagsak={fagsak}
-                                        åpenBehandling={åpenBehandling.data}
-                                    />
+                                    <EndreUtbetalingsperiodePriovider>
+                                        <TilkjentYtelse
+                                            fagsak={fagsak}
+                                            åpenBehandling={åpenBehandling.data}
+                                        />
+                                    </EndreUtbetalingsperiodePriovider>
                                 </TidslinjeProvider>
                             );
                         }}
