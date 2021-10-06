b5e57d52219e375cf21f244b9b52419a865b2145
André Wolden <wolden.andre@gmail.com>
2021-10-06 11:25:23 +0200

    returnerer alle featuretoggles true

--- src/backend/cypress-server.ts
+++ src/backend/cypress-server.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 
+import bodyParser from 'body-parser';
 import { Express, Request, Response } from 'express';
 import expressStaticGzip from 'express-static-gzip';
 
 import { logInfo } from '@navikt/familie-logging';
 
-import { fagsakMock, featureMock, oppgaveMock, personMock, profileMock } from './mock-data';
+import { fagsakMock, oppgaveMock, personMock, profileMock } from './mock-data';
 
 // eslint-disable-next-line
 const express = require('express');
@@ -15,9 +16,16 @@ const port = 8000;
 const app: Express = express();
 
 app.use('/assets', expressStaticGzip(path.join(process.cwd(), 'frontend_production'), {}));
+app.use(bodyParser.json());
 
-app.post('/familie-ba-sak/api/feature', (_, res) => {
-    res.status(200).send(featureMock);
+app.post('/familie-ba-sak/api/feature', (req, res) => {
+    return res.status(200).send({
+        data: req.body.reduce((prev: object, curr: string) => ({ ...prev, [curr]: true }), {}),
+        status: 'SUKSESS',
+        melding: 'Innhenting av data var vellykket',
+        frontendFeilmelding: null,
+        stacktrace: null,
+    });
 });
 app.post('/familie-ba-sak/api/oppgave/hent-oppgaver', (_, res) => {
     res.status(200).send(oppgaveMock);
@@ -31,7 +39,6 @@ app.get('/familie-ba-sak/api/person', (_, res) => {
 app.get('/user/profile', (_, res) => {
     res.status(200).send(profileMock);
 });
-
 app.get('*', (_: Request, res: Response) => {
     res.sendFile('index.html', { root: path.join(process.cwd(), 'frontend_production') });
 });
