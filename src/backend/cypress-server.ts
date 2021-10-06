78a69b2af305b54cec76d39af9e7f90ca3177ce1
André Wolden <wolden.andre@gmail.com>
2021-10-06 13:10:10 +0200

    fjerner endrepunkt for /feature i cypress-server

--- src/backend/cypress-server.ts
+++ src/backend/cypress-server.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 
-import bodyParser from 'body-parser';
 import { Express, Request, Response } from 'express';
 import expressStaticGzip from 'express-static-gzip';
 
@@ -16,17 +15,7 @@ const port = 8000;
 const app: Express = express();
 
 app.use('/assets', expressStaticGzip(path.join(process.cwd(), 'frontend_production'), {}));
-app.use(bodyParser.json());
 
-app.post('/familie-ba-sak/api/feature', (req, res) => {
-    return res.status(200).send({
-        data: req.body.reduce((prev: object, curr: string) => ({ ...prev, [curr]: true }), {}),
-        status: 'SUKSESS',
-        melding: 'Innhenting av data var vellykket',
-        frontendFeilmelding: null,
-        stacktrace: null,
-    });
-});
 app.post('/familie-ba-sak/api/oppgave/hent-oppgaver', (_, res) => {
     res.status(200).send(oppgaveMock);
 });
