1f90fad2ec0c8fba8cf8264bfb6b2054ec90598c
André Wolden <wolden.andre@gmail.com>
2021-10-21 10:46:38 +0200

    endrer vilkar tabell ihht skisser (#1540)
    * endrer vilkar tabell ihht skisser
    
    * eos toggle på sammendragsrad på tabell per vilkårs periode
    
    * Fikser tabell summary rad mtp vurdert av kolonne
    
    * noe forbedret styling
    
    * tabell min-width
    
    * fjerner glemt console log

---
+++ src/frontend/utils/vilkår.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+
+import { Home, Globe } from '@navikt/ds-icons';
+
+import { Regelverk, VilkårType } from '../typer/vilkår';
+
+export const erIkkeGenereltVilkår = (vilkårType: VilkårType): boolean =>
+    [
+        VilkårType.BOR_MED_SØKER,
+        VilkårType.BOSATT_I_RIKET,
+        VilkårType.LOVLIG_OPPHOLD,
+        VilkårType.UTVIDET_BARNETRYGD,
+    ].includes(vilkårType);
+
+export const alleRegelverk: Record<Regelverk, { tekst: string; symbol: ReactNode }> = {
+    [Regelverk.NASJONALE_REGLER]: {
+        tekst: 'Nasjonale regler',
+        symbol: <Home width={24} height={24} viewBox={'0 0 24 24'} />,
+    },
+    [Regelverk.EØS_FORORDNINGEN]: {
+        tekst: 'EØS-forordningen',
+        symbol: <Globe width={24} height={24} viewBox={'0 0 24 24'} />,
+    },
+};
