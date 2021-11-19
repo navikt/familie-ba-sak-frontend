8d75922485e38fcf693259bff6782c66f563c785
Henning Håkonsen <henning.hakonsen@nav.no>
2021-11-19 08:33:28 +0100

    Legger inn ny behandlingsårsak og lar SB kunne velge denne om toggle er påskrudd.

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -5,6 +5,7 @@ export interface IToggles {
 export enum ToggleNavn {
     visTekniskOpphør = 'familie-ba-sak.behandling.vis-teknisk-opphoer', // Deprecated, bruk kanBehandleTekniskEndring istedet.
     kanBehandleTekniskEndring = 'familie-ba-sak.behandling.teknisk-endring',
+    kanBehandleSmåbarnstillegg = 'familie-ba-sak.behandling.smaabarnstillegg',
     tilbakekreving = 'familie-ba-sak.behandling.tilbakekreving',
     brukErDeltBosted = 'familie-ba-sak.behandling.delt_bosted',
     brukBegrunnelserFraSanity = 'familie-ba-sak.behandling.begrunnelse-fra-sanity',
