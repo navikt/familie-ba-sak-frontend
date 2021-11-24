c0b65d029eecf936484a3b82c99abdeb38514963
halvorbmundal <halvorbmundal@users.noreply.github.com>
2021-11-24 15:45:50 +0100

    Azure config (#1636)
    * Azure config

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
-if (process.env.NODE_ENV === 'production') {
-    dotenv.config({ path: '/var/run/secrets/nais.io/vault/.env' });
-} else {
-    dotenv.config();
-}
+dotenv.config();
+
+// felles-backend bruker andre variabler enn det som blir satt opp av azureAd
+const settAzureAdPropsFraEnv = () => {
+    process.env.AAD_DISCOVERY_URL = process.env.AZURE_APP_WELL_KNOWN_URL;
+    process.env.CLIENT_ID = process.env.AZURE_APP_CLIENT_ID;
+    process.env.CLIENT_SECRET = process.env.AZURE_APP_CLIENT_SECRET;
+};
 
 export const konfigurerAzure = () => {
     const host = 'barnetrygd';
@@ -14,24 +17,17 @@ export const konfigurerAzure = () => {
             process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
             process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             break;
-        case 'e2e':
-            process.env.AAD_LOGOUT_REDIRECT_URL = `http://mock-oauth2-server:1111/v2.0/logout?post_logout_redirect_uri=http:\\\\localhost:8000`;
-            process.env.AAD_REDIRECT_URL =
-                'http://familie-ba-sak-frontend:8000/auth/openid/callback';
-            process.env.AAD_DISCOVERY_URL = `http://mock-oauth2-server:1111/v2.0/.well-known/openid-configuration`;
-            process.env.GRAPH_API = 'http://familie-mock-server:1337/graph/me';
-            break;
         case 'preprod':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.dev.adeo.no`;
             process.env.AAD_REDIRECT_URL = `https://${host}.dev.adeo.no/auth/openid/callback`;
-            process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
             process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
+            settAzureAdPropsFraEnv();
             break;
         case 'production':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.nais.adeo.no`;
             process.env.AAD_REDIRECT_URL = `https://${host}.nais.adeo.no/auth/openid/callback`;
-            process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
             process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
+            settAzureAdPropsFraEnv();
             break;
         default:
             break;
