fd30136a273b3ce21b088ae5c52a52b502dc8fd8
Henning Håkonsen <henning.hakonsen@nav.no>
2021-12-20 11:16:13 +0100

    Endrer til riktig callid navn.

--- src/backend/proxy.ts
+++ src/backend/proxy.ts
@@ -118,7 +118,7 @@ export const doPdfProxy: any = () => {
 export const attachToken = (authClient: Client) => {
     return async (req: Request, _res: Response, next: NextFunction) => {
         getOnBehalfOfAccessToken(authClient, req, oboConfig).then((accessToken: string) => {
-            req.headers['Nav-Call-Id'] = uuidv4();
+            req.headers['x_callId'] = uuidv4();
             req.headers['Nav-Consumer-Id'] = 'familie-ba-sak-front';
             req.headers.Authorization = `Bearer ${accessToken}`;
             return next();
