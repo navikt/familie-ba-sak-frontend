40105a299f80659757c9e978b5e85afecec8795f
Henning Håkonsen <henning.hakonsen@nav.no>
2021-12-21 16:15:38 +0100

    Sjekker for både fss og gcp hostnavn. (#1718)

--- src/frontend/utils/miljø.ts
+++ src/frontend/utils/miljø.ts
@@ -1,3 +1,5 @@
-export const erProd = () => window.location.host === 'barnetrygd.nais.adeo.no';
+export const erProd = () =>
+    window.location.host === 'barnetrygd.nais.adeo.no' ||
+    window.location.host === 'barnetrygd.intern.nav.no';
 
 export const hentSideHref = (pathname: string) => pathname.split('/')[4];
