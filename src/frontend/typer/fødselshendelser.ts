0cfa7797bae54e91032187965f98abfa4fcd0da8
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-02-14 08:14:16 +0100

    Oppdater tekst for filtreringsregel

--- src/frontend/typer/fødselshendelser.ts
+++ src/frontend/typer/fødselshendelser.ts
@@ -27,5 +27,6 @@ export const filtreringsregler: Record<Filtreringsregel, string> = {
     MOR_ER_OVER_18_ÅR: 'Mor er over 18 år',
     MOR_HAR_IKKE_VERGE: 'Mor har ikke verge',
     MOR_MOTTAR_IKKE_LØPENDE_UTVIDET: 'Mor mottar ikke utvidet barnetrygd',
-    LØPER_IKKE_BARNETRYGD_FOR_BARNET: 'Ikke utbetalt barnetrygd for barnet til annen mottaker',
+    LØPER_IKKE_BARNETRYGD_FOR_BARNET:
+        'Det er ikke utbetalt barnetrygd for barnet til annen mottaker',
 };
