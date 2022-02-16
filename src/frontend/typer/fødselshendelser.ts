92f152aaf234900c9c7963fa5a024e915324a673
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-02-16 15:43:24 +0100

    Legg til filtreringsregel LØPER_IKKE_BARNETRYGD_FOR_BARNET (#1856)
    * Legg til filtreringsregel LØPER_IKKE_BARNETRYGD_FOR_BARNET
    
    * Oppdater tekst for filtreringsregel

--- src/frontend/typer/fødselshendelser.ts
+++ src/frontend/typer/fødselshendelser.ts
@@ -15,6 +15,7 @@ export enum Filtreringsregel {
     MOR_ER_OVER_18_ÅR = 'MOR_ER_OVER_18_ÅR',
     MOR_HAR_IKKE_VERGE = 'MOR_HAR_IKKE_VERGE',
     MOR_MOTTAR_IKKE_LØPENDE_UTVIDET = 'MOR_MOTTAR_IKKE_LØPENDE_UTVIDET',
+    LØPER_IKKE_BARNETRYGD_FOR_BARNET = 'LØPER_IKKE_BARNETRYGD_FOR_BARNET',
 }
 
 export const filtreringsregler: Record<Filtreringsregel, string> = {
@@ -26,4 +27,6 @@ export const filtreringsregler: Record<Filtreringsregel, string> = {
     MOR_ER_OVER_18_ÅR: 'Mor er over 18 år',
     MOR_HAR_IKKE_VERGE: 'Mor har ikke verge',
     MOR_MOTTAR_IKKE_LØPENDE_UTVIDET: 'Mor mottar ikke utvidet barnetrygd',
+    LØPER_IKKE_BARNETRYGD_FOR_BARNET:
+        'Det er ikke utbetalt barnetrygd for barnet til annen mottaker',
 };
