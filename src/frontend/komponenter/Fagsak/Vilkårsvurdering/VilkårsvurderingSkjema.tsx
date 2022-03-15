fd6eaa129ee97a6eef8cc0824168d75b1bf9280b
Henning Håkonsen <henning.hakonsen@nav.no>
2022-03-15 14:06:59 +0100

    Åpner opp for å kunne legge til utvidet vilkår på flere behandlingsårsaker. (#1961)

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/VilkårsvurderingSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/VilkårsvurderingSkjema.tsx
@@ -7,7 +7,7 @@ import AlertStripe from 'nav-frontend-alertstriper';
 
 import { AddCircle } from '@navikt/ds-icons';
 import type { FeltState } from '@navikt/familie-skjema';
-import { RessursStatus } from '@navikt/familie-typer';
+import { hentDataFraRessurs, RessursStatus } from '@navikt/familie-typer';
 import type { Ressurs } from '@navikt/familie-typer';
 
 import { useBehandling } from '../../../context/behandlingContext/BehandlingContext';
@@ -17,6 +17,7 @@ import {
 } from '../../../context/Vilkårsvurdering/VilkårsvurderingContext';
 import FamilieChevron from '../../../ikoner/FamilieChevron';
 import type { IBehandling } from '../../../typer/behandling';
+import { BehandlingÅrsak } from '../../../typer/behandling';
 import { PersonType } from '../../../typer/person';
 import type { IPersonResultat, IVilkårConfig, IVilkårResultat } from '../../../typer/vilkår';
 import { vilkårConfig, Resultat, annenVurderingConfig, VilkårType } from '../../../typer/vilkår';
@@ -63,8 +64,18 @@ const VilkårsvurderingSkjema: React.FunctionComponent<IVilkårsvurderingSkjema>
     visFeilmeldinger,
 }) => {
     const { vilkårsvurdering, settVilkårSubmit, postVilkår } = useVilkårsvurdering();
-    const { erLesevisning, erMigreringsbehandling, settÅpenBehandling, aktivSettPåVent } =
-        useBehandling();
+    const {
+        erLesevisning,
+        erMigreringsbehandling,
+        settÅpenBehandling,
+        aktivSettPåVent,
+        åpenBehandling,
+    } = useBehandling();
+
+    const kanLeggeTilUtvidetVilkår =
+        erMigreringsbehandling ||
+        hentDataFraRessurs(åpenBehandling)?.årsak === BehandlingÅrsak.KORREKSJON_VEDTAKSBREV ||
+        hentDataFraRessurs(åpenBehandling)?.årsak === BehandlingÅrsak.TEKNISK_ENDRING;
 
     const personHarIkkevurdertVilkår = (personResultat: IPersonResultat) =>
         personResultat.vilkårResultater.some(
@@ -123,7 +134,7 @@ const VilkårsvurderingSkjema: React.FunctionComponent<IVilkårsvurderingSkjema>
                                 personErEkspandert[personResultat.personIdent] &&
                                 personResultat.person.type === PersonType.SØKER &&
                                 !harUtvidet &&
-                                erMigreringsbehandling && (
+                                kanLeggeTilUtvidetVilkår && (
                                     <VilkårDiv>
                                         <IkonKnapp
                                             erLesevisning={erLesevisning()}
