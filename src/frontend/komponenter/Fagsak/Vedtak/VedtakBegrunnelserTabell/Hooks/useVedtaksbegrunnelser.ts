db2d5643b72812ddfaaaa3e511dfa7f3e4d8dec8
Saikat Halder <saikat.halder@nav.no>
2022-03-21 11:41:07 +0100

    Endret slik at Reduksjonsperiode kan vise alle andre begrunnelser også bortsett fra Fortsatt Innvilget
    Signed-off-by: Saikat Halder <saikat.halder@nav.no>

--- src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/Hooks/useVedtaksbegrunnelser.ts
+++ src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/Hooks/useVedtaksbegrunnelser.ts
@@ -47,7 +47,12 @@ export const useVilkårBegrunnelser = ({
             case Vedtaksperiodetype.FORTSATT_INNVILGET:
                 return [VedtakBegrunnelseType.FORTSATT_INNVILGET];
             case Vedtaksperiodetype.REDUKSJON:
-                return [VedtakBegrunnelseType.REDUKSJON, VedtakBegrunnelseType.INNVILGET];
+                return [
+                    VedtakBegrunnelseType.REDUKSJON,
+                    VedtakBegrunnelseType.INNVILGET,
+                    VedtakBegrunnelseType.ETTER_ENDRET_UTBETALING,
+                    Vedtaksperiodetype.ENDRET_UTBETALING,
+                ];
             case Vedtaksperiodetype.OPPHØR:
                 return [VedtakBegrunnelseType.OPPHØR];
             case Vedtaksperiodetype.ENDRET_UTBETALING:
