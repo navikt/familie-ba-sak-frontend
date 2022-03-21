3e9654d7c6436a50c536ec59b53696811802e538
halvormundal <halvorbmundal@gmail.com>
2022-03-21 12:27:52 +0100

    Disable sett på vent dersom behandlingen er sendt til beslutter

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/SettEllerOppdaterVenting.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/SettEllerOppdaterVenting.tsx
@@ -6,6 +6,7 @@ import type { Ressurs } from '@navikt/familie-typer';
 
 import { useBehandling } from '../../../../../context/behandlingContext/BehandlingContext';
 import type { IBehandling } from '../../../../../typer/behandling';
+import { BehandlingSteg, hentStegNummer } from '../../../../../typer/behandling';
 import { SettBehandlingPåVentModal } from './SettBehandlingPåVentModal';
 import { useSettPåVentSkjema } from './useSettPåVentSkjema';
 
@@ -40,6 +41,9 @@ const SettEllerOppdaterVenting: React.FC<IProps> = ({ onListElementClick, behand
         }
     };
 
+    const behandlingErSendtTilBeslutter =
+        hentStegNummer(behandling.steg) > hentStegNummer(BehandlingSteg.SEND_TIL_BESLUTTER);
+
     return (
         <>
             <KnappBase
@@ -48,6 +52,7 @@ const SettEllerOppdaterVenting: React.FC<IProps> = ({ onListElementClick, behand
                     settVisModal(true);
                     onListElementClick();
                 }}
+                disabled={behandlingErSendtTilBeslutter}
             >
                 {erBehandlingAlleredePåVent
                     ? 'Endre ventende behandling'
