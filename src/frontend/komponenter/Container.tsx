295ac2ac16e3ebe6d253fe55869eb2c656a6c490
Henning Håkonsen <henning.hakonsen@nav.no>
2022-03-25 09:57:41 +0100

    Feature/poc forvaltningstidslinje (#1988)
    * Init tidslinje-poc
    
    * WIP tidslinjer per barn. Labels på vilkår er ikke tilkoblet riktige kjeder enda.
    
    * Match label mot riktig tidslinje.
    
    * Skriver om til å bruke generics på tidslinjeperiode.
    
    Co-authored-by: Geir Roger Moen <geir.roger.moen@nav.no>

--- src/frontend/komponenter/Container.tsx
+++ src/frontend/komponenter/Container.tsx
@@ -7,11 +7,13 @@ import { useApp } from '../context/AppContext';
 import { BehandlingProvider } from '../context/behandlingContext/BehandlingContext';
 import { FagsakProvider } from '../context/FagsakContext';
 import { Oppgaver } from '../context/OppgaverContext';
+import { TidslinjeProvider } from '../context/TidslinjeContext';
 import FagsakContainer from './Fagsak/FagsakContainer';
 import { HeaderMedSøk } from './Felleskomponenter/HeaderMedSøk/HeaderMedSøk';
 import UgyldigSesjon from './Felleskomponenter/Modal/SesjonUtløpt';
 import UIModalWrapper from './Felleskomponenter/Modal/UIModalWrapper';
 import SystemetLaster from './Felleskomponenter/SystemetLaster/SystemetLaster';
+import TidslinjeVisualisering from './Felleskomponenter/TidslinjeVisualisering/TidslinjeVisualisering';
 import Toasts from './Felleskomponenter/Toast/Toasts';
 import { Infotrygd } from './Infotrygd/Infotrygd';
 import Internstatistikk from './Internstatistikk';
@@ -45,6 +47,15 @@ const Container: React.FC = () => {
                                     <Route exact={true} path="/oppgaver" component={Oppgaver} />
                                     <Route path="/internstatistikk" component={Internstatistikk} />
                                     <Route path="/infotrygd" component={Infotrygd} />
+                                    <Route
+                                        exact={true}
+                                        path="/tidslinjer/:behandlingId"
+                                        render={() => (
+                                            <TidslinjeProvider>
+                                                <TidslinjeVisualisering />
+                                            </TidslinjeProvider>
+                                        )}
+                                    />
                                     <Redirect to="/oppgaver" />
                                 </Switch>
                             </BehandlingProvider>
