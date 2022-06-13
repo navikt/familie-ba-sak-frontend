5898ac57138be538236aa08c502851c4fb95ddb5
Tor Nærland <tor.nerland@nav.no>
2022-06-13 11:36:46 +0200

    Modal for bekreftelse av ny fagsak

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettFagsak/OpprettFagsak.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettFagsak/OpprettFagsak.tsx
@@ -1,27 +1,121 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import styled from 'styled-components';
+
+import { Flatknapp, Knapp } from 'nav-frontend-knapper';
 import KnappBase from 'nav-frontend-knapper';
+import { Feilmelding, Normaltekst } from 'nav-frontend-typografi';
 
+import { useApp } from '../../../../../context/AppContext';
 import type { IMinimalFagsak } from '../../../../../typer/fagsak';
 import { FagsakEier } from '../../../../../typer/fagsak';
+import useOpprettFagsak from '../../../../Felleskomponenter/HeaderMedSøk/useOpprettFagsak';
 
 interface IProps {
     onListElementClick: () => void;
     minimalFagsak: IMinimalFagsak;
 }
 
+const StyledDiv = styled.div`
+    margin-bottom: 2rem;
+`;
+
+const bekreftNyFagsakModal = ({
+    nyFagsakEier,
+    feilmelding,
+    onClick,
+    onClose,
+}: {
+    nyFagsakEier: FagsakEier;
+    feilmelding: string;
+    onClick: () => void;
+    onClose: () => void;
+}) => {
+    return {
+        tittel:
+            nyFagsakEier === FagsakEier.BARN
+                ? 'Opprett fagsak på institusjon eller enslig mindreårig'
+                : 'Opprett ordinær fagsak',
+        lukkKnapp: true,
+        visModal: true,
+        onClose: onClose,
+        innhold: () => {
+            return (
+                <StyledDiv>
+                    <Normaltekst>Ønsker du å opprette ny fagsak av denne typen?</Normaltekst>
+                    {!!feilmelding && <Feilmelding children={feilmelding} />}
+                </StyledDiv>
+            );
+        },
+        actions: [
+            <Flatknapp key={'avbryt'} onClick={onClose} mini={true}>
+                Avbryt
+            </Flatknapp>,
+            <Knapp
+                key={'bekreft'}
+                type={'hoved'}
+                mini={true}
+                onClick={onClick}
+                children={'Bekreft'}
+            />,
+        ],
+    };
+};
+
 const OpprettFagsak: React.FC<IProps> = ({ onListElementClick, minimalFagsak }) => {
+    const { opprettFagsak, feilmelding } = useOpprettFagsak();
+    const { modal, settModal, lukkModal } = useApp();
+    const [visFeilmelding, settVisFeilmelding] = React.useState(false);
+    const nyFagsakEier =
+        minimalFagsak.fagsakEier === FagsakEier.BARN ? FagsakEier.OMSORGSPERSON : FagsakEier.BARN;
+
+    const opprettNyFagsak = () => {
+        opprettFagsak(
+            {
+                personIdent: minimalFagsak.søkerFødselsnummer,
+                aktørId: null,
+                fagsakEier: nyFagsakEier,
+            },
+            lukkModal
+        );
+        settVisFeilmelding(true);
+    };
+
+    const onClose = () => {
+        lukkModal();
+        settVisFeilmelding(false);
+    };
+
+    useEffect(() => {
+        if (modal.visModal) {
+            settModal(
+                bekreftNyFagsakModal({
+                    nyFagsakEier,
+                    feilmelding,
+                    onClick: opprettNyFagsak,
+                    onClose: onClose,
+                })
+            );
+        }
+    }, [feilmelding, visFeilmelding]);
+
     return (
         <>
             <KnappBase
                 mini={true}
                 onClick={() => {
                     onListElementClick();
+                    settModal(
+                        bekreftNyFagsakModal({
+                            nyFagsakEier,
+                            feilmelding: '',
+                            onClick: opprettNyFagsak,
+                            onClose: onClose,
+                        })
+                    );
                 }}
             >
-                {minimalFagsak.fagsakEier === FagsakEier.BARN
-                    ? 'Opprett normal fagsak'
-                    : 'Opprett institusjon fagsak'}
+                Opprett ny fagsak
             </KnappBase>
         </>
     );
