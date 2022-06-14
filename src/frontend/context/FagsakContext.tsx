44ddca0893d76feff257c3cbc8ba1928e081b8b7
Meng Zhu <meng.zhu@nav.no>
2022-06-14 15:16:36 +0200

    hent bruker nå opprett fagsak

--- src/frontend/context/FagsakContext.tsx
+++ src/frontend/context/FagsakContext.tsx
@@ -156,6 +156,7 @@ const [FagsakProvider, useFagsakRessurser] = createUseContext(() => {
         internstatistikk,
         minimalFagsak,
         settMinimalFagsak,
+        hentBruker,
     };
 });
 
--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettFagsak/OpprettFagsak.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettFagsak/OpprettFagsak.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import KnappBase from 'nav-frontend-knapper';
 
+import { useFagsakRessurser } from '../../../../../context/FagsakContext';
 import type { IMinimalFagsak } from '../../../../../typer/fagsak';
 import { FagsakEier } from '../../../../../typer/fagsak';
+import useOpprettFagsak from '../../../../Felleskomponenter/HeaderMedSøk/useOpprettFagsak';
 
 interface IProps {
     onListElementClick: () => void;
@@ -11,12 +13,30 @@ interface IProps {
 }
 
 const OpprettFagsak: React.FC<IProps> = ({ onListElementClick, minimalFagsak }) => {
+    const { opprettFagsak, senderInn, settSenderInn } = useOpprettFagsak();
+    const { hentBruker } = useFagsakRessurser();
     return (
         <>
             <KnappBase
                 mini={true}
+                spinner={!!senderInn}
                 onClick={() => {
+                    const eier =
+                        minimalFagsak.fagsakEier === FagsakEier.OMSORGSPERSON
+                            ? FagsakEier.BARN
+                            : FagsakEier.OMSORGSPERSON;
+                    settSenderInn(eier);
                     onListElementClick();
+                    opprettFagsak(
+                        {
+                            personIdent: minimalFagsak.søkerFødselsnummer,
+                            aktørId: null,
+                            fagsakEier: eier,
+                        },
+                        () => {
+                            hentBruker(minimalFagsak.søkerFødselsnummer);
+                        }
+                    );
                 }}
             >
                 {minimalFagsak.fagsakEier === FagsakEier.BARN
