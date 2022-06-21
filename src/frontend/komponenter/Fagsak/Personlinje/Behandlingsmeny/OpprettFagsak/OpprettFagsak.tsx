11c6177ece3ee039d41327d473eae7ae9ea5c9df
tnarland <47184872+tnarland@users.noreply.github.com>
2022-06-21 12:30:43 +0200

    Opprett fagsak for verge via behandlingsmeny (#2159)
    Legger inn mulighet til å opprette ny fagsak via behandlingsmenyen på saksoversiktsiden, med fagsakeier satt til motsatt verdi av den eksisterende. Skjuler menyvalget hvis personen allerede har 2 fagsaker med forskjellig fagsakeier.
    
    Co-authored-by: Meng Zhu <meng.zhu@nav.no>

---
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettFagsak/OpprettFagsak.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react';
+
+import styled from 'styled-components';
+
+import { Flatknapp, Knapp } from 'nav-frontend-knapper';
+import KnappBase from 'nav-frontend-knapper';
+import { Feilmelding, Normaltekst } from 'nav-frontend-typografi';
+
+import { useApp } from '../../../../../context/AppContext';
+import { useFagsakRessurser } from '../../../../../context/FagsakContext';
+import type { IMinimalFagsak } from '../../../../../typer/fagsak';
+import { FagsakEier } from '../../../../../typer/fagsak';
+import useOpprettFagsak from '../../../../Felleskomponenter/HeaderMedSøk/useOpprettFagsak';
+
+interface IProps {
+    onListElementClick: () => void;
+    minimalFagsak: IMinimalFagsak;
+}
+
+const StyledDiv = styled.div`
+    margin-bottom: 2rem;
+`;
+
+const bekreftNyFagsakModal = ({
+    nyFagsakEier,
+    feilmelding,
+    onClick,
+    onClose,
+}: {
+    nyFagsakEier: FagsakEier;
+    feilmelding: string;
+    onClick: () => void;
+    onClose: () => void;
+}) => {
+    return {
+        tittel:
+            nyFagsakEier === FagsakEier.BARN
+                ? 'Opprett fagsak på institusjon eller enslig mindreårig'
+                : 'Opprett standard fagsak',
+        lukkKnapp: true,
+        visModal: true,
+        onClose: onClose,
+        innhold: () => {
+            return (
+                <StyledDiv>
+                    <Normaltekst>Ønsker du å opprette ny fagsak av denne typen?</Normaltekst>
+                    {!!feilmelding && <Feilmelding children={feilmelding} />}
+                </StyledDiv>
+            );
+        },
+        actions: [
+            <Flatknapp key={'avbryt'} onClick={onClose} mini={true}>
+                Avbryt
+            </Flatknapp>,
+            <Knapp
+                key={'bekreft'}
+                type={'hoved'}
+                mini={true}
+                onClick={onClick}
+                children={'Bekreft'}
+            />,
+        ],
+    };
+};
+
+const OpprettFagsak: React.FC<IProps> = ({ onListElementClick, minimalFagsak }) => {
+    const { opprettFagsak, feilmelding } = useOpprettFagsak();
+    const { modal, settModal, lukkModal } = useApp();
+    const [visFeilmelding, settVisFeilmelding] = React.useState(false);
+    const { hentBruker } = useFagsakRessurser();
+    const nyFagsakEier =
+        minimalFagsak.fagsakEier === FagsakEier.BARN ? FagsakEier.OMSORGSPERSON : FagsakEier.BARN;
+
+    const opprettNyFagsak = () => {
+        opprettFagsak(
+            {
+                personIdent: minimalFagsak.søkerFødselsnummer,
+                aktørId: null,
+                fagsakEier: nyFagsakEier,
+            },
+            () => {
+                settVisFeilmelding(false);
+                hentBruker(minimalFagsak.søkerFødselsnummer);
+                lukkModal();
+            }
+        );
+        settVisFeilmelding(true);
+    };
+
+    const onClose = () => {
+        lukkModal();
+        settVisFeilmelding(false);
+    };
+
+    useEffect(() => {
+        if (modal.visModal) {
+            settModal(
+                bekreftNyFagsakModal({
+                    nyFagsakEier,
+                    feilmelding,
+                    onClick: opprettNyFagsak,
+                    onClose: onClose,
+                })
+            );
+        }
+    }, [feilmelding, visFeilmelding]);
+
+    return (
+        <>
+            <KnappBase
+                mini={true}
+                onClick={() => {
+                    onListElementClick();
+                    settModal(
+                        bekreftNyFagsakModal({
+                            nyFagsakEier,
+                            feilmelding: '',
+                            onClick: opprettNyFagsak,
+                            onClose: onClose,
+                        })
+                    );
+                }}
+            >
+                Opprett ny fagsak
+            </KnappBase>
+        </>
+    );
+};
+
+export default OpprettFagsak;
