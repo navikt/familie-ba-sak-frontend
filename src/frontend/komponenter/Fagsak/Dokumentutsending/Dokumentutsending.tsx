a0fdfb36f48816b7e75a6f18d343163abcb5bac9
norafutsaeter <nora.futseter@nav.no>
2022-10-25 14:03:00 +0200

    Ds modal for dokumentutsending

--- src/frontend/komponenter/Fagsak/Dokumentutsending/Dokumentutsending.tsx
+++ src/frontend/komponenter/Fagsak/Dokumentutsending/Dokumentutsending.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { Button } from '@navikt/ds-react';
+import { Button, Heading, Modal } from '@navikt/ds-react';
 import { RessursStatus } from '@navikt/familie-typer';
 
 import { useDokumentutsending } from '../../../context/DokumentutsendingContext';
 import { fagsakHeaderHøydeRem } from '../../../typer/styling';
-import UIModalWrapper from '../../Felleskomponenter/Modal/UIModalWrapper';
 import DokumentutsendingSkjema from './DokumentutsendingSkjema';
 
 const Container = styled.div`
@@ -16,6 +15,24 @@ const Container = styled.div`
     grid-template-columns: 35rem 1fr;
     grid-template-rows: 1fr;
     height: calc(100vh - ${fagsakHeaderHøydeRem}rem);
+`;
+
+const StyledModal = styled(Modal)`
+    padding: 2.5rem;
+    width: 35rem;
+`;
+
+const StyledModalHeader = styled(Heading)`
+    margin-bottom: 2rem;
+`;
+
+const KnappHøyre = styled(Button)`
+    margin-left: 1rem;
+`;
+
+const Knapperad = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
 const Dokumentutsending: React.FC = () => {
@@ -26,36 +43,35 @@ const Dokumentutsending: React.FC = () => {
 
     return (
         <Container>
-            {visInnsendtBrevModal && (
-                <UIModalWrapper
-                    modal={{
-                        tittel: 'Brevet er sendt',
-                        lukkKnapp: true,
-                        visModal: visInnsendtBrevModal,
-                        actions: [
-                            <Button
-                                key={'til oppgavebenken'}
-                                size="small"
-                                variant="secondary"
-                                onClick={() => {
-                                    navigate('/oppgaver');
-                                }}
-                                children={'Gå til oppgavebenken'}
-                            />,
-                            <Button
-                                key={'til dokumentoversikt'}
-                                size="small"
-                                variant="secondary"
-                                onClick={() => {
-                                    navigate(`/fagsak/${fagsakId}/dokumenter`);
-                                }}
-                                children={'Gå til Dokumentoversikt'}
-                            />,
-                        ],
-                        onClose: () => settVisInnsendtBrevModal(false),
-                    }}
-                />
-            )}
+            <StyledModal
+                open={visInnsendtBrevModal}
+                onClose={() => settVisInnsendtBrevModal(false)}
+            >
+                <StyledModalHeader size="medium" level={'2'}>
+                    Brevet er sendt
+                </StyledModalHeader>
+                <Knapperad>
+                    <Button
+                        variant={'secondary'}
+                        key={'til oppgavebenken'}
+                        size={'medium'}
+                        onClick={() => {
+                            navigate('/oppgaver');
+                        }}
+                        children={'Gå til oppgavebenken'}
+                    />
+                    <KnappHøyre
+                        variant={'secondary'}
+                        key={'til saksoversikt'}
+                        size={'medium'}
+                        onClick={() => {
+                            navigate(`/fagsak/${fagsakId}/saksoversikt`);
+                            settVisInnsendtBrevModal(false);
+                        }}
+                        children={'Gå til saksoversikt'}
+                    />
+                </Knapperad>
+            </StyledModal>
             <DokumentutsendingSkjema />
 
             <iframe
