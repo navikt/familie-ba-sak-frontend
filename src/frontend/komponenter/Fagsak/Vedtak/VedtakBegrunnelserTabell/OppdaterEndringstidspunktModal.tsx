5de88a8ba5276fc19e2638503f969e0d473c3783
Kristine Steine <kristine.steine@bekk.no>
2022-11-22 22:46:56 +0100

    Endre knapper hvis lesevisning

--- src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/OppdaterEndringstidspunktModal.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/VedtakBegrunnelserTabell/OppdaterEndringstidspunktModal.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 import { SkjemaGruppe } from 'nav-frontend-skjema';
 
-import { Button } from '@navikt/ds-react';
+import { Button, Heading, Modal } from '@navikt/ds-react';
 import type { ISkjema } from '@navikt/familie-skjema';
 import { RessursStatus } from '@navikt/familie-typer';
 
@@ -13,10 +13,22 @@ import type { IBehandling } from '../../../../typer/behandling';
 import type { FamilieIsoDate } from '../../../../utils/kalender';
 import { hentFrontendFeilmelding } from '../../../../utils/ressursUtils';
 import { FamilieDatovelgerWrapper } from '../../../../utils/skjema/FamilieDatovelgerWrapper';
-import UIModalWrapper from '../../../Felleskomponenter/Modal/UIModalWrapper';
 
 const Feltmargin = styled.div`
-    margin-bottom: 2rem;
+    margin: 2rem 0;
+`;
+
+const StyledModal = styled(Modal)`
+    width: 35rem;
+`;
+
+const KnappHøyre = styled(Button)`
+    margin-left: 1rem;
+`;
+
+const Knapperad = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
 interface IProps {
@@ -33,49 +45,56 @@ export const OppdaterEndringstidspunktModal: React.FC<IProps> = ({
     skjema,
 }) => {
     const { vurderErLesevisning } = useBehandling();
+    const erLesevisning = vurderErLesevisning();
     return (
-        <UIModalWrapper
-            modal={{
-                tittel: 'Oppdater endringstidspunkt',
-                visModal: visModal,
-                lukkKnapp: true,
-                onClose: onAvbryt,
-                actions: [
-                    <Button
-                        variant={'tertiary'}
-                        key={'Avbryt'}
-                        size={'small'}
-                        onClick={onAvbryt}
-                        children={'Avbryt'}
-                    />,
-                    <Button
-                        variant={'primary'}
-                        key={'Oppdater'}
-                        size={'small'}
-                        onClick={oppdaterEndringstidspunkt}
-                        children={'Oppdater'}
-                        loading={skjema.submitRessurs.status === RessursStatus.HENTER}
-                        disabled={skjema.submitRessurs.status === RessursStatus.HENTER}
-                    />,
-                ],
-            }}
-        >
-            <SkjemaGruppe
-                feil={hentFrontendFeilmelding(skjema.submitRessurs)}
-                utenFeilPropagering={true}
-            >
-                <Feltmargin>
-                    <FamilieDatovelgerWrapper
-                        {...skjema.felter.endringstidspunkt.hentNavInputProps(
-                            skjema.visFeilmeldinger
-                        )}
-                        valgtDato={skjema.felter.endringstidspunkt.verdi}
-                        label={'Endringstidspunkt'}
-                        placeholder={'DD.MM.ÅÅÅÅ'}
-                        erLesesvisning={vurderErLesevisning()}
-                    />
-                </Feltmargin>
-            </SkjemaGruppe>
-        </UIModalWrapper>
+        <StyledModal open={visModal} onClose={onAvbryt}>
+            <Modal.Content>
+                <Heading size="medium" level="2" spacing>
+                    Oppdater endringstidspunkt
+                </Heading>
+                <SkjemaGruppe
+                    feil={hentFrontendFeilmelding(skjema.submitRessurs)}
+                    utenFeilPropagering={true}
+                >
+                    <Feltmargin>
+                        <FamilieDatovelgerWrapper
+                            {...skjema.felter.endringstidspunkt.hentNavInputProps(
+                                skjema.visFeilmeldinger
+                            )}
+                            valgtDato={skjema.felter.endringstidspunkt.verdi}
+                            label={'Endringstidspunkt'}
+                            placeholder={'DD.MM.ÅÅÅÅ'}
+                            erLesesvisning={erLesevisning}
+                        />
+                    </Feltmargin>
+                </SkjemaGruppe>
+                <Knapperad>
+                    {erLesevisning ? (
+                        <Button variant="primary" key="Lukk" size="small">
+                            Lukk
+                        </Button>
+                    ) : (
+                        <>
+                            <Button
+                                variant={'tertiary'}
+                                key={'Avbryt'}
+                                size={'small'}
+                                onClick={onAvbryt}
+                                children={'Avbryt'}
+                            />
+                            <KnappHøyre
+                                variant={'primary'}
+                                key={'Oppdater'}
+                                size={'small'}
+                                onClick={oppdaterEndringstidspunkt}
+                                children={'Oppdater'}
+                                loading={skjema.submitRessurs.status === RessursStatus.HENTER}
+                                disabled={skjema.submitRessurs.status === RessursStatus.HENTER}
+                            />
+                        </>
+                    )}
+                </Knapperad>
+            </Modal.Content>
+        </StyledModal>
     );
 };
