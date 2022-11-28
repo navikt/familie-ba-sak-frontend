9d997aa3bc241c02a110a9fbbf88f2571a9e6d0b
sarahjelle <sara.hjelle@bekk.no>
2022-11-28 16:49:21 +0100

    Trigg modal ved trykk

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggTilEllerFjernBrevmottakere/LeggTilEllerFjernBrevmottakere.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggTilEllerFjernBrevmottakere/LeggTilEllerFjernBrevmottakere.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { Heading, Modal } from '@navikt/ds-react';
 import { Dropdown } from '@navikt/ds-react-internal';
 
 const LeggTilEllerFjernBrevmottakere: React.FC = () => {
+    const [visModal, settVisModal] = useState(false);
     return (
         <>
-            <Dropdown.Menu.List.Item>Legg til / fjern brevmottakere</Dropdown.Menu.List.Item>
+            <Dropdown.Menu.List.Item onClick={() => settVisModal(true)}>
+                Legg til / fjern brevmottakere
+            </Dropdown.Menu.List.Item>
+            <Modal
+                open={visModal}
+                aria-label="Legg til eller fjern brevmottakere"
+                onClose={() => settVisModal(false)}
+            >
+                <Modal.Content>
+                    <Heading spacing level="1" size="medium" id="modal-heading">
+                        Legg til eller fjern brevmottakere
+                    </Heading>
+                </Modal.Content>
+            </Modal>
         </>
     );
 };
