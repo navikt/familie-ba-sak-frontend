20c96a8dfc88ab5134e54e479a4f4281cf58bcff
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-01-02 14:48:55 +0100

    Rename erHeltall -> erPositivtHeltall

--- src/frontend/context/KorrigertEtterbetaling/ValideringKorrigertEtterbetaling.ts
+++ src/frontend/context/KorrigertEtterbetaling/ValideringKorrigertEtterbetaling.ts
@@ -14,12 +14,12 @@ export const erÅrsakForKorrigeringGyldig = (felt: FeltState<string>) => {
 };
 
 export const erEtterbetalingsbeløpGyldig = (felt: FeltState<string>) => {
-    return !isEmpty(felt.verdi) && erHeltall(felt.verdi)
+    return !isEmpty(felt.verdi) && erPositivtHeltall(felt.verdi)
         ? ok(felt)
         : feil(felt, 'Skriv inn etterbetalingsbeløp. Desimaltall støttes ikke.');
 };
 
-const erHeltall = (string: string) => {
+const erPositivtHeltall = (string: string) => {
     const tall = Number(string);
 
     return Number.isInteger(tall) && tall > 0;
