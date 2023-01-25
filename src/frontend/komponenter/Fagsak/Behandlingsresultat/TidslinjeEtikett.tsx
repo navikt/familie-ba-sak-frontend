ec30dea36e3068c2395dfe15e8c86e9c8ee4cef8
Kristine Steine <kristine.steine@bekk.no>
2023-01-25 15:29:25 +0100

    Bruk ny fargekoder i TidslinjeEtikett

--- src/frontend/komponenter/Fagsak/Behandlingsresultat/TidslinjeEtikett.tsx
+++ src/frontend/komponenter/Fagsak/Behandlingsresultat/TidslinjeEtikett.tsx
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 
 import styled from 'styled-components';
 
-import navFarger from 'nav-frontend-core';
-
+import { ABlue500, ABlue600, ABlue800, AGray300, AGray900 } from '@navikt/ds-tokens/dist/tokens';
 import type { Etikett } from '@navikt/familie-tidslinje';
 
 import { TidslinjeVindu, useTidslinje } from '../../../context/TidslinjeContext';
@@ -18,15 +17,15 @@ const EtikettKnapp = styled(FamilieBaseKnapp)<{ disabled: boolean; valgt: boolea
     width: 90%;
     text-align: left;
     cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
-    border-left: ${({ valgt }) => (valgt ? `1px solid ${navFarger.navGra20}` : 'none')};
+    border-left: ${({ valgt }) => (valgt ? `1px solid ${AGray300}` : 'none')};
 
     > span {
         text-decoration: ${({ disabled, valgt }) => (disabled || valgt ? 'none' : 'underline')};
         font-weight: ${({ valgt }) => (valgt ? 'bold' : 'normal')};
         color: ${({ disabled, valgt }) => {
-            if (disabled) return navFarger.navMorkGra;
-            else if (valgt) return navFarger.navDypBla;
-            else return navFarger.navBla;
+            if (disabled) return AGray900;
+            else if (valgt) return ABlue600;
+            else return ABlue500;
         }};
     }
 
@@ -38,7 +37,7 @@ const EtikettKnapp = styled(FamilieBaseKnapp)<{ disabled: boolean; valgt: boolea
 
     :focus,
     :active {
-        background-color: ${navFarger.fokusFarge};
+        background-color: ${ABlue800};
         > span {
             color: #fff;
         }
