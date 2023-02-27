bc3a22f542506b5cc5c9a237190906fe5d4148c0
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-02-27 15:19:50 +0100

    Grupper toggles basert på om de er operasjonelle eller release

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -3,16 +3,19 @@ export interface IToggles {
 }
 
 export enum ToggleNavn {
+    // Operasjonelle
     kanBehandleTekniskEndring = 'familie-ba-sak.behandling.teknisk-endring',
     kanManueltKorrigereMedVedtaksbrev = 'familie-ba-sak.behandling.korreksjon-vedtaksbrev',
     skalIkkeStoppeMigreringsbehandlig = 'familie-ba-sak.ikke.stopp.migeringsbehandling',
     tekniskVedlikeholdHenleggelse = 'familie-ba-sak.teknisk-vedlikehold-henleggelse.tilgangsstyring',
+    kanAutomatiskSetteVilkår = 'familie-ba-sak.kan-automatisk-sette-vilkaar',
+
+    // Release
     endreMottakerEndringsårsaker = 'familie-ba-sak.behandling.endringsperiode.endre-mottaker-aarsaker.utgivelse',
     trekkILøpendeUtbetaling = 'familie-ba-sak.trekk-i-loepende-utbetaling',
     kanBehandleKlage = 'familie-ba-sak.klage',
     støtterEnsligMindreårig = 'familie-ba-sak.behandling.enslig-mindreaarig',
     kanKjøreSatsendringManuelt = 'familie-ba-sak.kan-kjore-satsendring-manuelt',
-    kanAutomatiskSetteVilkår = 'familie-ba-sak.kan-automatisk-sette-vilkaar',
     nyMåteÅBeregneBehandlingsresultat = 'familie-ba-sak.behandling.behandlingsresultat',
     manuellPostering = 'familie-ba-sak.manuell-postering',
 }
