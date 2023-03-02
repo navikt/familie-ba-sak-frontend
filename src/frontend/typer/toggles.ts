42c9f992b86c3d8f0b187e8acffa02e2e7a15ecc
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-03-02 13:31:46 +0100

    Chore: fjern toggle for å legge til mottaker (#2555)
    * Fjern toggle for å legge til brevmottaker
    
    * Grupper toggles basert på om de er operasjonelle eller release

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -3,16 +3,18 @@ export interface IToggles {
 }
 
 export enum ToggleNavn {
+    // Operasjonelle
     kanBehandleTekniskEndring = 'familie-ba-sak.behandling.teknisk-endring',
     kanManueltKorrigereMedVedtaksbrev = 'familie-ba-sak.behandling.korreksjon-vedtaksbrev',
     skalIkkeStoppeMigreringsbehandlig = 'familie-ba-sak.ikke.stopp.migeringsbehandling',
     tekniskVedlikeholdHenleggelse = 'familie-ba-sak.teknisk-vedlikehold-henleggelse.tilgangsstyring',
+    kanAutomatiskSetteVilkår = 'familie-ba-sak.kan-automatisk-sette-vilkaar',
+
+    // Release
     endreMottakerEndringsårsaker = 'familie-ba-sak.behandling.endringsperiode.endre-mottaker-aarsaker.utgivelse',
-    leggTilMottaker = 'familie-ba-sak.behandling.legg-til-mottaker',
     kanBehandleKlage = 'familie-ba-sak.klage',
     støtterEnsligMindreårig = 'familie-ba-sak.behandling.enslig-mindreaarig',
     kanKjøreSatsendringManuelt = 'familie-ba-sak.kan-kjore-satsendring-manuelt',
-    kanAutomatiskSetteVilkår = 'familie-ba-sak.kan-automatisk-sette-vilkaar',
     nyMåteÅBeregneBehandlingsresultat = 'familie-ba-sak.behandling.behandlingsresultat',
     manuellPostering = 'familie-ba-sak.manuell-postering',
 }
