724a9da171f3318aa60fa3458be4c4f735b1d2b4
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-03-22 13:26:45 +0100

    Oppdaterer kanSøkeFritekst til å bruke fieldset istedenfor skjemagruppe

--- src/frontend/komponenter/Fagsak/Dokumentutsending/KanSøke/KanSøkeFritekst.tsx
+++ src/frontend/komponenter/Fagsak/Dokumentutsending/KanSøke/KanSøkeFritekst.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { Label, SkjemaGruppe } from 'nav-frontend-skjema';
-
 import { AddCircle, Delete } from '@navikt/ds-icons';
-import { Button } from '@navikt/ds-react';
+import { Button, Fieldset } from '@navikt/ds-react';
 import { FamilieTextarea } from '@navikt/familie-form-elements';
 import type { FeltState } from '@navikt/familie-skjema';
 
@@ -70,66 +68,64 @@ const KanSøkeFritekst = ({
 
     return (
         <>
-            <Label htmlFor={skjemaGruppeId}>Fritekst til kulepunkt i brev</Label>
-            <>
-                <SkjemaGruppe
-                    id={skjemaGruppeId}
-                    feil={skjema.visFeilmeldinger && hentFrontendFeilmelding(skjema.submitRessurs)}
+            <Fieldset
+                id={skjemaGruppeId}
+                error={skjema.visFeilmeldinger && hentFrontendFeilmelding(skjema.submitRessurs)}
+                legend="Fritekst felt til kulepunkt i brev"
+            >
+                {friteksterFelt.verdi.map((fritekst: FeltState<IFritekstFelt>) => {
+                    const fritekstId = fritekst.verdi.id;
+
+                    return (
+                        <StyledFamilieFritekstFelt key={`fritekst-${fritekstId}`}>
+                            <FamilieTextareaBegrunnelseFritekst
+                                erLesevisning={false}
+                                key={`fritekst-${fritekstId}`}
+                                id={`${fritekstId}`}
+                                className={'fritekst-textarea'}
+                                value={fritekst.verdi.tekst}
+                                label={`Kulepunkt ${fritekstId}`}
+                                hideLabel={true}
+                                maxLength={makslengdeFritekst}
+                                onChange={event => onChangeFritekst(event, fritekstId)}
+                                error={skjema.visFeilmeldinger && fritekst.feilmelding}
+                                /* eslint-disable-next-line jsx-a11y/no-autofocus */
+                                autoFocus
+                            />
+
+                            <SletteKnapp
+                                variant={'tertiary'}
+                                onClick={() => {
+                                    friteksterFelt.validerOgSettFelt([
+                                        ...friteksterFelt.verdi.filter(
+                                            mapFritekst =>
+                                                mapFritekst.verdi.id !== fritekst.verdi.id
+                                        ),
+                                    ]);
+                                }}
+                                id={`fjern_fritekst-${fritekstId}`}
+                                size={'small'}
+                                aria-label={'Fjern fritekst'}
+                                icon={<Delete />}
+                            >
+                                {'Fjern'}
+                            </SletteKnapp>
+                        </StyledFamilieFritekstFelt>
+                    );
+                })}
+            </Fieldset>
+
+            {!erMaksAntallKulepunkter && (
+                <Button
+                    variant={'tertiary'}
+                    onClick={leggTilFritekst}
+                    id={`legg-til-fritekst`}
+                    size={'small'}
+                    icon={<AddCircle />}
                 >
-                    {friteksterFelt.verdi.map((fritekst: FeltState<IFritekstFelt>) => {
-                        const fritekstId = fritekst.verdi.id;
-
-                        return (
-                            <StyledFamilieFritekstFelt key={`fritekst-${fritekstId}`}>
-                                <FamilieTextareaBegrunnelseFritekst
-                                    erLesevisning={false}
-                                    key={`fritekst-${fritekstId}`}
-                                    id={`${fritekstId}`}
-                                    className={'fritekst-textarea'}
-                                    value={fritekst.verdi.tekst}
-                                    label={`Kulepunkt ${fritekstId}`}
-                                    hideLabel={true}
-                                    maxLength={makslengdeFritekst}
-                                    onChange={event => onChangeFritekst(event, fritekstId)}
-                                    error={skjema.visFeilmeldinger && fritekst.feilmelding}
-                                    /* eslint-disable-next-line jsx-a11y/no-autofocus */
-                                    autoFocus
-                                />
-
-                                <SletteKnapp
-                                    variant={'tertiary'}
-                                    onClick={() => {
-                                        friteksterFelt.validerOgSettFelt([
-                                            ...friteksterFelt.verdi.filter(
-                                                mapFritekst =>
-                                                    mapFritekst.verdi.id !== fritekst.verdi.id
-                                            ),
-                                        ]);
-                                    }}
-                                    id={`fjern_fritekst-${fritekstId}`}
-                                    size={'small'}
-                                    aria-label={'Fjern fritekst'}
-                                    icon={<Delete />}
-                                >
-                                    {'Fjern'}
-                                </SletteKnapp>
-                            </StyledFamilieFritekstFelt>
-                        );
-                    })}
-                </SkjemaGruppe>
-
-                {!erMaksAntallKulepunkter && (
-                    <Button
-                        variant={'tertiary'}
-                        onClick={leggTilFritekst}
-                        id={`legg-til-fritekst`}
-                        size={'small'}
-                        icon={<AddCircle />}
-                    >
-                        {'Legg til fritekst'}
-                    </Button>
-                )}
-            </>
+                    {'Legg til fritekst'}
+                </Button>
+            )}
         </>
     );
 };
