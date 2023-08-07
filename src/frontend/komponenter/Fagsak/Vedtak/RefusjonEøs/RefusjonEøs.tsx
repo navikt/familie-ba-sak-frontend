b036155e4196768caeafe5676f21b2aed3fe00c6
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-08-07 15:30:35 +0200

    Bytt ut CopyToClipboard med CopyButton i RefusjonEøs

--- src/frontend/komponenter/Fagsak/Vedtak/RefusjonEøs/RefusjonEøs.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/RefusjonEøs/RefusjonEøs.tsx
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { AddCircle } from '@navikt/ds-icons';
-import { Button, Heading, Table } from '@navikt/ds-react';
-import { CopyToClipboard } from '@navikt/ds-react-internal';
+import { Button, CopyButton, Heading, Table } from '@navikt/ds-react';
 import { ATextAction } from '@navikt/ds-tokens/dist/tokens';
 
 import { useBehandling } from '../../../../context/behandlingContext/BehandlingContext';
@@ -35,7 +34,7 @@ const FlexRowDiv = styled.div`
     justify-content: space-between;
 `;
 
-const KopierTilNøsKnapp = styled(CopyToClipboard)`
+const KopierTilNøsKnapp = styled(CopyButton)`
     :not(:hover):not(:active) {
         color: ${ATextAction};
     }
@@ -124,9 +123,12 @@ const RefusjonEøs: React.FC<IRefusjonEøs> = ({
                         Legg til ny periode
                     </Button>
                 )}
-                <KopierTilNøsKnapp copyText={tekstTilNØS} popoverText="Kopiert!" size="small">
-                    Kopier tekst til NØS
-                </KopierTilNøsKnapp>
+                <KopierTilNøsKnapp
+                    copyText={tekstTilNØS}
+                    text="Kopier tekst til NØS"
+                    activeText="Kopiert!"
+                    size="small"
+                />
             </FlexRowDiv>
         </FlexColumnDiv>
     );
