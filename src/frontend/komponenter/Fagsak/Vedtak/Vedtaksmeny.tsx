56355e3f3698dd147c724554ed7810dccc09b977
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-09-06 14:58:06 +0200

    Tilgjengeliggjør korriger vedtaksknapp så lenge det er behandling med vedtaksbrevutsending

--- src/frontend/komponenter/Fagsak/Vedtak/Vedtaksmeny.tsx
+++ src/frontend/komponenter/Fagsak/Vedtak/Vedtaksmeny.tsx
@@ -15,7 +15,7 @@ import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/behandlingContext/BehandlingContext';
 import { useFagsakContext } from '../../../context/fagsak/FagsakContext';
 import type { IBehandling } from '../../../typer/behandling';
-import { Behandlingstype, BehandlingÅrsak } from '../../../typer/behandling';
+import { Behandlingstype } from '../../../typer/behandling';
 import { BehandlingKategori } from '../../../typer/behandlingstema';
 import { FagsakType } from '../../../typer/fagsak';
 import { ToggleNavn } from '../../../typer/toggles';
@@ -53,10 +53,6 @@ const Vedtaksmeny: React.FunctionComponent<IVedtakmenyProps> = ({
     const minimalFagsak = hentDataFraRessurs(minimalFagsakRessurs);
     const fagsakType = minimalFagsak?.fagsakType;
 
-    const kanIkkeKorrigereVedtak =
-        åpenBehandling.type === Behandlingstype.REVURDERING &&
-        [BehandlingÅrsak.KLAGE, BehandlingÅrsak.DØDSFALL_BRUKER].includes(åpenBehandling.årsak);
-
     return (
         <Dropdown>
             <KnappHøyreHjørne
@@ -77,13 +73,11 @@ const Vedtaksmeny: React.FunctionComponent<IVedtakmenyProps> = ({
                                 korrigertEtterbetaling={åpenBehandling.korrigertEtterbetaling}
                                 behandlingId={åpenBehandling.behandlingId}
                             />
-                            {(!kanIkkeKorrigereVedtak || åpenBehandling.korrigertVedtak) && (
-                                <KorrigerVedtak
-                                    erLesevisning={erLesevisning}
-                                    korrigertVedtak={åpenBehandling.korrigertVedtak}
-                                    behandlingId={åpenBehandling.behandlingId}
-                                />
-                            )}
+                            <KorrigerVedtak
+                                erLesevisning={erLesevisning}
+                                korrigertVedtak={åpenBehandling.korrigertVedtak}
+                                behandlingId={åpenBehandling.behandlingId}
+                            />
                         </>
                     )}
 
