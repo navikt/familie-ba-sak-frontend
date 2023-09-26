070e7b346f4678be5acc90692d5c9b632e448d5a
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-09-26 16:38:08 +0200

    Fiks Brev sendt-modal

--- src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/BrevModul/Brev.tsx
+++ src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/BrevModul/Brev.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 
-import { Button, Heading, Modal } from '@navikt/ds-react';
+import { Button, Modal } from '@navikt/ds-react';
 
 import Brevskjema from './Brevskjema';
 import useSakOgBehandlingParams from '../../../../hooks/useSakOgBehandlingParams';
@@ -11,16 +10,6 @@ import useSakOgBehandlingParams from '../../../../hooks/useSakOgBehandlingParams
 interface IProps {
     onIModalClick: () => void;
 }
-
-const KnappHøyre = styled(Button)`
-    margin-left: 1rem;
-`;
-
-const Knapperad = styled.div`
-    margin-top: 2rem;
-    display: flex;
-    justify-content: center;
-`;
 
 const Brev = ({ onIModalClick }: IProps) => {
     const { fagsakId } = useSakOgBehandlingParams();
@@ -35,29 +24,21 @@ const Brev = ({ onIModalClick }: IProps) => {
                     settVisInnsendtBrevModal(true);
                 }}
             />
-            <Modal
-                open={visInnsendtBrevModal}
-                onClose={() => {
-                    settVisInnsendtBrevModal(false);
-                    onIModalClick();
-                }}
-            >
-                <Modal.Content>
-                    <Heading size="medium" level={'2'}>
-                        Brevet er sendt
-                    </Heading>
-                    <Knapperad>
+            {visInnsendtBrevModal && (
+                <Modal
+                    open={visInnsendtBrevModal}
+                    onClose={() => {
+                        settVisInnsendtBrevModal(false);
+                        onIModalClick();
+                    }}
+                    header={{
+                        heading: 'Brevet er sendt',
+                        size: 'medium',
+                    }}
+                    portal
+                >
+                    <Modal.Footer>
                         <Button
-                            variant={'secondary'}
-                            key={'til oppgavebenken'}
-                            size={'medium'}
-                            onClick={() => {
-                                onIModalClick();
-                                navigate('/oppgaver');
-                            }}
-                            children={'Se oppgavebenk'}
-                        />
-                        <KnappHøyre
                             variant={'secondary'}
                             key={'til saksoversikt'}
                             size={'medium'}
@@ -68,9 +49,19 @@ const Brev = ({ onIModalClick }: IProps) => {
                             }}
                             children={'Se saksoversikt'}
                         />
-                    </Knapperad>
-                </Modal.Content>
-            </Modal>
+                        <Button
+                            variant={'secondary'}
+                            key={'til oppgavebenken'}
+                            size={'medium'}
+                            onClick={() => {
+                                onIModalClick();
+                                navigate('/oppgaver');
+                            }}
+                            children={'Se oppgavebenk'}
+                        />
+                    </Modal.Footer>
+                </Modal>
+            )}
         </div>
     );
 };
