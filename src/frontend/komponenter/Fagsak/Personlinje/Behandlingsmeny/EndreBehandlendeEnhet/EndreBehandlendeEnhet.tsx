f1c5e18f493bb560859668c32fc8ff7af3cb880e
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-09-27 10:13:35 +0200

    Fiks modal for endre behandlende enhet

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/EndreBehandlendeEnhet/EndreBehandlendeEnhet.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/EndreBehandlendeEnhet/EndreBehandlendeEnhet.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Button, Fieldset } from '@navikt/ds-react';
+import { Button, Fieldset, Modal } from '@navikt/ds-react';
 import { Dropdown } from '@navikt/ds-react';
 import { FamilieSelect, FamilieTextarea } from '@navikt/familie-form-elements';
 import { byggTomRessurs, hentDataFraRessurs, RessursStatus } from '@navikt/familie-typer';
@@ -12,15 +12,9 @@ import { BehandlingSteg, hentStegNummer } from '../../../../../typer/behandling'
 import type { IArbeidsfordelingsenhet } from '../../../../../typer/enhet';
 import { behandendeEnheter } from '../../../../../typer/enhet';
 import { hentFrontendFeilmelding } from '../../../../../utils/ressursUtils';
-import UIModalWrapper from '../../../../Felleskomponenter/Modal/UIModalWrapper';
 
 const EndreBehandlendeEnhet: React.FC = () => {
-    const {
-        åpenBehandling,
-        vurderErLesevisning,
-        erBehandleneEnhetMidlertidig,
-        erBehandlingAvsluttet,
-    } = useBehandling();
+    const { åpenBehandling, vurderErLesevisning, erBehandleneEnhetMidlertidig } = useBehandling();
     const [visModal, settVisModal] = useState(erBehandleneEnhetMidlertidig);
     const { innloggetSaksbehandler } = useApp();
 
@@ -64,16 +58,66 @@ const EndreBehandlendeEnhet: React.FC = () => {
             <Dropdown.Menu.List.Item onClick={() => settVisModal(true)}>
                 Endre behandlende enhet
             </Dropdown.Menu.List.Item>
-            <UIModalWrapper
-                modal={{
-                    actions: [
-                        <Button
-                            key={'avbryt'}
-                            size="small"
-                            variant="secondary"
-                            onClick={lukkBehandlendeEnhetModal}
-                            children={'Avbryt'}
-                        />,
+            {visModal && (
+                <Modal
+                    open={visModal}
+                    onClose={lukkBehandlendeEnhetModal}
+                    width={'small'}
+                    header={{
+                        heading: 'Endre enhet for denne behandlingen',
+                        size: 'small',
+                    }}
+                    portal
+                >
+                    <Modal.Body>
+                        <Fieldset
+                            error={hentFrontendFeilmelding(submitRessurs)}
+                            legend="Endre enhet"
+                            hideLegend
+                        >
+                            <FamilieSelect
+                                erLesevisning={erLesevisningPåBehandling()}
+                                lesevisningVerdi={valgtArbeidsfordelingsenhet?.enhetNavn}
+                                name="enhet"
+                                value={enhetId}
+                                label={'Velg ny enhet'}
+                                onChange={(event: React.ChangeEvent<HTMLSelectElement>): void => {
+                                    settEnhetId(event.target.value);
+                                    settSubmitRessurs(byggTomRessurs());
+                                }}
+                            >
+                                {behandendeEnheter.map((enhet: IArbeidsfordelingsenhet) => {
+                                    return (
+                                        <option
+                                            aria-selected={enhetId === enhet.enhetId}
+                                            key={enhet.enhetId}
+                                            value={enhet.enhetId}
+                                            disabled={
+                                                hentDataFraRessurs(åpenBehandling)
+                                                    ?.arbeidsfordelingPåBehandling
+                                                    .behandlendeEnhetId === enhet.enhetId
+                                            }
+                                        >
+                                            {`${enhet.enhetId} ${enhet.enhetNavn}`}
+                                        </option>
+                                    );
+                                })}
+                            </FamilieSelect>
+
+                            <FamilieTextarea
+                                disabled={submitRessurs.status === RessursStatus.HENTER}
+                                erLesevisning={erLesevisningPåBehandling()}
+                                label={'Begrunnelse'}
+                                value={begrunnelse}
+                                maxLength={4000}
+                                onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                                    settBegrunnelse(event.target.value);
+                                    settSubmitRessurs(byggTomRessurs());
+                                }}
+                            />
+                        </Fieldset>
+                    </Modal.Body>
+                    <Modal.Footer>
                         <Button
                             key={'bekreft'}
                             variant="primary"
@@ -86,61 +130,17 @@ const EndreBehandlendeEnhet: React.FC = () => {
                             }}
                             children={'Bekreft'}
                             loading={submitRessurs.status === RessursStatus.HENTER}
-                        />,
-                    ],
-                    onClose: lukkBehandlendeEnhetModal,
-                    lukkKnapp: true,
-                    tittel: 'Endre enhet for denne behandlingen',
-                    visModal: visModal && !erBehandlingAvsluttet,
-                }}
-            >
-                <Fieldset
-                    error={hentFrontendFeilmelding(submitRessurs)}
-                    legend="Endre enhet"
-                    hideLegend
-                >
-                    <FamilieSelect
-                        erLesevisning={erLesevisningPåBehandling()}
-                        lesevisningVerdi={valgtArbeidsfordelingsenhet?.enhetNavn}
-                        name="enhet"
-                        value={enhetId}
-                        label={'Velg ny enhet'}
-                        onChange={(event: React.ChangeEvent<HTMLSelectElement>): void => {
-                            settEnhetId(event.target.value);
-                            settSubmitRessurs(byggTomRessurs());
-                        }}
-                    >
-                        {behandendeEnheter.map((enhet: IArbeidsfordelingsenhet) => {
-                            return (
-                                <option
-                                    aria-selected={enhetId === enhet.enhetId}
-                                    key={enhet.enhetId}
-                                    value={enhet.enhetId}
-                                    disabled={
-                                        hentDataFraRessurs(åpenBehandling)
-                                            ?.arbeidsfordelingPåBehandling.behandlendeEnhetId ===
-                                        enhet.enhetId
-                                    }
-                                >
-                                    {`${enhet.enhetId} ${enhet.enhetNavn}`}
-                                </option>
-                            );
-                        })}
-                    </FamilieSelect>
-
-                    <FamilieTextarea
-                        disabled={submitRessurs.status === RessursStatus.HENTER}
-                        erLesevisning={erLesevisningPåBehandling()}
-                        label={'Begrunnelse'}
-                        value={begrunnelse}
-                        maxLength={4000}
-                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-                            settBegrunnelse(event.target.value);
-                            settSubmitRessurs(byggTomRessurs());
-                        }}
-                    />
-                </Fieldset>
-            </UIModalWrapper>
+                        />
+                        <Button
+                            key={'avbryt'}
+                            size="small"
+                            variant="secondary"
+                            onClick={lukkBehandlendeEnhetModal}
+                            children={'Avbryt'}
+                        />
+                    </Modal.Footer>
+                </Modal>
+            )}
         </>
     );
 };
