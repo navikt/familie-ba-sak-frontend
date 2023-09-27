954d2bdf0369bf40825897dc0420f7abeaca59c3
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2023-09-27 16:45:58 +0200

    Filtrer ut avslagsbegrunnelser i vilkårsvurdering (#2794)
    * Filtrer_ut_avslagsbegrunnelser_basert_på_regelverk_og_fagsaktype
    
    * prettier
    
    prettier
    
    * Ternaries til If

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/useAvslagBegrunnelseMultiselect.ts
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/useAvslagBegrunnelseMultiselect.ts
@@ -6,23 +6,31 @@ import type { VilkårType } from '../../../../typer/vilkår';
 import { Regelverk } from '../../../../typer/vilkår';
 import { useVedtaksbegrunnelseTekster } from '../../Vedtak/VedtakBegrunnelserTabell/Context/VedtaksbegrunnelseTeksterContext';
 
-const useAvslagBegrunnelseMultiselect = (vilkårType: VilkårType, regelverk: Regelverk | null) => {
+const useAvslagBegrunnelseMultiselect = (
+    vilkårType: VilkårType,
+    regelverk: Regelverk | null,
+    gjelderInstitusjon: boolean
+) => {
     const { vedtaksbegrunnelseTekster } = useVedtaksbegrunnelseTekster();
 
     if (vedtaksbegrunnelseTekster.status !== RessursStatus.SUKSESS) {
         return { avslagBegrunnelseTeksterForGjeldendeVilkår: [] };
     }
 
-    const begrunnelsestyperForRegelverk: VedtakBegrunnelseType[] =
-        regelverk === Regelverk.EØS_FORORDNINGEN
-            ? [VedtakBegrunnelseType.EØS_AVSLAG]
-            : [VedtakBegrunnelseType.AVSLAG, VedtakBegrunnelseType.INSTITUSJON_AVSLAG];
+    let begrunnelsestypeGyldigForBehandling;
 
-    const avslagBegrunnelseTeksterForGjeldendeVilkår = begrunnelsestyperForRegelverk.flatMap(type =>
-        vedtaksbegrunnelseTekster.data[type].filter(
-            (begrunnelse: IRestVedtakBegrunnelseTilknyttetVilkår) =>
-                begrunnelse.vilkår === vilkårType
-        )
+    if (regelverk === Regelverk.EØS_FORORDNINGEN) {
+        begrunnelsestypeGyldigForBehandling = VedtakBegrunnelseType.EØS_AVSLAG;
+    } else if (gjelderInstitusjon) {
+        begrunnelsestypeGyldigForBehandling = VedtakBegrunnelseType.INSTITUSJON_AVSLAG;
+    } else {
+        begrunnelsestypeGyldigForBehandling = VedtakBegrunnelseType.AVSLAG;
+    }
+
+    const avslagBegrunnelseTeksterForGjeldendeVilkår = vedtaksbegrunnelseTekster.data[
+        begrunnelsestypeGyldigForBehandling
+    ].filter(
+        (begrunnelse: IRestVedtakBegrunnelseTilknyttetVilkår) => begrunnelse.vilkår === vilkårType
     );
 
     return {
