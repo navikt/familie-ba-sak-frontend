e789808f76fc7a39cecc2ca7e5ad42ec5c8ce066
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-10-16 12:28:43 +0200

    Flytt kode for modal-innhold inn i modal-komponent så data rendres riktig

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/SettEllerOppdaterVenting.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/SettEllerOppdaterVenting.tsx
@@ -1,47 +1,18 @@
 import React, { useState } from 'react';
 
 import { Dropdown } from '@navikt/ds-react';
-import type { Ressurs } from '@navikt/familie-typer';
 
 import { SettBehandlingPåVentModal } from './SettBehandlingPåVentModal';
-import { useSettPåVentSkjema } from './useSettPåVentSkjema';
-import { useBehandling } from '../../../../../context/behandlingContext/BehandlingContext';
-import type { IBehandling, ISettPåVent } from '../../../../../typer/behandling';
-import { formatterDateTilIsoString } from '../../../../../utils/dato';
+import type { IBehandling } from '../../../../../typer/behandling';
 
 interface IProps {
     behandling: IBehandling;
 }
 
 const SettEllerOppdaterVenting: React.FC<IProps> = ({ behandling }) => {
-    const { settÅpenBehandling } = useBehandling();
     const [visModal, settVisModal] = useState<boolean>(!!behandling.aktivSettPåVent);
-    const { skjema, kanSendeSkjema, onSubmit } = useSettPåVentSkjema(
-        behandling.aktivSettPåVent,
-        visModal
-    );
-    const { årsak, frist } = skjema.felter;
 
     const erBehandlingAlleredePåVent = !!behandling.aktivSettPåVent;
-
-    const settBehandlingPåVent = () => {
-        if (kanSendeSkjema() && årsak.verdi && frist.verdi) {
-            onSubmit<ISettPåVent>(
-                {
-                    method: erBehandlingAlleredePåVent ? 'PUT' : 'POST',
-                    data: {
-                        frist: formatterDateTilIsoString(frist.verdi),
-                        årsak: årsak.verdi,
-                    },
-                    url: `/familie-ba-sak/api/sett-på-vent/${behandling.behandlingId}`,
-                },
-                (ressurs: Ressurs<IBehandling>) => {
-                    settÅpenBehandling(ressurs);
-                    settVisModal(false);
-                }
-            );
-        }
-    };
 
     return (
         <>
@@ -53,10 +24,8 @@ const SettEllerOppdaterVenting: React.FC<IProps> = ({ behandling }) => {
 
             {visModal && (
                 <SettBehandlingPåVentModal
-                    onAvbryt={() => settVisModal(false)}
-                    settBehandlingPåVent={settBehandlingPåVent}
-                    skjema={skjema}
-                    erBehandlingAlleredePåVent={erBehandlingAlleredePåVent}
+                    lukkModal={() => settVisModal(false)}
+                    behandling={behandling}
                 />
             )}
         </>
--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/useSettPåVentSkjema.ts
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggBehandlingPåVent/useSettPåVentSkjema.ts
@@ -10,7 +10,7 @@ import { dagensDato, validerGyldigDato } from '../../../../../utils/dato';
 
 const STANDARD_ANTALL_DAGER_FRIST = 3 * 7;
 
-export const useSettPåVentSkjema = (settPåVent: ISettPåVent | undefined, modalVises: boolean) => {
+export const useSettPåVentSkjema = (settPåVent: ISettPåVent | undefined) => {
     const standardfrist = addDays(dagensDato(), STANDARD_ANTALL_DAGER_FRIST);
     const settPåVentFrist = settPåVent?.frist ? new Date(settPåVent?.frist) : undefined;
 
@@ -46,7 +46,7 @@ export const useSettPåVentSkjema = (settPåVent: ISettPåVent | undefined, moda
     };
 
     useEffect(() => {
-        if (modalVises && settPåVent) {
+        if (settPåVent) {
             settPåVentSkjema.skjema.felter.frist.validerOgSettFelt(settPåVentFrist);
             settPåVentSkjema.skjema.felter.årsak.validerOgSettFelt(settPåVent.årsak);
         } else {
