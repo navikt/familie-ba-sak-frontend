68707abc0ccc23c24990d04c9116afdc43b7026d
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-10-30 15:50:56 +0100

    Fjern funksjon som kun ble brukt i test

--- src/frontend/utils/kalender/sammenligning.ts
+++ src/frontend/utils/kalender/sammenligning.ts
@@ -1,9 +1,5 @@
 import type { DagMånedÅr } from '.';
 
-export const erISammeMåned = (dato1: DagMånedÅr, dato2: DagMånedÅr) => {
-    return dato1.måned === dato2.måned && dato1.år === dato2.år;
-};
-
 export const erFør = (dato1: DagMånedÅr, dato2: DagMånedÅr) => {
     if (dato1.dag < dato2.dag && dato1.måned <= dato2.måned && dato1.år <= dato2.år) {
         return true;
