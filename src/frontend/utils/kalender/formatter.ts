16e90fb80fc50114fba9139810396399c03c509a
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-11-10 10:13:29 +0100

    Fjern bruk av serializeIso8601String-funksjon og slett ubrukte funksjoner

--- src/frontend/utils/kalender/formatter.ts
+++ src/frontend/utils/kalender/formatter.ts
@@ -1,15 +1,5 @@
-import type { DagMånedÅr, MånedÅr } from './typer';
+import type { MånedÅr } from './typer';
 import { capString } from './utils';
-
-// Plusser på 1 måned da dato objektet bruker 0-11 tallrekke for måneder
-export const tilVisning = (dagMånedÅr?: DagMånedÅr) => {
-    return dagMånedÅr
-        ? `${capString(2, dagMånedÅr.dag)}.${capString(2, dagMånedÅr.måned + 1)}.${capString(
-              4,
-              dagMånedÅr.år
-          )}`
-        : '';
-};
 
 export const yearMonthTilVisning = (månedÅr?: MånedÅr) => {
     return månedÅr ? `${capString(2, månedÅr.måned + 1)}.${capString(4, månedÅr.år)}` : '';
--- src/frontend/utils/kalender/io.ts
+++ src/frontend/utils/kalender/io.ts
@@ -1,27 +1,12 @@
 import { isValid, parseISO } from 'date-fns';
 
 import type { DagMånedÅr, FamilieIsoDate, MånedÅr, YearMonth } from './typer';
-import { capString } from './utils';
 
 export const erIsoStringGyldig = (familieIsoDato?: FamilieIsoDate): boolean => {
     if (!familieIsoDato) return false;
 
     const dato = parseISO(familieIsoDato);
     return isValid(dato);
-};
-
-export const parseIso8601String = (familieIsoDato: FamilieIsoDate): DagMånedÅr => {
-    const dato = parseISO(familieIsoDato);
-
-    if (!isValid(dato)) {
-        throw new Error(`Dato '${familieIsoDato}' er ugyldig`);
-    }
-
-    return {
-        dag: dato.getDate(),
-        måned: dato.getMonth(),
-        år: dato.getFullYear(),
-    };
 };
 
 export const parseIso8601StringMånedÅr = (dato: YearMonth): MånedÅr => {
@@ -49,11 +34,4 @@ export const parseIso8601MånedString = (familieIsoDato: FamilieIsoDate): DagMå
         måned: dato.getMonth(),
         år: dato.getFullYear(),
     };
-};
-
-export const serializeIso8601String = ({ år, måned, dag }: DagMånedÅr): FamilieIsoDate => {
-    const yyyy = capString(4, år);
-    const mm = capString(2, måned + 1);
-    const dd = capString(2, dag);
-    return `${yyyy}-${mm}-${dd}`;
 };
