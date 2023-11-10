b556862f7e6b2345221d814bc8ecd35870da80e2
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-11-10 09:49:54 +0100

    Fjern ubrukte funksjoner og konstanter

--- src/frontend/utils/kalender/kalender.ts
+++ src/frontend/utils/kalender/kalender.ts
@@ -1,26 +1,10 @@
 import { parseIso8601String, parseIso8601StringMånedÅr } from './io';
 import type { DagMånedÅr, FamilieIsoDate, MånedÅr, YearMonth } from './typer';
 
-export const TIDENES_MORGEN: DagMånedÅr = {
-    dag: 1,
-    måned: 1,
-    år: 1000,
-};
-export const TIDENES_ENDE: DagMånedÅr = {
-    dag: 1,
-    måned: 1,
-    år: 3000,
-};
-
 export const kalenderDato = (dato: FamilieIsoDate): DagMånedÅr => parseIso8601String(dato);
 
 export const yearMonthTilKalenderMåned = (yearMonth: YearMonth): MånedÅr =>
     parseIso8601StringMånedÅr(yearMonth);
-
-export const kalenderDatoMedFallback = (
-    dato: FamilieIsoDate | undefined,
-    fallbackDato: DagMånedÅr
-): DagMånedÅr => (dato ? parseIso8601String(dato) : fallbackDato);
 
 export const kalenderDatoFraDate = (date: Date): DagMånedÅr => ({
     dag: date.getDate(),
@@ -40,5 +24,3 @@ export const kalenderDatoTilDate = (
         timer ? timer : 0,
         minutter ? minutter : 0
     );
-
-export const iDag = (): DagMånedÅr => kalenderDatoFraDate(new Date());
