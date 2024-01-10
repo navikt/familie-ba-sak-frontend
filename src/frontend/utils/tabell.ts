1d82cb3e0fb0e4c7079501c67e212cda43a0a314
Kristine Steine <kristine.steine@nav.no>
2024-01-10 10:47:35 +0100

    Erstatt react-table i oppgavebenk med Table fra Aksel (#2939)
    * Endre html-tags i containerelementer til mer uu
    
    * Bytt ut react-table i oppgavebenken med Table fra Aksel
    
    * Bruk liten font i tabellcellene
    
    * Flytt tabellsorteringsutils ut av journalpostmappen
    
    * Unngå p-tag inni tabellrad
    
    * Lag enum for sorteringsnøkler
    
    * Sorter oppgavebenk basert på lagret sorteringsnøkkel
    
    * Flytt paginering ut av react-table
    
    * Flytt sorteringslogikk opp i context
    
    * Sorter oppgaverader etter valgt nøkkel
    
    * Bruk sorterte og paginerte oppgaver i listen
    
    * Fjern tom linje
    
    * Håndter styling av tilordnet ressurs med aksel-primitiver
    
    * Bruk medium fontstørrelse på tabellinnhold
    
    * Trunker beskrivelse og legg i tooltip slik som tidligere
    
    * Fjern gammel styling i less
    
    * Unngå at kolonneheaderne brekker i høyden
    
    * Fjern ubrukt kode fra oppgavecontext
    
    * Legg tli tooltip på registreringsdato
    
    * Legg til tooltip på oppgavetype og gjelder

---
+++ src/frontend/utils/tabell.ts
@@ -0,0 +1,32 @@
+import type { SortState } from '@navikt/ds-react';
+
+export enum Sorteringsrekkefølge {
+    STIGENDE = 'ascending',
+    SYNKENDE = 'descending',
+    INGEN_SORTERING = '',
+}
+
+export const hentSortState = (
+    sortering: Sorteringsrekkefølge,
+    sortKey: string
+): SortState | undefined =>
+    sortering === Sorteringsrekkefølge.INGEN_SORTERING
+        ? undefined
+        : {
+              orderBy: sortKey,
+              direction: sortering === Sorteringsrekkefølge.STIGENDE ? 'ascending' : 'descending',
+          };
+
+export const hentNesteSorteringsrekkefølge = (
+    forrigeSorteringsrekkefølge: string
+): Sorteringsrekkefølge => {
+    switch (forrigeSorteringsrekkefølge) {
+        case undefined:
+            return Sorteringsrekkefølge.STIGENDE;
+        case 'ascending':
+            return Sorteringsrekkefølge.SYNKENDE;
+        case 'descending':
+        default:
+            return Sorteringsrekkefølge.INGEN_SORTERING;
+    }
+};
