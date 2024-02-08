16e975af087f549c31a129f9020eb93a7da8b0e2
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-02-08 19:44:25 +0100

    fiks styling for stepper-lignende funksjonalitet

--- src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/komponenter/HendelseItem.tsx
+++ src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/komponenter/HendelseItem.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { BodyShort } from '@navikt/ds-react';
-import { ASpacing2, ASpacing3, ASpacing9 } from '@navikt/ds-tokens/dist/tokens';
+import { BodyShort, HStack, VStack } from '@navikt/ds-react';
+import {
+    ABorderDefault,
+    ABorderSubtle,
+    ASpacing2,
+    ASpacing4,
+    ASpacing6,
+} from '@navikt/ds-tokens/dist/tokens';
 
 import { BehandlerRolle, behandlerRoller } from '../../../../typer/behandling';
 import type { Hendelse } from '../typer';
@@ -18,24 +24,47 @@ const Hendelsesbeskrivelse = styled(BodyShort)`
     padding: ${ASpacing2} 0;
 `;
 
-const ListeElement = styled.li`
-    padding: ${ASpacing3} ${ASpacing3} ${ASpacing3} ${ASpacing9};
+const StyledVStack = styled(VStack)`
+    margin-bottom: ${ASpacing6};
 `;
 
+const Sirkel = styled.div`
+    border: 2px solid ${ABorderDefault};
+    height: ${ASpacing4};
+    width: ${ASpacing4};
+    border-radius: 50%;
+`;
+
+const Strek = styled.div`
+    height: 100%;
+    width: 1px;
+    background: ${ABorderSubtle};
+    top: 2px;
+    left: 0.5rem;
+    transform: translateX(-0.5px);
+`;
 const HendelseItem = ({ hendelse }: IHendelseItemProps) => (
-    <ListeElement>
-        <BodyShort weight="semibold">{hendelse.tittel}</BodyShort>
-        {hendelse.beskrivelse && (
-            <Hendelsesbeskrivelse>{hendelse.beskrivelse}</Hendelsesbeskrivelse>
-        )}
-        <BodyShort textColor="subtle">{`${hendelse.dato}`}</BodyShort>
-        <BodyShort textColor="subtle">{`${hendelse.utførtAv} ${
-            hendelse.rolle.toString() !== BehandlerRolle[BehandlerRolle.SYSTEM] &&
-            behandlerRoller[hendelse.rolle]
-                ? `(${behandlerRoller[hendelse.rolle].navn})`
-                : ''
-        }`}</BodyShort>
-    </ListeElement>
+    <li>
+        <HStack gap="4">
+            <VStack align="center">
+                <Sirkel />
+                <Strek />
+            </VStack>
+            <StyledVStack>
+                <BodyShort weight="semibold">{hendelse.tittel}</BodyShort>
+                {hendelse.beskrivelse && (
+                    <Hendelsesbeskrivelse>{hendelse.beskrivelse}</Hendelsesbeskrivelse>
+                )}
+                <BodyShort textColor="subtle">{`${hendelse.dato}`}</BodyShort>
+                <BodyShort textColor="subtle">{`${hendelse.utførtAv} ${
+                    hendelse.rolle.toString() !== BehandlerRolle[BehandlerRolle.SYSTEM] &&
+                    behandlerRoller[hendelse.rolle]
+                        ? `(${behandlerRoller[hendelse.rolle].navn})`
+                        : ''
+                }`}</BodyShort>
+            </StyledVStack>
+        </HStack>
+    </li>
 );
 
 export default HendelseItem;
