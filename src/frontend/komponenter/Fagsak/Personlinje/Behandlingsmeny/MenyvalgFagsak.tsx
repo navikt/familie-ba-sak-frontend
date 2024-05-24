3a4b86e9a7cb1bf7e10dda4f2a1c477840b0502b
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-05-24 09:57:23 +0200

    Skjul dokumentutsending for veileder (#3172)
    * Skjul 'Send informasjonsbrev' for veiledere
    
    * Legg til *.iml i gitignore
    
    * Fjern ubrukt toggle og splitt opp i separate and conditions for å rydde opp i logikktreet

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/MenyvalgFagsak.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/MenyvalgFagsak.tsx
@@ -9,9 +9,9 @@ import OpprettBehandling from './OpprettBehandling/OpprettBehandling';
 import OpprettFagsak from './OpprettFagsak/OpprettFagsak';
 import { useApp } from '../../../../context/AppContext';
 import { useFagsakContext } from '../../../../context/fagsak/FagsakContext';
+import { BehandlerRolle } from '../../../../typer/behandling';
 import type { IMinimalFagsak } from '../../../../typer/fagsak';
 import type { IPersonInfo } from '../../../../typer/person';
-import { ToggleNavn } from '../../../../typer/toggles';
 
 interface IProps {
     bruker?: IPersonInfo;
@@ -20,21 +20,23 @@ interface IProps {
 
 const MenyvalgFagsak = ({ bruker, minimalFagsak }: IProps) => {
     const navigate = useNavigate();
-    const { toggles } = useApp();
+    const { hentSaksbehandlerRolle } = useApp();
 
     const erPåDokumentutsending = useLocation().pathname.includes('dokumentutsending');
     const { manuelleBrevmottakerePåFagsak } = useFagsakContext();
+    const erSaksbehandlerEllerHøyere = hentSaksbehandlerRolle() >= BehandlerRolle.SAKSBEHANDLER;
 
     return (
         <>
             <OpprettBehandling minimalFagsak={minimalFagsak} />
             {!!bruker && <OpprettFagsak personInfo={bruker} />}
-            {toggles[ToggleNavn.manuellMottakerInfobrev] && erPåDokumentutsending ? (
+            {erPåDokumentutsending && (
                 <LeggTilEllerFjernBrevmottakere
                     erPåBehandling={false}
                     brevmottakere={manuelleBrevmottakerePåFagsak}
                 />
-            ) : (
+            )}
+            {!erPåDokumentutsending && erSaksbehandlerEllerHøyere && (
                 <Dropdown.Menu.List.Item
                     onClick={() => navigate(`/fagsak/${minimalFagsak.id}/dokumentutsending`)}
                 >
