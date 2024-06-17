79d627580885e905e634b1beb3afcb922a032de7
halvor <halvorbmundal@gmail.com>
2024-06-17 12:37:44 +0200

    Oppdater valutakurser og simulering automatisk for beslutter når de går inn på Behandlingsresultat-steget

---
+++ src/frontend/komponenter/Fagsak/Behandlingsresultat/Valutakurs/useOppdaterValutakursOgSimuleringPåBeslutterSteg.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+import { useHttp } from '@navikt/familie-http';
+
+import { useBehandling } from '../../../../context/behandlingContext/BehandlingContext';
+import { BehandlingStatus, BehandlingSteg, type IBehandling } from '../../../../typer/behandling';
+
+export const useOppdaterValutakursOgSimuleringPåBeslutterSteg = () => {
+    const { request } = useHttp();
+    const { settÅpenBehandling, behandling } = useBehandling();
+
+    useEffect(() => {
+        if (
+            behandling.status === BehandlingStatus.FATTER_VEDTAK &&
+            behandling.steg == BehandlingSteg.BESLUTTE_VEDTAK
+        ) {
+            request<void, IBehandling>({
+                method: 'POST',
+                url: `/familie-ba-sak/api/behandlinger/${behandling.behandlingId}/oppdater-valutakurser-og-simulering-automatisk`,
+            }).then(behandlingRessurs => {
+                settÅpenBehandling(behandlingRessurs);
+            });
+        }
+    }, [behandling.behandlingId]);
+};
