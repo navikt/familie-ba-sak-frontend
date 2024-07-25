5e2826a1b113df57546461a066e5878e6ee65893
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-07-25 16:16:09 +0200

    Erstatt less med styled components og aksel komponent for SystemetLaster

--- src/frontend/komponenter/Felleskomponenter/SystemetLaster/SystemetLaster.tsx
+++ src/frontend/komponenter/Felleskomponenter/SystemetLaster/SystemetLaster.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
 
-import { Loader, Heading } from '@navikt/ds-react';
+import styled from 'styled-components';
+
+import { Loader, Heading, VStack } from '@navikt/ds-react';
+import { AZIndexTooltip } from '@navikt/ds-tokens/dist/tokens';
+
+const PosisjonertVStack = styled(VStack)`
+    position: fixed;
+    width: 100vw;
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: ${AZIndexTooltip};
+`;
 
 const SystemetLaster = () => {
     return (
-        <div className={'systemet-laster'}>
-            <div className={'systemet-laster__content'}>
-                <Heading
-                    size={'medium'}
-                    className={'systemet-laster__content--tekst'}
-                    children={'Systemet laster'}
-                />
-                <Loader size="large" transparent={true} title="Systemet laster data" />
-            </div>
-        </div>
+        <PosisjonertVStack gap="2">
+            <Heading size={'medium'} children={'Systemet laster'} />
+            <Loader size="large" transparent={true} title="Systemet laster data" />
+        </PosisjonertVStack>
     );
 };
 
