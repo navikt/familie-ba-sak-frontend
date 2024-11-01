9a3361ea87017d33e48efa66a661ee3e7dff3f3e
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-11-01 11:21:56 +0100

    Slett release toggle som har vært på i over en måned

--- src/frontend/komponenter/Fagsak/Behandlingsresultat/Behandlingsresultat.tsx
+++ src/frontend/komponenter/Fagsak/Behandlingsresultat/Behandlingsresultat.tsx
@@ -18,7 +18,6 @@ import TilkjentYtelseTidslinje from './TilkjentYtelseTidslinje';
 import UtbetaltAnnetLand from './UtbetaltAnnetLand/UtbetaltAnnetLand';
 import { useOppdaterValutakursOgSimuleringPåBeslutterSteg } from './Valutakurs/useOppdaterValutakursOgSimuleringPåBeslutterSteg';
 import Valutakurser from './Valutakurs/Valutakurser';
-import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/behandlingContext/BehandlingContext';
 import { useEøs } from '../../../context/Eøs/EøsContext';
 import { useFagsakContext } from '../../../context/Fagsak/FagsakContext';
@@ -34,7 +33,6 @@ import type {
     IRestUtenlandskPeriodeBeløp,
     IRestValutakurs,
 } from '../../../typer/eøsPerioder';
-import { ToggleNavn } from '../../../typer/toggles';
 import type { IRestEndretUtbetalingAndel } from '../../../typer/utbetalingAndel';
 import type { Utbetalingsperiode } from '../../../typer/vedtaksperiode';
 import { periodeOverlapperMedValgtDato } from '../../../utils/dato';
@@ -87,7 +85,6 @@ const Behandlingsresultat: React.FunctionComponent<IBehandlingsresultatProps> = 
     useOppdaterValutakursOgSimuleringPåBeslutterSteg();
 
     const { request } = useHttp();
-    const { toggles } = useApp();
 
     const hentPersonerMedUgyldigEtterbetalingsperiode = () => {
         request<void, string[]>({
@@ -177,10 +174,6 @@ const Behandlingsresultat: React.FunctionComponent<IBehandlingsresultatProps> = 
 
     const harEøs = harKompetanser || harUtenlandskeBeløper || harValutakurser;
 
-    const gyldigEtterbetalingsperiode = toggles[ToggleNavn.erEtterbetaling3MndGyldigÅrsak]
-        ? 'tre måneder'
-        : 'tre år';
-
     return (
         <Skjemasteg
             senderInn={behandlingsstegSubmitressurs.status === RessursStatus.HENTER}
@@ -202,8 +195,7 @@ const Behandlingsresultat: React.FunctionComponent<IBehandlingsresultatProps> = 
         >
             {personerMedUgyldigEtterbetalingsperiode.length > 0 && (
                 <StyledAlert variant={'warning'}>
-                    Du har perioder som kan føre til etterbetaling utover{' '}
-                    {gyldigEtterbetalingsperiode} for person{' '}
+                    Du har perioder som kan føre til etterbetaling utover tre måneder for person{' '}
                     {slåSammenListeTilStreng(
                         personerMedUgyldigEtterbetalingsperiode.map(ident => formaterIdent(ident))
                     )}
