51b4456eb8b688a03f4e012162bb2e4a1f485c23
Magnus Tønnessen <magnus.tonnessen@nav.no>
2024-11-06 08:51:27 +0100

    Slett ubrukt toggle (#3412)

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -14,7 +14,6 @@ export enum ToggleNavn {
     // Release
     kanBehandleKlage = 'familie-ba-sak.klage',
     selvstendigRettInfobrev = 'familie-ba-sak.selvstendig-rett-infobrev',
-    erEtterbetaling3MndGyldigÅrsak = 'familie-ba-sak.etterbetaling-3-mnd',
 }
 
 export const alleTogglerAv = (): IToggles => {
