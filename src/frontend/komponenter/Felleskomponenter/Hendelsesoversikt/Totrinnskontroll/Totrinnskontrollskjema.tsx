4396c50a41c1162ac9358bc7fc72db32f3afb8a4
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-11-14 13:20:19 +0100

    fjern kode som aldri nås

--- src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/Totrinnskontroll/Totrinnskontrollskjema.tsx
+++ src/frontend/komponenter/Felleskomponenter/Hendelsesoversikt/Totrinnskontroll/Totrinnskontrollskjema.tsx
@@ -59,8 +59,7 @@ const Totrinnskontrollskjema: React.FunctionComponent<IProps> = ({
     const saksbehandler = totrinnskontroll?.saksbehandler ?? 'UKJENT SAKSBEHANDLER';
     const opprettetTidspunkt = totrinnskontroll?.opprettetTidspunkt ?? undefined;
 
-    const egetVedtak =
-        totrinnskontroll?.saksbehandler === innloggetSaksbehandler?.displayName ?? false;
+    const egetVedtak = totrinnskontroll?.saksbehandler === innloggetSaksbehandler?.displayName;
 
     return (
         <Fieldset
