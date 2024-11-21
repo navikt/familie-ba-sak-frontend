b4c293bfbf067b07f107e47836b11404136b7ba6
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-11-21 12:25:45 +0100

    Fix: ESLint-error fra webpack (#3434)
    * fjern eslint fra fork-ts-checker-webpack-plugin siden eslint-webpack-plugin fikser biffen og sett opp eslint-webpack-plugin til å støtte flat-config
    
    * oppgrader fork-ts-checker-webpack-plugin til nyeste versjon
    
    kunne ikke gjøre dette før nå siden vi trengte støtte for eslint, som siden v > 6 er droppet
    
    * legg til ts og tsx-filer i eslint webpack oppsettet så vi faktisk sjekker mer enn bare js-filer
    
    * fjern eslint.config.mjs fra prettierignore (som var skrevet feil og ikke hadde noen effekt)
    
    tenker også det er fint at den blir formattert jeg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import prettier from "eslint-plugin-prettier";
-import importPlugin from "eslint-plugin-import";
-import globals from "globals";
-import js from "@eslint/js";
-import eslintConfigPrittier from "eslint-config-prettier";
+import js from '@eslint/js';
+import eslintConfigPrittier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
@@ -14,7 +14,7 @@ export default [
     ...tseslint.configs.recommended,
     {
         plugins: {
-            "@typescript-eslint": tseslint.plugin,
+            '@typescript-eslint': tseslint.plugin,
             prettier: prettier,
             import: importPlugin,
         },
@@ -26,7 +26,7 @@ export default [
 
             parser: tseslint.parser,
             ecmaVersion: 2020,
-            sourceType: "module",
+            sourceType: 'module',
 
             parserOptions: {
                 ecmaFeatures: {
@@ -37,59 +37,73 @@ export default [
 
         settings: {
             react: {
-                version: "detect",
+                version: 'detect',
             },
         },
 
         rules: {
-            "no-case-declarations": "off",
+            'no-case-declarations': 'off',
 
-            "import/extensions": ["off", "ignorePackages", {
-                js: "never",
-                jsx: "never",
-                ts: "never",
-                tsx: "never",
-            }],
-
-            "@typescript-eslint/consistent-type-imports": "error",
-            "@typescript-eslint/ban-ts-ignore": "off",
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "prettier/prettier": "error",
-            "@typescript-eslint/interface-name-prefix": "off",
-            "@typescript-eslint/no-var-requires": "warn",
-            "@typescript-eslint/explicit-function-return-type": "off",
-
-            "@typescript-eslint/no-unused-vars": ["warn", {
-                vars: "all",
-                args: "all",
-                argsIgnorePattern: "^_",
-            }],
-
-            "import/named": "error",
-            "import/namespace": "error",
-            "import/default": "error",
-            "import/export": "error",
-
-            "import/order": ["error", {
-                alphabetize: {
-                    order: "asc",
-                    caseInsensitive: true,
+            'import/extensions': [
+                'off',
+                'ignorePackages',
+                {
+                    js: 'never',
+                    jsx: 'never',
+                    ts: 'never',
+                    tsx: 'never',
                 },
+            ],
 
-                "newlines-between": "always",
-                groups: ["builtin", "external", "internal", ["parent", "sibling"], "index"],
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/ban-ts-ignore': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'prettier/prettier': 'error',
+            '@typescript-eslint/interface-name-prefix': 'off',
+            '@typescript-eslint/no-var-requires': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'off',
 
-                pathGroups: [{
-                    pattern: "react",
-                    group: "external",
-                    position: "before",
-                }, {
-                    pattern: "@navikt/**",
-                    group: "internal",
-                    position: "before",
-                }],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                },
+            ],
 
-                pathGroupsExcludedImportTypes: ["builtin"],
-            }],
+            'import/named': 'error',
+            'import/namespace': 'error',
+            'import/default': 'error',
+            'import/export': 'error',
+
+            'import/order': [
+                'error',
+                {
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+
+                    'newlines-between': 'always',
+                    groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+
+                    pathGroups: [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: '@navikt/**',
+                            group: 'internal',
+                            position: 'before',
+                        },
+                    ],
+
+                    pathGroupsExcludedImportTypes: ['builtin'],
+                },
+            ],
         },
-}];
+    },
+];
