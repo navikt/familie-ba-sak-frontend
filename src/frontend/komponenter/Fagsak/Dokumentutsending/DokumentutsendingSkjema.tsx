9bfdfac6dfa8d52d94c8ff13aaec4ed9447992e6
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-02-24 23:02:34 +0100

    Refaktorering av kode

--- src/frontend/komponenter/Fagsak/Dokumentutsending/DokumentutsendingSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Dokumentutsending/DokumentutsendingSkjema.tsx
@@ -58,6 +58,11 @@ const StyledBrevmottakereAlert = styled(BrevmottakereAlert)`
     margin: 1rem 0;
 `;
 
+enum BarnIBrevÅrsak {
+    BARN_SØKT_FOR,
+    BARN_BOSATT_MED_SØKER,
+}
+
 const DokumentutsendingSkjema: React.FC<Props> = ({ bruker }) => {
     const {
         hentForhåndsvisningPåFagsak,
@@ -81,17 +86,29 @@ const DokumentutsendingSkjema: React.FC<Props> = ({ bruker }) => {
 
     const årsakVerdi = skjema.felter.årsak.verdi;
 
-    const barnSøktForÅrsaker = [
-        DokumentÅrsak.TIL_FORELDER_MED_SELVSTENDIG_RETT_VI_HAR_FÅTT_F016_KAN_SØKE_OM_BARNETRYGD,
-        DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_FÅTT_EN_SØKNAD_FRA_ANNEN_FORELDER,
-        DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_GJORT_VEDTAK_TIL_ANNEN_FORELDER,
-        DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_VARSEL_OM_ÅRLIG_KONTROLL,
-        DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HENTER_IKKE_REGISTEROPPLYSNINGER,
-    ];
-
-    const barnBosattMedSøkerÅrsaker = [DokumentÅrsak.KAN_HA_RETT_TIL_PENGESTØTTE_FRA_NAV];
-
     const { toggles } = useApp();
+
+    const finnBarnIBrevÅrsak = (årsak: DokumentÅrsak | undefined): BarnIBrevÅrsak | undefined => {
+        switch (årsak) {
+            case DokumentÅrsak.TIL_FORELDER_MED_SELVSTENDIG_RETT_VI_HAR_FÅTT_F016_KAN_SØKE_OM_BARNETRYGD:
+            case DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_FÅTT_EN_SØKNAD_FRA_ANNEN_FORELDER:
+            case DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_GJORT_VEDTAK_TIL_ANNEN_FORELDER:
+            case DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_VARSEL_OM_ÅRLIG_KONTROLL:
+            case DokumentÅrsak.TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HENTER_IKKE_REGISTEROPPLYSNINGER:
+                return BarnIBrevÅrsak.BARN_SØKT_FOR;
+            case DokumentÅrsak.KAN_HA_RETT_TIL_PENGESTØTTE_FRA_NAV:
+                return BarnIBrevÅrsak.BARN_BOSATT_MED_SØKER;
+            default:
+                return undefined;
+        }
+    };
+
+    const barnIBrevÅrsakTilTittel: Record<BarnIBrevÅrsak, string> = {
+        [BarnIBrevÅrsak.BARN_SØKT_FOR]: 'Hvilke barn er søkt for?',
+        [BarnIBrevÅrsak.BARN_BOSATT_MED_SØKER]: 'Hvilke barn er bosatt med søker?',
+    };
+
+    const barnIBrevÅrsak = finnBarnIBrevÅrsak(årsakVerdi);
 
     useEffect(() => {
         hentDistribusjonskanal(bruker.personIdent);
@@ -143,15 +160,6 @@ const DokumentutsendingSkjema: React.FC<Props> = ({ bruker }) => {
                 );
         }
     };
-
-    function hentTittelForBarnIBrevSkjema(årsak: DokumentÅrsak) {
-        if (barnBosattMedSøkerÅrsaker.includes(årsak)) {
-            return 'Hvilke barn er bosatt med søker?';
-        } else if (barnSøktForÅrsaker.includes(årsak)) {
-            return 'Hvilke barn er søkt for?';
-        }
-        throw new Error('Må definere hva slags tittel skjemaet skal ha');
-    }
 
     return (
         <Container>
@@ -217,16 +225,14 @@ const DokumentutsendingSkjema: React.FC<Props> = ({ bruker }) => {
                         />
                     )}
 
-                    {årsakVerdi !== undefined &&
-                        (barnSøktForÅrsaker.includes(årsakVerdi) ||
-                            barnBosattMedSøkerÅrsaker.includes(årsakVerdi)) && (
-                            <BarnIBrevSkjema
-                                barnIBrevFelt={skjema.felter.barnIBrev}
-                                visFeilmeldinger={skjema.visFeilmeldinger}
-                                settVisFeilmeldinger={settVisfeilmeldinger}
-                                tittel={hentTittelForBarnIBrevSkjema(årsakVerdi)}
-                            />
-                        )}
+                    {barnIBrevÅrsak != undefined && (
+                        <BarnIBrevSkjema
+                            barnIBrevFelt={skjema.felter.barnIBrev}
+                            visFeilmeldinger={skjema.visFeilmeldinger}
+                            settVisFeilmeldinger={settVisfeilmeldinger}
+                            tittel={barnIBrevÅrsakTilTittel[barnIBrevÅrsak]}
+                        />
+                    )}
 
                     {skjema.felter.årsak.verdi === DokumentÅrsak.KAN_SØKE && <KanSøkeSkjema />}
                 </ÅrsakSkjema>
