d31b9208616280acd7b8ea78549afb5570ef9cfe
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-03-03 15:31:19 +0100

    Legg til AvregningAlert

---
+++ src/frontend/komponenter/Fagsak/Simulering/AvregningAlert.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+import { Alert, BodyLong, List } from '@navikt/ds-react';
+
+const StyledAlert = styled(Alert)`
+    margin-top: 2rem;
+    width: fit-content;
+`;
+
+const AvregningAlert = () => {
+    return (
+        <StyledAlert variant="warning">
+            <BodyLong spacing>
+                Denne saken inneholder både en etterbetaling og en feilutbetaling. Vi kan ikke
+                automatisk avregne feilutbetalinger mot etterbetalinger.
+            </BodyLong>
+            <BodyLong>Du må derfor:</BodyLong>
+            <List>
+                <List.Item>
+                    Først gjøre vedtak om etterbetalingen, og deretter gjøre nytt vedtak om
+                    feilutbetalingen og opprette t-sak («splitte saken»).
+                </List.Item>
+            </List>
+        </StyledAlert>
+    );
+};
+
+export default AvregningAlert;
