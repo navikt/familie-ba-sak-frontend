f2d7f70f6cfb21cfcf6b2089cf7d8351183ffc11
Hanne Olsen <hanne.olsen@nav.no>
2025-03-10 13:11:57 +0100

    Fjerner console.log

--- src/frontend/utils/umami.ts
+++ src/frontend/utils/umami.ts
@@ -1,6 +1,5 @@
 export function sendTilUmami(eventNavn: string, obj: object) {
     if (typeof window !== 'undefined' && window.umami) {
-        console.log('Tracker til umami', obj);
         window.umami.track(eventNavn, obj);
     }
 }
