dcf77c88db5cf0ac3b11afd33e11242c84cbb367
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-05-05 14:24:31 +0200

    Fjern modal for bekrefting av sletting av tilbakekrevingsvedtak motregning

--- src/frontend/sider/Fagsak/Behandling/Sider/Simulering/UlovfestetMotregning/BekreftSamtykkeTilMotregning.tsx
+++ src/frontend/sider/Fagsak/Behandling/Sider/Simulering/UlovfestetMotregning/BekreftSamtykkeTilMotregning.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
-import { Alert, BodyLong, Button, HStack, Modal } from '@navikt/ds-react';
+import { Alert, BodyLong, Button, HStack } from '@navikt/ds-react';
 
 const StyledAlert = styled(Alert)`
     margin-top: 2rem;
@@ -18,10 +18,6 @@ export const BekreftSamtykkeTilMotregning = ({
     slettTilbakekrevingsvedtakMotregning,
     bekreftSamtykkeTilMotregning,
 }: IProps) => {
-    const [visBekreftSlettingModal, settVisBekreftSlettingModal] = useState<boolean>(false);
-    const åpneModal = () => settVisBekreftSlettingModal(true);
-    const lukkModal = () => settVisBekreftSlettingModal(false);
-
     const [oppdaterer, settOppdaterer] = useState(false);
     const [sletter, settSletter] = useState(false);
 
@@ -33,7 +29,17 @@ export const BekreftSamtykkeTilMotregning = ({
                     feilutbetalingen
                 </BodyLong>
                 <HStack gap="4" justify="center">
-                    <Button onClick={åpneModal} disabled={oppdaterer}>
+                    <Button
+                        onClick={() => {
+                            settSletter(true);
+                            slettTilbakekrevingsvedtakMotregning().finally(() =>
+                                settSletter(false)
+                            );
+                        }}
+                        loading={sletter}
+                        disabled={sletter || oppdaterer}
+                        variant="secondary"
+                    >
                         Nei
                     </Button>
                     <Button
@@ -42,48 +48,12 @@ export const BekreftSamtykkeTilMotregning = ({
                             bekreftSamtykkeTilMotregning().finally(() => settOppdaterer(false));
                         }}
                         loading={oppdaterer}
-                        disabled={oppdaterer}
+                        disabled={oppdaterer || sletter}
                     >
                         Ja
                     </Button>
                 </HStack>
             </StyledAlert>
-            <Modal
-                open={visBekreftSlettingModal}
-                onClose={lukkModal}
-                header={{
-                    heading:
-                        'Er du sikker på at du vil slette tilbakekrevingsvedtak for motregning?',
-                    size: 'small',
-                    closeButton: false,
-                }}
-                width={'35rem'}
-            >
-                <Modal.Footer>
-                    <Button
-                        key={'bekreft'}
-                        onClick={() => {
-                            settSletter(true);
-                            slettTilbakekrevingsvedtakMotregning().finally(() => {
-                                settSletter(false);
-                                settVisBekreftSlettingModal(false);
-                            });
-                        }}
-                        disabled={sletter}
-                        loading={sletter}
-                    >
-                        Bekreft
-                    </Button>
-                    <Button
-                        variant="secondary"
-                        key={'avbryt'}
-                        onClick={lukkModal}
-                        disabled={sletter}
-                    >
-                        Avbryt
-                    </Button>
-                </Modal.Footer>
-            </Modal>
         </>
     );
 };
