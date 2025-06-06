ea8c1f854efbc3333730ac15a2c4c67a2395b8e0
thoalm <thoalm@outlook.com>
2025-06-06 11:39:17 +0200

    NAV-25329: Forenklet litt logikk for utledning av undertitel

--- src/frontend/komponenter/Modal/fagsak/Undertittel.tsx
+++ src/frontend/komponenter/Modal/fagsak/Undertittel.tsx
@@ -5,7 +5,7 @@ import { BodyShort } from '@navikt/ds-react';
 import type { IBaseFagsak } from '../../../typer/fagsak';
 
 function utledUndertittel(fagsakerPåBruker: IBaseFagsak[]) {
-    const harFagsak = (fagsakerPåBruker ?? []).length > 0;
+    const harFagsak = fagsakerPåBruker.length > 0;
     if (harFagsak) {
         return 'Personen har allerede en tilknyttet fagsak. Ønsker du å opprette ny fagsak for denne personen?';
     } else {
