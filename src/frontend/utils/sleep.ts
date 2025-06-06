c23c6ba9e0744de13264582a03885a2ffda49e00
thoalm <thoalm@outlook.com>
2025-06-06 15:40:46 +0200

    NAV-25329: La til en kommentar for "sleep" metoden

--- src/frontend/utils/sleep.ts
+++ src/frontend/utils/sleep.ts
@@ -1,3 +1,8 @@
+/**
+ * Kun brukt for manuell testing. Kjekt for å f.eks. simulere et tregt nettverkskall.
+ *
+ * @param ms - Antall millisekunder man skal vente.
+ */
 export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
