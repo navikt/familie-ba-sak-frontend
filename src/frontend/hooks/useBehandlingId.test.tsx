b84557e4ace90f78c5c49b6d41631e142cab108e
thoalm <thoalm@outlook.com>
2025-06-16 09:29:50 +0200

    NAV-24802: Refaktorer fagsakId og behandlingId hook til enklere mer spesifikke hooks

---
+++ src/frontend/hooks/useBehandlingId.test.tsx
@@ -0,0 +1,66 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import type { PropsWithChildren } from 'react';
+import React from 'react';
+
+import { renderHook } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router';
+
+import { useBehandlingId } from './useBehandlingId';
+
+function Router(entry: string) {
+    return ({ children }: PropsWithChildren) => {
+        return (
+            <MemoryRouter initialEntries={[entry]}>
+                <Routes>
+                    <Route path={'/fagsak/:fagsakId'} element={children} />
+                    <Route path={'/behandling/:behandlingId'} element={children} />
+                    <Route path={'/url'} element={children} />
+                </Routes>
+            </MemoryRouter>
+        );
+    };
+}
+
+describe('useBehandlingId', () => {
+    it('skal returnere behandlingId for gyldig URL sti', () => {
+        const behandlingId = '1234';
+        const { result } = renderHook(() => useBehandlingId(), {
+            wrapper: Router(`/behandling/${behandlingId}`),
+        });
+        expect(result.current).toEqual('1234');
+    });
+
+    it('skal returnere undefined of behandlingId er undefined', () => {
+        const behandlingId = undefined;
+        const { result } = renderHook(() => useBehandlingId(), {
+            wrapper: Router(`/behandling/${behandlingId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for en behandlingId som ikke er et tall', () => {
+        const behandlingId = '123a';
+        const { result } = renderHook(() => useBehandlingId(), {
+            wrapper: Router(`/behandling/${behandlingId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for URL sti uten behandlingId', () => {
+        const fagsakId = '1234';
+        const { result } = renderHook(() => useBehandlingId(), {
+            wrapper: Router(`/fagsak/${fagsakId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for URL sti uten noen parameter', () => {
+        const { result } = renderHook(() => useBehandlingId(), {
+            wrapper: Router(`/url`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+});
---
+++ src/frontend/hooks/useFagsakId.test.tsx
@@ -0,0 +1,66 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import type { PropsWithChildren } from 'react';
+import React from 'react';
+
+import { renderHook } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router';
+
+import { useFagsakId } from './useFagsakId';
+
+function Router(entry: string) {
+    return ({ children }: PropsWithChildren) => {
+        return (
+            <MemoryRouter initialEntries={[entry]}>
+                <Routes>
+                    <Route path={'/fagsak/:fagsakId'} element={children} />
+                    <Route path={'/behandling/:behandlingId'} element={children} />
+                    <Route path={'/url'} element={children} />
+                </Routes>
+            </MemoryRouter>
+        );
+    };
+}
+
+describe('useFagsakId', () => {
+    it('skal returnere fagsakId for gyldig URL sti', () => {
+        const fagsakId = '1234';
+        const { result } = renderHook(() => useFagsakId(), {
+            wrapper: Router(`/fagsak/${fagsakId}`),
+        });
+        expect(result.current).toEqual('1234');
+    });
+
+    it('skal returnere undefined of fagsakId er undefined', () => {
+        const fagsakId = undefined;
+        const { result } = renderHook(() => useFagsakId(), {
+            wrapper: Router(`/fagsak/${fagsakId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for en fagsakid som ikke er et tall', () => {
+        const fagsakId = '123a';
+        const { result } = renderHook(() => useFagsakId(), {
+            wrapper: Router(`/fagsak/${fagsakId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for URL sti uten fagsakid', () => {
+        const behandlingId = '1234';
+        const { result } = renderHook(() => useFagsakId(), {
+            wrapper: Router(`/behandling/${behandlingId}`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+
+    it('skal returnere undefined for URL sti uten noen parameter', () => {
+        const { result } = renderHook(() => useFagsakId(), {
+            wrapper: Router(`/url`),
+        });
+        expect(result.current).toEqual(undefined);
+    });
+});
