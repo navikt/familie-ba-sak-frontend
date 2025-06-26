db9f1df95cdfb24f3dfbbb3590b200b0f9e30af0
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-06-26 10:35:15 +0200

    Fjern styled componentes for Behandlingsmeny

--- src/frontend/sider/Fagsak/Personlinje/Behandlingsmeny/Behandlingsmeny.tsx
+++ src/frontend/sider/Fagsak/Personlinje/Behandlingsmeny/Behandlingsmeny.tsx
@@ -18,30 +18,22 @@ interface IProps {
     behandling?: IBehandling;
 }
 
-const PosisjonertMenyknapp = styled(Button)`
-    margin-left: 3rem;
-`;
-
-const StyletDropdownMenu = styled(Dropdown.Menu)`
-    width: 30ch;
-`;
-
 const Behandlingsmeny: React.FC<IProps> = ({ bruker, minimalFagsak, behandling }) => {
     const skalViseMenyvalgForBehandling =
         behandling && behandling.status !== BehandlingStatus.AVSLUTTET;
 
     return (
         <Dropdown>
-            <PosisjonertMenyknapp
+            <Button
                 variant="secondary"
                 size="small"
                 icon={<ChevronDownIcon />}
                 iconPosition={'right'}
-                forwardedAs={Dropdown.Toggle}
+                as={Dropdown.Toggle}
             >
                 Meny
-            </PosisjonertMenyknapp>
-            <StyletDropdownMenu>
+            </Button>
+            <Dropdown.Menu>
                 <Dropdown.Menu.List>
                     <MenyvalgFagsak minimalFagsak={minimalFagsak} bruker={bruker} />
                     {skalViseMenyvalgForBehandling && <Dropdown.Menu.Divider />}
@@ -52,7 +44,7 @@ const Behandlingsmeny: React.FC<IProps> = ({ bruker, minimalFagsak, behandling }
                         />
                     )}
                 </Dropdown.Menu.List>
-            </StyletDropdownMenu>
+            </Dropdown.Menu>
         </Dropdown>
     );
 };
