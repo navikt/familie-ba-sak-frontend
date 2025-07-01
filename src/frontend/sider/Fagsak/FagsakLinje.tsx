6e443f7514574fc8245fbb2574f4c2c0235efd31
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-07-01 15:39:21 +0200

    Flytter useLocation() ut av funksjon og bytter fra as "a" til "ReactRouterLink"

--- src/frontend/sider/Fagsak/FagsakLinje.tsx
+++ src/frontend/sider/Fagsak/FagsakLinje.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useLocation } from 'react-router';
+import { useLocation, Link as ReactRouterLink } from 'react-router';
 
 import { FileTextIcon, HouseIcon, MagnifyingGlassIcon } from '@navikt/aksel-icons';
 import { Box, Button, HStack } from '@navikt/ds-react';
@@ -16,49 +16,46 @@ interface FagsaklinjeProps {
     behandling?: IBehandling;
 }
 
-const aktivFaneStyle = (fanenavn: string) => {
-    const location = useLocation();
-    const urlSplit = location.pathname.split('/');
+const aktivFaneStyle = (fanenavn: string, pathname: string) => {
+    const urlSplit = pathname.split('/');
     const sluttenPåUrl = urlSplit[urlSplit.length - 1];
     return sluttenPåUrl === fanenavn ? { textDecoration: 'underline' } : {};
 };
 
-const FagsakLinje: React.FunctionComponent<FagsaklinjeProps> = ({
-    minimalFagsak,
-    bruker,
-    behandling,
-}) => {
+const FagsakLinje = ({ minimalFagsak, bruker, behandling }: FagsaklinjeProps) => {
+    const { pathname } = useLocation();
+
     return (
         <Box borderWidth="0 0 1 0" borderColor="border-subtle">
             <HStack paddingInline="2 4" paddingBlock="2" justify="space-between">
                 <HStack>
                     <Button
-                        as="a"
+                        as={ReactRouterLink}
                         size="small"
                         variant="tertiary"
                         icon={<HouseIcon />}
-                        href={`/fagsak/${minimalFagsak.id}/saksoversikt`}
-                        style={aktivFaneStyle('saksoversikt')}
+                        to={`/fagsak/${minimalFagsak.id}/saksoversikt`}
+                        style={aktivFaneStyle('saksoversikt', pathname)}
                     >
                         Saksoversikt
                     </Button>
                     <Button
-                        as="a"
+                        as={ReactRouterLink}
                         size="small"
                         variant="tertiary"
                         icon={<MagnifyingGlassIcon />}
-                        href="infotrygd"
-                        style={aktivFaneStyle('infotrygd')}
+                        to="infotrygd"
+                        style={aktivFaneStyle('infotrygd', pathname)}
                     >
                         Infotrygd
                     </Button>
                     <Button
-                        as="a"
+                        as={ReactRouterLink}
                         size="small"
                         variant="tertiary"
                         icon={<FileTextIcon />}
-                        href={`/fagsak/${minimalFagsak.id}/dokumenter`}
-                        style={aktivFaneStyle('dokumenter')}
+                        to={`/fagsak/${minimalFagsak.id}/dokumenter`}
+                        style={aktivFaneStyle('dokumenter', pathname)}
                     >
                         Dokumenter
                     </Button>
