ee655cbde2a2c4a20b9c16c35ed9bf3f32c23742
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-08-29 12:59:38 +0200

    NAV-26033: Tar i bruk ny PDF modal for tilbakekreving brev (#3989)

---
+++ src/frontend/hooks/useOpprettForhåndsvisbarTilbakekrevingVarselbrevPdf.ts
@@ -0,0 +1,37 @@
+import { type DefaultError, useMutation, type UseMutationOptions } from '@tanstack/react-query';
+
+import { useHttp } from '@navikt/familie-http';
+
+import {
+    opprettForhåndsvisbarTilbakekrevingVarselbrev,
+    type OpprettForhåndsvisTilbakekrevingVarselbrevRequest,
+} from '../api/opprettForhåndsvisbarTilbakekrevingVarselbrev';
+import { opprettPdfBlob } from '../utils/blob';
+
+interface MutationParameters {
+    behandlingId: number;
+    payload: OpprettForhåndsvisTilbakekrevingVarselbrevRequest;
+}
+
+type Options = Omit<UseMutationOptions<Blob, DefaultError, MutationParameters>, 'mutationFn'>;
+
+export function useOpprettForhåndsvisbarTilbakekrevingVarselbrevPdf(options: Options) {
+    const { request } = useHttp();
+    return useMutation({
+        mutationFn: async ({ behandlingId, payload }: MutationParameters) => {
+            try {
+                const bytes = await opprettForhåndsvisbarTilbakekrevingVarselbrev(
+                    request,
+                    behandlingId,
+                    payload
+                );
+                const blob = opprettPdfBlob(bytes);
+                return Promise.resolve(blob);
+            } catch (e) {
+                const error = e instanceof Error ? e : Error('En ukjent feil oppstod.');
+                return Promise.reject(error);
+            }
+        },
+        ...options,
+    });
+}
