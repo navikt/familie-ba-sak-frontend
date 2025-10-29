219991aa09c369f0abd669c7af53ab003f624fb0
NghiNg <60237320+NghiNg@users.noreply.github.com>
2025-10-29 09:25:36 +0100

    NAV-26614 refaktorerer til hook-form registrer dødsfall (#4113)
    * refaktorerer til hook-form registrer dødsfall
    
    ---------
    
    Co-authored-by: juliusbirkevold <62056951+juliusbirkevold@users.noreply.github.com>

---
+++ src/frontend/komponenter/PersonInformasjon/DødsfallDatoFelt.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { startOfDay } from 'date-fns';
+import { useController, useFormContext } from 'react-hook-form';
+
+import { DatePicker, useDatepicker } from '@navikt/ds-react';
+
+import { RegistrerDødsfallDatoFelt, type RegistrerDødsfallDatoFormValues } from './useRegistrerDødsfallDatoSkjema';
+
+export function DødsfallDatoFelt() {
+    const { control } = useFormContext<RegistrerDødsfallDatoFormValues>();
+
+    const {
+        field,
+        fieldState: { error },
+        formState: { isSubmitting },
+    } = useController({
+        name: RegistrerDødsfallDatoFelt.DØDSFALL_DATO,
+        control,
+        rules: { required: 'Du må velge en gyldig dato.' },
+    });
+
+    const { datepickerProps, inputProps } = useDatepicker({
+        onDateChange: field.onChange,
+        toDate: startOfDay(new Date()),
+        required: true,
+    });
+
+    return (
+        <DatePicker {...datepickerProps}>
+            <DatePicker.Input
+                {...inputProps}
+                label={'Dødsdato'}
+                placeholder={'DD.MM.ÅÅÅÅ'}
+                ref={field.ref}
+                name={field.name}
+                onBlur={field.onBlur}
+                disabled={isSubmitting}
+                readOnly={isSubmitting}
+                error={error?.message}
+            />
+        </DatePicker>
+    );
+}
--- src/frontend/komponenter/PersonInformasjon/RegistrerDødsfallDatoModal.tsx
+++ src/frontend/komponenter/PersonInformasjon/RegistrerDødsfallDatoModal.tsx
@@ -1,28 +1,40 @@
 import * as React from 'react';
 
-import { Button, Fieldset, Modal, Textarea } from '@navikt/ds-react';
-import { RessursStatus } from '@navikt/familie-typer';
+import { FormProvider } from 'react-hook-form';
 
+import { Button, Fieldset, Modal } from '@navikt/ds-react';
+
+import { BegrunnelseFelt } from './BegrunnelseFelt';
+import { DødsfallDatoFelt } from './DødsfallDatoFelt';
 import { useRegistrerDødsfallDatoSkjema } from './useRegistrerDødsfallDatoSkjema';
 import type { IGrunnlagPerson } from '../../typer/person';
-import Datovelger from '../Datovelger/Datovelger';
 
-interface IProps {
+interface Props {
     lukkModal: () => void;
     person: IGrunnlagPerson;
-    erLesevisning: boolean;
 }
 
-const RegistrerDødsfallDatoModal = ({ lukkModal, person, erLesevisning }: IProps) => {
-    const { skjema, valideringErOk, registrerManuellDødsfall, restFeil } = useRegistrerDødsfallDatoSkjema({
+export const RegistrerDødsfallDatoModal = ({ lukkModal, person }: Props) => {
+    const { form, onSubmit } = useRegistrerDødsfallDatoSkjema({
         lukkModal,
         person,
     });
 
+    const {
+        handleSubmit,
+        formState: { isSubmitting, errors },
+        reset,
+    } = form;
+
+    const onClose = () => {
+        lukkModal();
+        reset();
+    };
+
     return (
         <Modal
             open
-            onClose={lukkModal}
+            onClose={onClose}
             header={{
                 heading: 'Registrere dødsdato',
                 size: 'medium',
@@ -30,47 +42,29 @@ const RegistrerDødsfallDatoModal = ({ lukkModal, person, erLesevisning }: IProp
             width={'35rem'}
             portal
         >
-            <Modal.Body>
-                <Fieldset
-                    legend="Registrer dødsfall"
-                    hideLegend
-                    error={skjema.visFeilmeldinger && restFeil}
-                    errorPropagation={false}
-                >
-                    <Datovelger
-                        felt={skjema.felter.dødsfallDato}
-                        label={'Dødsdato'}
-                        visFeilmeldinger={skjema.visFeilmeldinger}
-                        readOnly={erLesevisning}
-                        kanKunVelgeFortid
-                    />
-                    <Textarea
-                        {...skjema.felter.begrunnelse?.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
-                        id={'manuell-dødsdato-begrunnelse'}
-                        label={'Begrunnelse'}
-                        readOnly={erLesevisning}
-                        value={skjema.felter.begrunnelse.verdi}
-                        onChange={changeEvent => skjema.felter.begrunnelse.validerOgSettFelt(changeEvent.target.value)}
-                    />
-                </Fieldset>
-            </Modal.Body>
-            {!erLesevisning && (
-                <Modal.Footer>
-                    <Button
-                        onClick={registrerManuellDødsfall}
-                        variant={valideringErOk() ? 'primary' : 'secondary'}
-                        loading={skjema.submitRessurs.status === RessursStatus.HENTER}
-                        disabled={skjema.submitRessurs.status === RessursStatus.HENTER}
-                    >
-                        Bekreft
-                    </Button>
-                    <Button onClick={lukkModal} variant={'tertiary'}>
-                        Avbryt
-                    </Button>
-                </Modal.Footer>
-            )}
+            <FormProvider {...form}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Modal.Body>
+                        <Fieldset
+                            legend="Registrer dødsfall"
+                            hideLegend
+                            error={errors.root?.message}
+                            errorPropagation={false}
+                        >
+                            <DødsfallDatoFelt />
+                            <BegrunnelseFelt />
+                        </Fieldset>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type={'submit'} variant={'primary'} loading={isSubmitting} disabled={isSubmitting}>
+                            Bekreft
+                        </Button>
+                        <Button onClick={onClose} variant={'tertiary'}>
+                            Avbryt
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </FormProvider>
         </Modal>
     );
 };
-
-export default RegistrerDødsfallDatoModal;
