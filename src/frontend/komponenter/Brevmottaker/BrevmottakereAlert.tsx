9d7a75c705bcf6a2ed8e9950e20a9449c1d91b02
Nguyen <nghi.nguyen@nav.no>
2025-10-31 13:23:03 +0100

    BrevMottakereAlert: gå bort fra styled-components

--- src/frontend/komponenter/Brevmottaker/BrevmottakereAlert.tsx
+++ src/frontend/komponenter/Brevmottaker/BrevmottakereAlert.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import { useState } from 'react';
 
 import { useLocation } from 'react-router';
-import styled from 'styled-components';
 
 import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
-import { Alert, Button } from '@navikt/ds-react';
+import { Alert, Button, VStack } from '@navikt/ds-react';
 
 import BrevmottakerListe from './BrevmottakerListe';
 import { sider } from '../../sider/Fagsak/Behandling/Sider/sider';
@@ -36,10 +35,6 @@ interface BrevmottakereAlertFagsakProps extends Props {
     brevmottakere: SkjemaBrevmottaker[];
 }
 
-const StyledAlert = styled(Alert)`
-    margin-bottom: 1.5rem;
-`;
-
 export const BrevmottakereAlert: React.FC<
     BrevmottakereAlertBehandlingProps | BrevmottakereAlertFagsakProps
 > = props => {
@@ -61,20 +56,21 @@ export const BrevmottakereAlert: React.FC<
     return (
         <>
             {brevmottakere && brevmottakere.length !== 0 && (
-                <StyledAlert variant="info" className={className}>
-                    {`Brevmottaker(e) er endret, og ${hentBrevtypetekst(location.pathname)} sendes til:`}
-                    <BrevmottakerListe brevmottakere={brevmottakere} bruker={bruker} />
-                    <Button
-                        variant={'tertiary'}
-                        onClick={() => settVisManuelleMottakereModal(true)}
-                        icon={<MagnifyingGlassIcon />}
-                        size={'xsmall'}
-                    >
-                        Se detaljer
-                    </Button>
-                </StyledAlert>
+                <VStack marginBlock={'space-40 space-24'}>
+                    <Alert variant="info" className={className}>
+                        {`Brevmottaker(e) er endret, og ${hentBrevtypetekst(location.pathname)} sendes til:`}
+                        <BrevmottakerListe brevmottakere={brevmottakere} bruker={bruker} />
+                        <Button
+                            variant={'tertiary'}
+                            onClick={() => settVisManuelleMottakereModal(true)}
+                            icon={<MagnifyingGlassIcon />}
+                            size={'xsmall'}
+                        >
+                            Se detaljer
+                        </Button>
+                    </Alert>
+                </VStack>
             )}
-
             {visManuelleMottakereModal &&
                 (props.erPåBehandling ? (
                     <LeggTilBrevmottakerModalBehandling
