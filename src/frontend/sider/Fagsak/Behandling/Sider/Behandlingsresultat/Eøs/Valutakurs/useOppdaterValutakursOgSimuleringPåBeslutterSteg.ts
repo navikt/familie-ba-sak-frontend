c79fe3803fa387bab14c7b3aff02863cda60781b
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-02-16 13:05:38 +0100

    Bruk eget endepunkt for beslutter som har oppdatert valutakurs/simulering (#4351)

--- src/frontend/sider/Fagsak/Behandling/Sider/Behandlingsresultat/Eøs/Valutakurs/useOppdaterValutakursOgSimuleringPåBeslutterSteg.ts
+++ src/frontend/sider/Fagsak/Behandling/Sider/Behandlingsresultat/Eøs/Valutakurs/useOppdaterValutakursOgSimuleringPåBeslutterSteg.ts
@@ -3,24 +3,28 @@ import { useEffect } from 'react';
 import { useHttp } from '@navikt/familie-http';
 
 import { useAppContext } from '../../../../../../../context/AppContext';
+import { useFeatureToggles } from '../../../../../../../hooks/useFeatureToggles';
 import {
     BehandlerRolle,
     BehandlingStatus,
     BehandlingSteg,
     type IBehandling,
 } from '../../../../../../../typer/behandling';
+import { FeatureToggle } from '../../../../../../../typer/featureToggles';
 import { useBehandlingContext } from '../../../../context/BehandlingContext';
 
 export const useOppdaterValutakursOgSimuleringPåBeslutterSteg = () => {
     const { hentSaksbehandlerRolle } = useAppContext();
     const { request } = useHttp();
     const { settÅpenBehandling, behandling } = useBehandlingContext();
+    const toggles = useFeatureToggles();
 
     useEffect(() => {
         if (
             behandling.status === BehandlingStatus.FATTER_VEDTAK &&
             behandling.steg == BehandlingSteg.BESLUTTE_VEDTAK &&
-            hentSaksbehandlerRolle() === BehandlerRolle.BESLUTTER
+            hentSaksbehandlerRolle() === BehandlerRolle.BESLUTTER &&
+            !toggles[FeatureToggle.hentBehandlingEndepunktForBeslutter]
         ) {
             request<void, IBehandling>({
                 method: 'PUT',
