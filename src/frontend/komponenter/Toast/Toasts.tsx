2b65c435ca2e5357f655d71281c433f3c1ebb3a6
Nguyen <nghi.nguyen@nav.no>
2026-02-25 17:53:02 +0100

    wip

--- src/frontend/komponenter/Toast/Toasts.tsx
+++ src/frontend/komponenter/Toast/Toasts.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
 import Toast from './Toast';
+import styles from './Toasts.module.css';
 import { useAppContext } from '../../context/AppContext';
-
-const Container = styled.div`
-    position: fixed;
-    right: 2rem;
-    float: right;
-    bottom: 0;
-    z-index: 9999;
-`;
 
 const Toasts: React.FC = () => {
     const { toasts } = useAppContext();
 
     return (
-        <Container>
+        <div className={styles.container}>
             {Object.entries(toasts).map(([toastId, toast]) => (
-                <Toast key={toastId} toastId={toastId} toast={toast} />
+                <Toast key={toastId + 'ya'} toastId={toastId} toast={toast} />
             ))}
-        </Container>
+        </div>
     );
 };
 
