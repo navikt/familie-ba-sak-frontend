438aec0f963fe483f8ce711f1a546cd5e2647a09
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-03-06 14:46:23 +0100

    Bruk egen URL for umami dev-miljø (#4380)

--- src/frontend/hooks/useStartUmami.ts
+++ src/frontend/hooks/useStartUmami.ts
@@ -5,11 +5,12 @@ import { erProd } from '../utils/miljø';
 export const useStartUmami = () => {
     useEffect(() => {
         const websiteId = erProd() ? 'e0e5e641-6d90-46ef-823c-e11ef40472cc' : 'a95c215b-4c37-450f-8ec6-b1355d53a36b';
+        const hostUrl = erProd() ? 'https://umami.nav.no' : 'https://reops-event-proxy.ekstern.dev.nav.no';
 
         const script = document.createElement('script');
         script.src = 'https://cdn.nav.no/team-researchops/sporing/sporing.js';
         script.defer = true;
-        script.setAttribute('data-host-url', 'https://umami.nav.no');
+        script.setAttribute('data-host-url', hostUrl);
         script.setAttribute('data-website-id', websiteId);
 
         document.body.appendChild(script);
