bbba75107a874e0113fe018e8c743bd821046a69
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-03-17 14:30:30 +0100

    Rydd opp bruk av NODE_ENV

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -8,13 +8,14 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { erLokal, erProd } from './utils/miljø';
 
 // Setter default locale til norsk bokmål for date-fns
 setDefaultOptions({ locale: nb });
 
 const environment = window.location.hostname;
 
-if (process.env.NODE_ENV !== 'development') {
+if (!erLokal()) {
     Sentry.init({
         dsn: 'https://10239ce4baed4db79d080d85f08b5878@sentry.gc.nav.no/26',
         environment,
@@ -23,7 +24,7 @@ if (process.env.NODE_ENV !== 'development') {
     });
 }
 
-if (process.env.NODE_ENV !== 'production') {
+if (!erProd()) {
     axe(React, ReactDOM, 1000);
 }
 
