769b97c60da169493a184baa4b7a488acb9b736f
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2026-04-16 09:13:26 +0200

    NAV-28039:Tar i bruk ny RegionCombobox (#4427)

--- src/frontend/sider/Fagsak/Behandling/Sider/Vedtak/RefusjonEøs/form/LandkodeField.tsx
+++ src/frontend/sider/Fagsak/Behandling/Sider/Vedtak/RefusjonEøs/form/LandkodeField.tsx
@@ -4,6 +4,9 @@ import { useController, useFormContext } from 'react-hook-form';
 
 import Styles from './LandkodeField.module.css';
 import { Fields, type FormValues } from './useRefusjonEøsForm';
+import { useFeatureToggles } from '../../../../../../../hooks/useFeatureToggles';
+import { EØS_LAND_REGIONKODER, RegionCombobox, type Regionkode } from '../../../../../../../komponenter/FlaggCombobox';
+import { FeatureToggle } from '../../../../../../../typer/featureToggles';
 import { FamilieLandvelger } from '../../../Behandlingsresultat/Eøs/EøsKomponenter/FamilieLandvelger';
 
 interface Props {
@@ -12,6 +15,7 @@ interface Props {
 
 export function LandkodeField({ readOnly = false }: Props) {
     const id = useId();
+    const toggles = useFeatureToggles();
 
     const { control } = useFormContext<FormValues>();
 
@@ -26,16 +30,30 @@ export function LandkodeField({ readOnly = false }: Props) {
     });
 
     return (
-        <FamilieLandvelger
-            id={`refusjon-eøs-form${id}`}
-            label={'EØS-land'}
-            kunEøs={true}
-            eksluderLand={['NO']}
-            value={value}
-            onChange={value => onChange(value.value)}
-            erLesevisning={readOnly || isSubmitting}
-            feil={error?.message}
-            className={Styles.landvelger}
-        />
+        <>
+            {toggles[FeatureToggle.brukNyFlagCombobox] ? (
+                <RegionCombobox
+                    label={'EØS-land'}
+                    value={value as Regionkode}
+                    options={EØS_LAND_REGIONKODER.filter(regionkode => regionkode !== 'NO')}
+                    onChange={value => onChange(value)}
+                    readOnly={readOnly || isSubmitting}
+                    error={error?.message}
+                    className={Styles.landvelger}
+                />
+            ) : (
+                <FamilieLandvelger
+                    id={`refusjon-eøs-form${id}`}
+                    label={'EØS-land'}
+                    kunEøs={true}
+                    eksluderLand={['NO']}
+                    value={value}
+                    onChange={value => onChange(value.value)}
+                    erLesevisning={readOnly || isSubmitting}
+                    feil={error?.message}
+                    className={Styles.landvelger}
+                />
+            )}
+        </>
     );
 }
