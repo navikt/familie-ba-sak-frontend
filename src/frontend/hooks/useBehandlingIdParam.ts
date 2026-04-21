e6dccae76c580a63f7cdc5488d85491fb5c202a1
thoalm <thoalm@outlook.com>
2026-04-21 06:37:16 +0200

    NAV-28633: La til kommentarer på param hookene

--- src/frontend/hooks/useBehandlingIdParam.ts
+++ src/frontend/hooks/useBehandlingIdParam.ts
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router';
 
+/**
+ * Bruk {@link useBehandlingId} framfor denne, hvis du befinner deg inne i BehandlingContext.
+ */
 export function useBehandlingIdParam(): number | undefined {
     const { behandlingId } = useParams();
     const erTall = behandlingId !== undefined && !isNaN(Number(behandlingId));
--- src/frontend/hooks/useFagsakIdParam.ts
+++ src/frontend/hooks/useFagsakIdParam.ts
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router';
 
+/**
+ * Bruk {@link useFagsakId} framfor denne, hvis du befinner deg inne i FagsakContext.
+ */
 export function useFagsakIdParam(): number | undefined {
     const { fagsakId } = useParams();
     const erTall = fagsakId !== undefined && !isNaN(Number(fagsakId));
