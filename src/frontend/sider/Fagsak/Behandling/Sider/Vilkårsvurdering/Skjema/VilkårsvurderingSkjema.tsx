8ec8e229ee18d8e3d44e69beb052e741dfc910cc
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2026-05-12 10:43:20 +0200

    NAV-28170: Tilpasser vilkårsvurdering for småe skjermer (#4513)

--- src/frontend/sider/Fagsak/Behandling/Sider/Vilkårsvurdering/Skjema/VilkårsvurderingSkjema.tsx
+++ src/frontend/sider/Fagsak/Behandling/Sider/Vilkårsvurdering/Skjema/VilkårsvurderingSkjema.tsx
@@ -1,29 +1,30 @@
-import VilkårsvurderingSkjemaEnsligMindreårig from './VilkårsvurderingSkjemaEnsligMindreårig';
-import VilkårsvurderingSkjemaInstitusjon from './VilkårsvurderingSkjemaInstitusjon';
-import VilkårsvurderingSkjemaNormal from './VilkårsvurderingSkjemaNormal';
-import { FagsakType } from '../../../../../../typer/fagsak';
-import { useFagsakContext } from '../../../../FagsakContext';
-import { useBehandlingContext } from '../../../context/BehandlingContext';
+import { useFagsak } from '@hooks/useFagsak';
+import { FagsakType } from '@typer/fagsak';
 
-interface IProps {
+import { VilkårsvurderingSkjemaEnsligMindreårig } from './VilkårsvurderingSkjemaEnsligMindreårig';
+import { VilkårsvurderingSkjemaInstitusjon } from './VilkårsvurderingSkjemaInstitusjon';
+import { VilkårsvurderingSkjemaNormal } from './VilkårsvurderingSkjemaNormal';
+
+interface Props {
     visFeilmeldinger: boolean;
 }
 
-const VilkårsvurderingSkjema = ({ visFeilmeldinger }: IProps) => {
-    const { fagsak } = useFagsakContext();
+export function VilkårsvurderingSkjema({ visFeilmeldinger }: Props) {
+    const fagsak = useFagsak();
 
-    const { samhandlerOrgnr } = useBehandlingContext();
+    const samhandlerOrgnr = fagsak.institusjon?.orgNummer;
 
     if (fagsak.fagsakType === FagsakType.NORMAL || fagsak.fagsakType === FagsakType.SKJERMET_BARN) {
         return <VilkårsvurderingSkjemaNormal visFeilmeldinger={visFeilmeldinger} />;
     }
+
     if (fagsak.fagsakType === FagsakType.INSTITUSJON && samhandlerOrgnr) {
         return <VilkårsvurderingSkjemaInstitusjon visFeilmeldinger={visFeilmeldinger} />;
     }
+
     if (fagsak.fagsakType === FagsakType.BARN_ENSLIG_MINDREÅRIG) {
         return <VilkårsvurderingSkjemaEnsligMindreårig visFeilmeldinger={visFeilmeldinger} />;
     }
-    return null;
-};
 
-export default VilkårsvurderingSkjema;
+    return null;
+}
--- src/frontend/sider/Fagsak/Behandling/Sider/Vilkårsvurdering/Skjema/VilkårsvurderingSkjemaInstitusjon.tsx
+++ src/frontend/sider/Fagsak/Behandling/Sider/Vilkårsvurdering/Skjema/VilkårsvurderingSkjemaInstitusjon.tsx
@@ -1,38 +1,53 @@
+import { useBehandlingId } from '@hooks/useBehandlingId';
+import { Skjermstørrelse, useSkjermstørrelse } from '@hooks/useSkjermstørrelse';
+import { PersonInformasjon } from '@komponenter/PersonInformasjon/PersonInformasjon';
+import { useSamhandlerRequest } from '@komponenter/Samhandler/useSamhandler';
+import { PersonType } from '@typer/person';
+import type { IPersonResultat } from '@typer/vilkår';
+import { annenVurderingConfig, AnnenVurderingType, vilkårConfigInstitusjon } from '@typer/vilkår';
+
 import { Bleed, Box, HStack, LocalAlert } from '@navikt/ds-react';
 import { RessursStatus } from '@navikt/familie-typer';
 
-import { PersonInformasjon } from '../../../../../../komponenter/PersonInformasjon/PersonInformasjon';
 import SamhandlerInformasjon from '../../../../../../komponenter/Samhandler/SamhandlerInformasjon';
-import { useSamhandlerRequest } from '../../../../../../komponenter/Samhandler/useSamhandler';
-import { PersonType } from '../../../../../../typer/person';
-import type { IPersonResultat } from '../../../../../../typer/vilkår';
-import { annenVurderingConfig, AnnenVurderingType, vilkårConfigInstitusjon } from '../../../../../../typer/vilkår';
-import { useBehandlingContext } from '../../../context/BehandlingContext';
 import GeneriskAnnenVurdering from '../GeneriskAnnenVurdering/GeneriskAnnenVurdering';
 import GeneriskVilkår from '../GeneriskVilkår/GeneriskVilkår';
 import Registeropplysninger from '../Registeropplysninger/Registeropplysninger';
 import { useVilkårsvurderingContext } from '../VilkårsvurderingContext';
 import styles from './VilkårsvurderingSkjema.module.css';
 
-interface IProps {
+interface Props {
     visFeilmeldinger: boolean;
 }
 
-const VilkårsvurderingSkjemaInstitusjon = ({ visFeilmeldinger }: IProps) => {
-    const { behandling } = useBehandlingContext();
+export function VilkårsvurderingSkjemaInstitusjon({ visFeilmeldinger }: Props) {
     const { vilkårsvurdering } = useVilkårsvurderingContext();
     const { hentOgSettSamhandler, samhandlerRessurs } = useSamhandlerRequest(true);
 
+    const behandlingId = useBehandlingId();
+    const skjermstørrelse = useSkjermstørrelse();
+
     if (samhandlerRessurs.status === RessursStatus.IKKE_HENTET) {
-        hentOgSettSamhandler(behandling.behandlingId);
+        hentOgSettSamhandler(behandlingId);
     }
 
+    const erStorSkjerm = skjermstørrelse > Skjermstørrelse['2XL'];
     const personResultat = vilkårsvurdering.find((value: IPersonResultat) => value.person.type === PersonType.BARN);
     const opplysningsplikt = personResultat?.andreVurderinger.find(
         value => value.verdi.type === AnnenVurderingType.OPPLYSNINGSPLIKT
     );
 
-    return personResultat ? (
+    if (!personResultat) {
+        return (
+            <LocalAlert status={'error'}>
+                <LocalAlert.Header>
+                    <LocalAlert.Title>Finner ingen vilkår på behandlingen</LocalAlert.Title>
+                </LocalAlert.Header>
+            </LocalAlert>
+        );
+    }
+
+    return (
         <>
             {opplysningsplikt && (
                 <>
@@ -40,7 +55,7 @@ const VilkårsvurderingSkjemaInstitusjon = ({ visFeilmeldinger }: IProps) => {
                         {samhandlerRessurs.status === RessursStatus.SUKSESS ? (
                             <SamhandlerInformasjon samhandler={samhandlerRessurs.data} somOverskrift />
                         ) : (
-                            <LocalAlert status="warning">
+                            <LocalAlert status={'warning'}>
                                 <LocalAlert.Header>
                                     <LocalAlert.Title>Klarte ikke hente opplysninger om institusjon</LocalAlert.Title>
                                 </LocalAlert.Header>
@@ -48,7 +63,7 @@ const VilkårsvurderingSkjemaInstitusjon = ({ visFeilmeldinger }: IProps) => {
                         )}
                     </HStack>
                     <Bleed marginBlock={'space-0 space-24'}>
-                        <Box paddingInline={'space-56 space-0'}>
+                        <Box paddingInline={erStorSkjerm ? 'space-56 space-0' : 'space-0'}>
                             <GeneriskAnnenVurdering
                                 person={personResultat.person}
                                 andreVurderinger={personResultat.andreVurderinger}
@@ -59,17 +74,17 @@ const VilkårsvurderingSkjemaInstitusjon = ({ visFeilmeldinger }: IProps) => {
                     </Bleed>
                 </>
             )}
-            <HStack paddingBlock={'space-56 space-32'} justify={'space-between'} className={styles.personLinje}>
+            <HStack paddingBlock={'space-32'} justify={'space-between'} className={styles.personLinje}>
                 <PersonInformasjon person={personResultat.person} />
             </HStack>
-            <Box paddingInline={'space-56 space-0'}>
+            <Box paddingInline={erStorSkjerm ? 'space-56 space-0' : 'space-0'}>
                 {personResultat.person.registerhistorikk ? (
                     <Registeropplysninger
                         registerHistorikk={personResultat.person.registerhistorikk}
                         fødselsdato={personResultat.person.fødselsdato}
                     />
                 ) : (
-                    <LocalAlert status="warning">
+                    <LocalAlert status={'warning'}>
                         <LocalAlert.Header>
                             <LocalAlert.Title>Klarte ikke hente registeropplysninger</LocalAlert.Title>
                         </LocalAlert.Header>
@@ -91,13 +106,5 @@ const VilkårsvurderingSkjemaInstitusjon = ({ visFeilmeldinger }: IProps) => {
                 })}
             </Box>
         </>
-    ) : (
-        <LocalAlert status="error">
-            <LocalAlert.Header>
-                <LocalAlert.Title>Finner ingen vilkår på behandlingen</LocalAlert.Title>
-            </LocalAlert.Header>
-        </LocalAlert>
     );
-};
-
-export default VilkårsvurderingSkjemaInstitusjon;
+}
