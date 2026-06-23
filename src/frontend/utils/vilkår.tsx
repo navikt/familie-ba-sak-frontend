cdb6f7127b1e29926f20c473d39468982e769b7e
Nguyen <nghi.nguyen@nav.no>
2026-06-23 10:51:09 +0200

    NAV-26302: vilkår

--- src/frontend/utils/vilkår.tsx
+++ src/frontend/utils/vilkår.tsx
@@ -1,28 +1,18 @@
 import type { ReactNode } from 'react';
 
-import styled from 'styled-components';
+import { Regelverk } from '@typer/vilkår';
 
-import { StarsEuIcon, FlagCrossIcon } from '@navikt/aksel-icons';
+import { FlagCrossIcon, StarsEuIcon } from '@navikt/aksel-icons';
 
-import { Regelverk } from '../typer/vilkår';
-
-const NorskFlaggIkon = styled(FlagCrossIcon)`
-    font-size: 1.5rem;
-    min-width: 1.5rem;
-`;
-
-const EuIkon = styled(StarsEuIcon)`
-    font-size: 1.5rem;
-    min-width: 1.5rem;
-`;
+import styles from './vilkår.module.css';
 
 export const alleRegelverk: Record<Regelverk, { tekst: string; symbol: ReactNode }> = {
     [Regelverk.NASJONALE_REGLER]: {
         tekst: 'Nasjonale regler',
-        symbol: <NorskFlaggIkon />,
+        symbol: <FlagCrossIcon className={styles.icon} />,
     },
     [Regelverk.EØS_FORORDNINGEN]: {
         tekst: 'EØS-forordningen',
-        symbol: <EuIkon />,
+        symbol: <StarsEuIcon className={styles.icon} />,
     },
 };
