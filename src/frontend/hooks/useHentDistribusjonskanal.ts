b04e2ee9d53674da06bb0db0adc1c36c1674be4a
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-07-01 13:04:26 +0200

    Legg til enabled: personIdent !== undefined

--- src/frontend/hooks/useHentDistribusjonskanal.ts
+++ src/frontend/hooks/useHentDistribusjonskanal.ts
@@ -1,9 +1,9 @@
 import { hentDistribusjonskanal } from '@api/hentDistribusjonskanal';
 import { MetaKey } from '@hooks/meta/metaKey';
-import { type DefaultError, useQuery, type UseQueryOptions } from '@tanstack/react-query';
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 import type { Distribusjonskanal } from '@typer/dokument';
 
-type Options = Omit<UseQueryOptions<Distribusjonskanal, DefaultError>, 'queryKey' | 'queryFn'>;
+type Options = Omit<UseQueryOptions<Distribusjonskanal>, 'queryKey' | 'queryFn' | 'enabled'>;
 
 export const HentDistribusjonskanalQueryKeyFactory = {
     distribusjonskanal: (personIdent: string | undefined) => ['hentDistribusjonskanal', personIdent],
@@ -19,6 +19,7 @@ export function useHentDistribusjonskanal(personIdent: string | undefined, optio
             return hentDistribusjonskanal(personIdent);
         },
         meta: { [MetaKey.VIS_SYSTEMET_LASTER]: true },
+        enabled: personIdent !== undefined,
         ...options,
     });
 }
