21d18b73bef2def1c5ec927becd6bdb508e22e1d
Nguyen <nghi.nguyen@nav.no>
2026-07-07 12:06:58 +0200

    NAV-26302: Simulering

--- src/frontend/sider/Fagsak/Behandling/Sider/Simulering/UlovfestetMotregning/SettBehandlingPåVentModalMotregning.tsx
+++ src/frontend/sider/Fagsak/Behandling/Sider/Simulering/UlovfestetMotregning/SettBehandlingPåVentModalMotregning.tsx
@@ -1,29 +1,16 @@
 import { useState } from 'react';
 
+import { type IBehandling, type ISettPåVent, SettPåVentÅrsak, settPåVentÅrsaker } from '@typer/behandling';
+import { dagensDato, dateTilIsoDatoString } from '@utils/dato';
+import { hentFrontendFeilmelding } from '@utils/ressursUtils';
 import { addDays } from 'date-fns';
-import styled from 'styled-components';
 
-import { BodyShort, Button, DatePicker, Fieldset, Modal, Select, useDatepicker } from '@navikt/ds-react';
+import { BodyShort, Button, DatePicker, Fieldset, Modal, Select, useDatepicker, VStack } from '@navikt/ds-react';
 import { useHttp } from '@navikt/familie-http';
 import { byggHenterRessurs, byggTomRessurs, type Ressurs, RessursStatus } from '@navikt/familie-typer';
 
 import { dagerFristForAvventerSamtykkeUlovfestetMotregning } from './useTilbakekrevingsvedtakMotregning';
-import {
-    type IBehandling,
-    type ISettPåVent,
-    SettPåVentÅrsak,
-    settPåVentÅrsaker,
-} from '../../../../../../typer/behandling';
-import { dagensDato, dateTilIsoDatoString } from '../../../../../../utils/dato';
-import { hentFrontendFeilmelding } from '../../../../../../utils/ressursUtils';
 import { useBehandlingContext } from '../../../context/BehandlingContext';
-
-const Feltmargin = styled.div`
-    margin-bottom: 2rem;
-`;
-const StyledBodyShort = styled(BodyShort)`
-    margin-bottom: 2rem;
-`;
 
 interface IProps {
     lukkModal: () => void;
@@ -80,23 +67,23 @@ export const SettBehandlingPåVentModalMotregning = ({ lukkModal, behandling }: 
                     legend="Sett behandling på vent"
                     hideLegend
                 >
-                    {erBehandlingAlleredePåVent && <StyledBodyShort>Behandlingen er satt på vent.</StyledBodyShort>}
+                    <VStack gap={'space-32'}>
+                        {erBehandlingAlleredePåVent && <BodyShort>Behandlingen er satt på vent.</BodyShort>}
 
-                    <StyledBodyShort>
-                        Behandlingen settes på vent i {dagerFristForAvventerSamtykkeUlovfestetMotregning} dager mens vi
-                        venter på svar fra bruker.
-                    </StyledBodyShort>
+                        <BodyShort>
+                            Behandlingen settes på vent i {dagerFristForAvventerSamtykkeUlovfestetMotregning} dager mens
+                            vi venter på svar fra bruker.
+                        </BodyShort>
 
-                    <Feltmargin>
                         <DatePicker dropdownCaption {...datepickerProps}>
                             <DatePicker.Input {...inputProps} label={'Frist'} readOnly={true} />
                         </DatePicker>
-                    </Feltmargin>
-                    <Select label={'Årsak'} readOnly={true}>
-                        <option value={årsak.valueOf()} key={årsak.valueOf()}>
-                            {settPåVentÅrsaker[årsak]}
-                        </option>
-                    </Select>
+                        <Select label={'Årsak'} readOnly={true}>
+                            <option value={årsak.valueOf()} key={årsak.valueOf()}>
+                                {settPåVentÅrsaker[årsak]}
+                            </option>
+                        </Select>
+                    </VStack>
                 </Fieldset>
             </Modal.Body>
             <Modal.Footer>
