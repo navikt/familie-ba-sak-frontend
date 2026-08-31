20281bce33462cd3d7d14d17c7948fed5147ac91
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2026-08-31 11:15:42 +0200

    NAV-30416: Deler vitest opp for backend og frontend (#4776)

--- src/frontend/vitest.config.ts
+++ src/frontend/vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    root: import.meta.dirname,
     test: {
         setupFiles: ['./vitest.setup.ts'],
         globals: true,
